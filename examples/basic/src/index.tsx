032fe2f28793e65be65c37db2d309990b4420f4e
Tanner Linsley <tannerlinsley@gmail.com>
2021-10-17 14:07:02 -0600

    Update index.tsx

--- examples/basic/src/index.tsx
+++ examples/basic/src/index.tsx
@@ -55,16 +55,6 @@ function Posts() {
 
   return (
     <div>
-      <p>
-        As you visit the posts below, you will notice them in a loading state
-        the first time you load them. However, after you return to this list and
-        click on any posts you have already visited again, you will see them
-        load instantly and background refresh right before your eyes!{" "}
-        <strong>
-          (You may need to throttle your network speed to simulate longer
-          loading sequences)
-        </strong>
-      </p>
       <h1>Posts</h1>
       <div>
         {posts.map((post) => (
