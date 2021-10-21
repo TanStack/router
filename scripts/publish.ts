74ed23f9fe82e008437144e8f9ccc3c6e45a6a67
Tanner Linsley <tannerlinsley@gmail.com>
2021-10-20 22:55:18 -0600

    chore: deploy

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -255,10 +255,10 @@ async function run() {
   packageNames.map((packageName) => {
     let packageDir = path.join(rootDir, 'packages', packageName)
     console.log()
-    console.log(`  npm publish ${packageDir} --tag ${tag} `)
+    console.log(`  yarn publish ${packageDir} --tag ${tag} `)
     console.log()
     execSync(
-      `npm publish ${packageDir} --tag ${tag} --token ${process.env.NPM_TOKEN}`,
+      `yarn publish ${packageDir} --tag ${tag} --token ${process.env.NPM_TOKEN}`,
       { stdio: 'inherit' }
     )
   })
