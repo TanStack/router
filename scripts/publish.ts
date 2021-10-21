62a46b11a0ebeae538dccb98419598cc9cf8ee40
Tanner Linsley <tannerlinsley@gmail.com>
2021-10-20 23:47:23 -0600

    chore:

--- scripts/publish.ts
+++ scripts/publish.ts
@@ -218,9 +218,8 @@ async function run() {
   }
 
   // Tag and commit
-  execSync(`git commit --all --message="Version ${version}"`)
-  execSync(`git tag -a -m "Version ${version}" v${version}`)
-  console.log(chalk.green(`  Committed and tagged version ${version}`))
+  execSync(`git tag -a -m "v${version}" v${version}`)
+  console.log(chalk.green(`  Tagged version ${version}`))
 
   let taggedVersion = getTaggedVersion()
   if (!taggedVersion) {
