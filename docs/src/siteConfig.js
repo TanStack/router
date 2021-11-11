fbd815283c56c3d67038af3d4b11f6566e52c74f
Tanner Linsley <tannerlinsley@gmail.com>
2021-11-11 15:56:11 -0700

    docs: fix edit this page

--- docs/src/siteConfig.js
+++ docs/src/siteConfig.js
@@ -1,7 +1,7 @@
 // List of projects/orgs using your project for the users page.
 export const siteConfig = {
   editUrl:
-    'https://github.com/tannerlinsley/react-location/edit/master/docs/src/pages',
+    'https://github.com/tannerlinsley/react-location/edit/next/docs/src/pages',
   copyright: `Copyright © ${new Date().getFullYear()} Tanner Linsley. All Rights Reserved.`,
   repoUrl: 'https://github.com/tannerlinsley/react-location',
   algolia: {
