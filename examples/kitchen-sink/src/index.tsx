8381b1d00c7626ae709c7848033bcbd6cd7cd9b0
Tanner Linsley <tannerlinsley@gmail.com>
2021-11-22 13:28:32 -0700

    Update index.tsx

--- examples/kitchen-sink/src/index.tsx
+++ examples/kitchen-sink/src/index.tsx
@@ -105,25 +105,22 @@ const routes: Route<LocationGenerics>[] = [
         element: <Invoices />,
         children: [
           { path: "/", element: <InvoicesHome /> },
-          // {
-          //   path: ":invoiceId",
-          //   element: <Invoice />,
-          //   loader: async ({ params: { invoiceId } }) => {
-          //     return {
-          //       invoice: await fetchInvoiceById(invoiceId),
-          //     };
-          //   },
-          //   onMatch: (match) => {
-          //     console.log(`Now rendering invoice ${match.params.invoiceId}`);
-          //     return () => {
-          //       console.log(
-          //         `No longer rendering invoice ${match.params.invoiceId}`
-          //       );
-          //     };
-          //   },
-          // },
           {
-            element: "Not Found!",
+            path: ":invoiceId",
+            element: <Invoice />,
+            loader: async ({ params: { invoiceId } }) => {
+              return {
+                invoice: await fetchInvoiceById(invoiceId),
+              };
+            },
+            onMatch: (match) => {
+              console.log(`Now rendering invoice ${match.params.invoiceId}`);
+              return () => {
+                console.log(
+                  `No longer rendering invoice ${match.params.invoiceId}`
+                );
+              };
+            },
           },
         ],
       },
