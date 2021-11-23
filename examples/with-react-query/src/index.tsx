efa13c3916a657314c67a75fdda077dbb24becd9
larsvankleef <larsvankleef@outlook.com>
2021-11-23 16:14:20 +0100

    Fixed broken Link style caused by an incorrect cache key

--- examples/with-react-query/src/index.tsx
+++ examples/with-react-query/src/index.tsx
@@ -122,7 +122,7 @@ function Posts() {
                       style={
                         // We can access the query data here to show bold links for
                         // ones that are cached
-                        queryClient.getQueryData(["post", post.id])
+                        queryClient.getQueryData(["posts", `${post.id}`])
                           ? {
                               fontWeight: "bold",
                               color: "green",
