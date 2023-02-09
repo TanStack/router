6c085415076816bfdffefd88a49fbc008cb2a6fd
Tanner Scadden <44242867+Tanner-Scadden@users.noreply.github.com>
2023-02-09 17:47:05 -0500

    fix: Added in a shallow check for missing keys to be removed from the store to make useSearch more compatible (#522)

--- packages/solid-store/src/index.tsx
+++ packages/solid-store/src/index.tsx
@@ -11,17 +11,48 @@ export function useStore<
 >(
   routerStore: Store<TState, TUpdater>,
   selector: (state: NoInfer<TState>) => TSelected = (d) => d as any,
+  options: { removeMissingKeys?: boolean } = {},
 ): TSelected {
   const [state, setState] = createStore<{ state: TSelected }>({
     state: selector(routerStore.state),
   })
 
   onMount(() => {
-    const unsubscribe = routerStore.subscribe((tState) =>
-      setState('state', reconcile(selector(tState))),
-    )
+    const unsubscribe = routerStore.subscribe((tState) => {
+      const updatedState = selector(tState)
+
+      setState('state', (s) => {
+        /* This is needed for removing keys that have been removed. Example is useSearch
+         when the params are removed. Need to make it more than just shallow, but 
+         should be okay for now. */
+        if (options.removeMissingKeys) {
+          const missingKeys = shallowFindMissingKeys(s, updatedState)
+          return reconcile({ ...updatedState, ...missingKeys })(s)
+        }
+        return reconcile(updatedState)(s)
+      })
+    })
     onCleanup(unsubscribe)
   })
 
   return state.state
+}
+
+function shallowFindMissingKeys<TState extends Object>(
+  oldState: TState,
+  newState: TState,
+): Record<keyof TState, undefined> {
+  const oldStateKeys = Object.keys(oldState) as (keyof typeof oldState)[]
+  const missingKeys: Record<keyof TState, undefined> = oldStateKeys.reduce(
+    (acc, key) => {
+      if (newState[key] === undefined) {
+        acc[key] = undefined
+      }
+
+      return acc
+    },
+    {} as Record<keyof TState, undefined>,
+  )
+
+  return missingKeys
 }
