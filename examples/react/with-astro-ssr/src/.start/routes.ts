acbcd35b17a114bead139278e5e3910a4f3ea9c1
Nikhil Saraf <nsaraf98@gmail.com>
2023-02-16 21:36:12 +0530

    fix router types

--- examples/react/with-astro-ssr/src/.start/routes.ts
+++ examples/react/with-astro-ssr/src/.start/routes.ts
@@ -3,8 +3,23 @@ import { indexRoute } from '../routes/index'
 import { postsRoute } from '../routes/posts'
 import { postsIndexRoute } from '../routes/posts/index'
 import { postIdRoute } from '../routes/posts/$postId'
+import { ReactRouter } from '@tanstack/react-router'
+import { loaderClient } from './loaders'
 
 export const routeTree = rootRoute.addChildren([
   indexRoute,
   postsRoute.addChildren([postsIndexRoute, postIdRoute]),
 ])
+
+export const router = new ReactRouter({
+  routeTree,
+  context: {
+    loaderClient,
+  },
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
