5a9c36f8a5dc12679618fa259eb28b4a9a57bcd2
Zack DeRose <zack.derose@gmail.com>
2023-04-22 16:47:13 -0700

    test passing

--- packages/router/vitest.config.ts
+++ packages/router/vitest.config.ts
@@ -1,3 +1,4 @@
+import { resolve } from 'path'
 import { defineConfig } from 'vitest/config'
 
 export default defineConfig({
@@ -6,5 +7,10 @@ export default defineConfig({
     watch: false,
     globals: true,
     environment: 'jsdom',
+  },
+  resolve: {
+    alias: {
+      '@tanstack/react-store': resolve(__dirname, '..', 'react-store', 'src'),
+    },
   },
 })
