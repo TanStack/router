c9a7364536f4eb3a335c87f937e9576066f94071
Tanner Linsley <tannerlinsley@gmail.com>
2023-07-06 16:39:16 -0600

    fix: build

--- packages/router/vitest.config.ts
+++ packages/router/vitest.config.ts
@@ -5,11 +5,18 @@ export default defineConfig({
   test: {
     name: 'router',
     watch: false,
+    // deps: {
+    //   interopDefault: true,
+    //   inline: true,
+    // },
   },
   resolve: {
     alias: {
-      '@tanstack/react-store': resolve(__dirname, '..', 'react-store', 'src'),
-      '@tanstack/store': resolve(__dirname, '..', 'store', 'src'),
+      '@tanstack/react-store': resolve(
+        __dirname,
+        '../react-store/build/esm/index.js',
+      ),
+      '@tanstack/store': resolve(__dirname, '../store/build/esm/index.js'),
     },
   },
 })
