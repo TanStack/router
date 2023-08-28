a83eff3ffcf5d0344d297cce5c1ad98971ebdfd4
Corbin Crutchley <git@crutchcorn.dev>
2023-08-27 19:34:08 -0700

    test(store): add batching tests

--- packages/store/__tests__/index.test.tsx
+++ packages/store/__tests__/index.test.tsx
@@ -52,4 +52,32 @@ describe('store', () => {
 
     expect(typeof store.state).toEqual("number")
   })
+
+  test("Batch prevents listeners from being called during repeated setStates", () => {
+    const store = new Store(0);
+
+    const listener = vi.fn();
+
+    store.subscribe(listener);
+
+    store.batch(() => {
+      store.setState(() => 1);
+      store.setState(() => 2);
+      store.setState(() => 3);
+      store.setState(() => 4);
+    })
+
+    expect(store.state).toEqual(4);
+    // Listener is only called once because of batching
+    expect(listener).toHaveBeenCalledTimes(1);
+
+    store.setState(() => 1);
+    store.setState(() => 2);
+    store.setState(() => 3);
+    store.setState(() => 4);
+
+    expect(store.state).toEqual(4);
+    // Listener is called 4 times because of a lack of batching
+    expect(listener).toHaveBeenCalledTimes(5);
+  })
 })
