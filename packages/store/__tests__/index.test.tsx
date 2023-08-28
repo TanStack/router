74162dd3280a13efbd834ec5d401b3bf97a914c6
Corbin Crutchley <git@crutchcorn.dev>
2023-08-27 19:11:46 -0700

    test(store): add initial functionality tests

--- packages/store/__tests__/index.test.tsx
+++ packages/store/__tests__/index.test.tsx
@@ -1,7 +1,31 @@
-import {describe, test, expect} from 'vitest'
+import {describe, test, expect, vi} from 'vitest'
+import {Store} from "../src/index";
 
 describe('store', () => {
-  test(`true is true`, () => {
-    expect(true).toEqual(true)
+  test(`should set the initial value`, () => {
+    const store = new Store(0);
+
+    expect(store.state).toEqual(0)
+  })
+
+  test(`basic subscriptions should work`, () => {
+    const store = new Store(0);
+
+    const subscription = vi.fn();
+
+    store.subscribe(subscription)
+
+    store.setState(() => 1);
+
+    expect(store.state).toEqual(1)
+    expect(subscription).toHaveBeenCalled()
+  })
+
+  test(`setState passes previous state`, () => {
+    const store = new Store(3);
+
+    store.setState((v) => v + 1);
+
+    expect(store.state).toEqual(4)
   })
 })
