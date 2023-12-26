cbeb4c23d4888d2171536d6c266af1ef6478455f
Niels Mentink <niels@ment.ink>
2023-12-26 19:12:08 +0100

    examples: Use RouteApi in kitchen-sink-file-based

--- examples/react/kitchen-sink-file-based/src/routes/expensive/index.tsx
+++ examples/react/kitchen-sink-file-based/src/routes/expensive/index.tsx
@@ -2,4 +2,5 @@ import { FileRoute, lazyRouteComponent } from '@tanstack/react-router'
 
 export const Route = new FileRoute('/expensive/').createRoute({
   component: lazyRouteComponent(() => import('./-components/Expensive')),
+  loader: () => ({ emoji: '😉' }),
 })
