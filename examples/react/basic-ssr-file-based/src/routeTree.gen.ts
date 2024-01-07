3ca0a9d1ce0b07bd4bb01ed2addddf76e02ac22c
Rui Saraiva <ruisaraiva19@gmail.com>
2024-01-07 22:03:57 +0000

    chore(deps): upgrade examples/react/basic-ssr-file-based dependencies

--- examples/react/basic-ssr-file-based/src/routeTree.gen.ts
+++ examples/react/basic-ssr-file-based/src/routeTree.gen.ts
@@ -1,45 +1,51 @@
+import { FileRoute, lazyFn, lazyRouteComponent } from "@tanstack/react-router"
+
 import { Route as rootRoute } from "./routes/__root"
-import { Route as PostsRoute } from "./routes/posts"
-import { Route as IndexRoute } from "./routes/index"
-import { Route as PostsPostIdRoute } from "./routes/posts/$postId"
-import { Route as PostsIndexRoute } from "./routes/posts/index"
+import { Route as PostsImport } from "./routes/posts"
+import { Route as IndexImport } from "./routes/index"
+import { Route as PostsPostIdImport } from "./routes/posts/$postId"
+import { Route as PostsIndexImport } from "./routes/posts/index"
+
+const PostsRoute = PostsImport.update({
+  path: "/posts",
+  getParentRoute: () => rootRoute,
+} as any)
+
+const IndexRoute = IndexImport.update({
+  path: "/",
+  getParentRoute: () => rootRoute,
+} as any)
+
+const PostsPostIdRoute = PostsPostIdImport.update({
+  path: "/$postId",
+  getParentRoute: () => PostsRoute,
+} as any)
+
+const PostsIndexRoute = PostsIndexImport.update({
+  path: "/",
+  getParentRoute: () => PostsRoute,
+} as any)
 
 declare module "@tanstack/react-router" {
   interface FileRoutesByPath {
     "/": {
+      preLoaderRoute: typeof IndexImport
       parentRoute: typeof rootRoute
     }
     "/posts": {
+      preLoaderRoute: typeof PostsImport
       parentRoute: typeof rootRoute
     }
     "/posts/": {
+      preLoaderRoute: typeof PostsIndexImport
       parentRoute: typeof PostsRoute
     }
     "/posts/$postId": {
+      preLoaderRoute: typeof PostsPostIdImport
       parentRoute: typeof PostsRoute
     }
   }
 }
-
-Object.assign(IndexRoute.options, {
-  path: "/",
-  getParentRoute: () => rootRoute,
-})
-
-Object.assign(PostsRoute.options, {
-  path: "/posts",
-  getParentRoute: () => rootRoute,
-})
-
-Object.assign(PostsIndexRoute.options, {
-  path: "/",
-  getParentRoute: () => PostsRoute,
-})
-
-Object.assign(PostsPostIdRoute.options, {
-  path: "/$postId",
-  getParentRoute: () => PostsRoute,
-})
 
 export const routeTree = rootRoute.addChildren([
   IndexRoute,
