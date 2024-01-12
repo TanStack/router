33e02b38ca1199f5e43dac4799dd4b932bc73186
Muhammad Amin Saffari Taheri <ma.saffaritaheri@gmail.com>
2024-01-13 02:43:58 +0330

    examples: add a vinxi client with server functions example.
    feat: add vinxi/server-function

---
+++ examples/react/vinxi-basic/app/app.tsx
@@ -0,0 +1,20 @@
+import React from 'react'
+
+import './style.css'
+import { Router, RouterProvider } from '@tanstack/react-router'
+import { routeTree } from './routeTree.gen'
+
+const router = new Router({
+  routeTree,
+  defaultPreload: 'intent',
+})
+
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+export default function App() {
+  return <RouterProvider router={router} />
+}
