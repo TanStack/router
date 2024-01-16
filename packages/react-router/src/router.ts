6d3357f5c798b31c4641ec0b929d60d3b47d1044
Nikola Hristov <nklhtv@gmail.com>
2024-01-16 12:23:19 -0800

    fix `pendingComponent` not shown when `pendingMs` is 0 (#1000)
    * fix `pendingComponent` not shown when `pendingMs` is 0
    
    * Handle nonpositive `pendingMs` values
    
    * fix type checking

--- packages/react-router/src/router.ts
+++ packages/react-router/src/router.ts
@@ -1060,9 +1060,9 @@ export class Router<
 
           const pendingMs =
             route.options.pendingMs ?? this.options.defaultPendingMs
-          const pendingPromise = new Promise<void>((r) =>
-            setTimeout(r, pendingMs),
-          )
+          const pendingPromise = typeof pendingMs === 'number' && pendingMs <= 0
+            ? Promise.resolve()
+            : new Promise<void>((r) => setTimeout(r, pendingMs))
 
           const beforeLoadContext =
             (await route.options.beforeLoad?.({
@@ -1145,7 +1145,7 @@ export class Router<
             route.options.pendingMinMs ?? this.options.defaultPendingMinMs
           const shouldPending =
             !preload &&
-            pendingMs &&
+            typeof pendingMs === 'number' &&
             (route.options.pendingComponent ??
               this.options.defaultPendingComponent)
 
