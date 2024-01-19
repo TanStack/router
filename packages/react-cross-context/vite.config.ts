3dc9044ddb83f7a2542792b13cae4e95b51c10b2
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-20 10:37:39 +1100

    Small vite/tsconfig tweaks

--- packages/react-cross-context/vite.config.ts
+++ packages/react-cross-context/vite.config.ts
@@ -1,7 +1,10 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 import { tanstackBuildConfig } from '@tanstack/config/build'
+import react from '@vitejs/plugin-react'
 
-const config = defineConfig({})
+const config = defineConfig({
+  plugins: [react()],
+})
 
 export default mergeConfig(
   config,
