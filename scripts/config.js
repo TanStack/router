37e495acaf90e53b372415f5c38b5c738a19a9eb
Lachlan Collins <1667261+lachlancollins@users.noreply.github.com>
2024-01-20 10:40:15 +1100

    Remove unnecessary config

--- scripts/config.js
+++ scripts/config.js
@@ -11,99 +11,34 @@ export const packages = [
   {
     name: '@tanstack/history',
     packageDir: 'packages/history',
-    builds: [
-      {
-        jsName: 'TanStackHistory',
-        entryFile: 'src/index.ts',
-        globals: {},
-      },
-    ],
   },
   {
     name: '@tanstack/react-router',
     packageDir: 'packages/react-router',
-    builds: [
-      {
-        jsName: 'ReactRouter',
-        entryFile: 'src/index.tsx',
-        globals: {
-          react: 'React',
-        },
-      },
-    ],
   },
   {
     name: '@tanstack/router-devtools',
     packageDir: 'packages/router-devtools',
-    builds: [
-      {
-        jsName: 'TanStackRouterDevtools',
-        entryFile: 'src/index.tsx',
-        globals: { react: 'React', '@tanstack/react-router': 'ReactRouter' },
-      },
-    ],
   },
   {
     name: '@tanstack/router-generator',
     packageDir: 'packages/router-generator',
-    builds: [
-      {
-        jsName: 'TanStackRouterGenerator',
-        entryFile: 'src/index.ts',
-        // esm: false,
-        // cjs: false,
-        umd: false,
-      },
-    ],
   },
   {
     name: '@tanstack/router-cli',
     packageDir: 'packages/router-cli',
-    builds: [
-      {
-        jsName: 'TanStackRouterCli',
-        entryFile: 'src/index.ts',
-        esm: false,
-        umd: false,
-      },
-    ],
   },
   {
     name: '@tanstack/router-vite-plugin',
     packageDir: 'packages/router-vite-plugin',
-    builds: [
-      {
-        jsName: 'TanStackRouterVitePlugin',
-        entryFile: 'src/index.ts',
-        // esm: false,
-        // cjs: false,
-        umd: false,
-      },
-    ],
   },
   {
     name: '@tanstack/react-cross-context',
     packageDir: 'packages/react-cross-context',
-    builds: [
-      {
-        jsName: 'ReactCrossContext',
-        entryFile: 'src/index.ts',
-      },
-    ],
   },
   {
     name: '@tanstack/react-router-server',
     packageDir: 'packages/react-router-server',
-    builds: [
-      {
-        jsName: 'TanStackRouterServer',
-        entryFile: 'src/server.tsx',
-      },
-      {
-        jsName: 'TanStackRouterServerClient',
-        entryFile: 'src/client.tsx',
-      },
-    ],
   },
 ]
 
