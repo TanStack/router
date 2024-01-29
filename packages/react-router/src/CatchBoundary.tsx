a46f2ec193066ddc3d28f28c9e628c087dd2302c
Tanner Linsley <tannerlinsley@gmail.com>
2024-01-29 09:54:10 -0700

    fix: Allow CatchBoundary without a getResetKey

--- packages/react-router/src/CatchBoundary.tsx
+++ packages/react-router/src/CatchBoundary.tsx
@@ -26,20 +26,20 @@ export function CatchBoundary(props: {
 }
 
 export class CatchBoundaryImpl extends React.Component<{
-  getResetKey: () => string
+  getResetKey?: () => string
   children: (props: { error: any; reset: () => void }) => any
   onCatch?: (error: any) => void
 }> {
   state = { error: null } as any
   static getDerivedStateFromProps(props: any) {
-    return { resetKey: props.getResetKey() }
+    return { resetKey: props.getResetKey?.() }
   }
   static getDerivedStateFromError(error: any) {
     return { error }
   }
   componentDidUpdate(
     prevProps: Readonly<{
-      getResetKey: () => string
+      getResetKey?: () => string
       children: (props: { error: any; reset: () => void }) => any
       onCatch?: ((error: any, info: any) => void) | undefined
     }>,
