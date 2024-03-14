5b64be47d3a51189cb4f71e2bfdce109ee33d925
Christopher Horobin <chrishorobin@hotmail.com>
2024-03-14 19:07:03 +0100

    examples: add example to generate large routes for typescript diagnostics (#1308)
    * chore: add large file based routing example to test peformance
    
    - ensure each Link points to a different route to make test realistic
    - support absolute and relative pathing for now
    
    * chore: remove posts
    
    * chore: update pnpm lock file
    
    * chore: rename to relative and absolute
    
    * chore: add content to README

---
+++ examples/react/large-file-based/src/routes/__root.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react'
+import { Link, Outlet, createRootRoute } from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+export const Route = createRootRoute({
+  component: RootComponent,
+  notFoundComponent: () => {
+    return <p>Not Found (on root route)</p>
+  },
+})
+
+function RootComponent() {
+  return (
+    <>
+      <div className="p-2 flex gap-2 text-lg">
+        <Link
+          to="/"
+          activeProps={{
+            className: 'font-bold',
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>
+      </div>
+      <hr />
+      <Outlet />
+      {/* Start rendering router matches */}
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  )
+}
