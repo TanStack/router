f5546673183eec539d099d49d45308313cb2421b
GoldFrosch <goldfrosch@daum.net>
2024-03-15 08:07:18 +0900

    example: move to basic-file-based

--- examples/react/basic-file-based/src/routes/index.tsx
+++ examples/react/basic-file-based/src/routes/index.tsx
@@ -1,6 +1,9 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
+import * as styles from './style.css'
+import { TEST_DATA } from './test.const'
+
 export const Route = createFileRoute('/')({
   component: Home,
 })
@@ -8,7 +11,7 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3 className={styles.indexPageTitle}>{TEST_DATA.welcome}</h3>
     </div>
   )
 }
--- examples/react/basic-with-file-based-library/src/routes/test.const.ts
+++
@@ -1,3 +0,0 @@
-export const TEST_DATA = {
-  it: 'just for Test Bro!',
-}
