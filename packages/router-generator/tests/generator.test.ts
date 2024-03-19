f84848f5a131f1eb6a3ff9c17ff4756e6c332d60
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-03-20 01:09:58 +1300

    test(router-generator): add tests to check the generated output of the `router-generator` (#1337)
    * chore: ignore the `*.gen.ts` outputs from the `tests` folder in `packges/router-generator`
    
    * chore: add the test script
    
    * test: add tests for 'single-level' routing
    
    * test: add tests for 'flat' routing
    
    * test: amend the 'flat' routing test to include a `route`-level config
    
    * test: add tests for 'nested' routing
    
    * test: match the dir structure between the 'flat' and 'nested' test cases
    
    * test: add tests for complex 'nested-layouts' routing
    
    * chore: disable logging from the `router-generator` package when testing
    
    * chore: space that wasn't supposed to be there
    
    * test: use `routeTree.expected.ts` for storing the expected outputs
    
    * chore: remove comment
    
    * test: run all the generator tests against all its subfolders

---
+++ packages/router-generator/tests/generator.test.ts
@@ -0,0 +1,62 @@
+import fs from 'fs/promises'
+import { describe, it, expect } from 'vitest'
+
+import { generator, getConfig, type Config } from '../src'
+
+function makeFolderDir(folder: string) {
+  return process.cwd() + `/tests/generator/${folder}`
+}
+
+async function getFoldersNames() {
+  const folders = await fs.readdir(process.cwd() + '/tests/generator')
+  return folders
+}
+
+async function setupConfig(
+  folder: string,
+  inlineConfig: Partial<Omit<Config, 'routesDirectory'>> = {},
+) {
+  const { generatedRouteTree = '/routeTree.gen.ts', ...rest } = inlineConfig
+  const dir = makeFolderDir(folder)
+
+  const config = await getConfig({
+    disableLogging: true,
+    routesDirectory: dir + '/routes',
+    generatedRouteTree: dir + generatedRouteTree,
+    ...rest,
+  })
+  return config
+}
+
+async function getRouteTreeFileText(config: Config) {
+  const location = config.generatedRouteTree
+  const text = await fs.readFile(location, 'utf-8')
+  return text
+}
+
+async function getExpectedRouteTreeFileText(folder: string) {
+  const dir = makeFolderDir(folder)
+  const location = dir + '/routeTree.expected.ts'
+  const text = await fs.readFile(location, 'utf-8')
+  return text
+}
+
+describe('with a default config', async () => {
+  const folderNames = await getFoldersNames()
+
+  it.each(folderNames.map((folder) => [folder]))(
+    'should wire-up the routes for a "%s" tree',
+    async (folderName) => {
+      const config = await setupConfig(folderName)
+
+      await generator(config)
+
+      const [expectedRouteTree, generatedRouteTree] = await Promise.all([
+        getExpectedRouteTreeFileText(folderName),
+        getRouteTreeFileText(config),
+      ])
+
+      expect(generatedRouteTree).equal(expectedRouteTree)
+    },
+  )
+})
