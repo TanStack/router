0f32819769994918e0fa3089e15fe9133c04039f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-03-19 23:48:11 +1300

    test: run all the generator tests against all its subfolders

--- packages/router-generator/tests/generator.test.ts
+++ packages/router-generator/tests/generator.test.ts
@@ -3,16 +3,27 @@ import { describe, it, expect } from 'vitest'
 
 import { generator, getConfig, type Config } from '../src'
 
-function getFolderDir(folder: string) {
-  return process.cwd() + `/tests/${folder}`
+function makeFolderDir(folder: string) {
+  return process.cwd() + `/tests/generator/${folder}`
 }
 
-async function setupConfig(folder: string) {
-  const dir = getFolderDir(folder)
+async function getFoldersNames() {
+  const folders = await fs.readdir(process.cwd() + '/tests/generator')
+  return folders
+}
+
+async function setupConfig(
+  folder: string,
+  inlineConfig: Partial<Omit<Config, 'routesDirectory'>> = {},
+) {
+  const { generatedRouteTree = '/routeTree.gen.ts', ...rest } = inlineConfig
+  const dir = makeFolderDir(folder)
+
   const config = await getConfig({
     disableLogging: true,
     routesDirectory: dir + '/routes',
-    generatedRouteTree: dir + '/routeTree.gen.ts',
+    generatedRouteTree: dir + generatedRouteTree,
+    ...rest,
   })
   return config
 }
@@ -24,66 +35,28 @@ async function getRouteTreeFileText(config: Config) {
 }
 
 async function getExpectedRouteTreeFileText(folder: string) {
-  const dir = getFolderDir(folder)
+  const dir = makeFolderDir(folder)
   const location = dir + '/routeTree.expected.ts'
   const text = await fs.readFile(location, 'utf-8')
   return text
 }
 
-describe('with a default config', () => {
-  it('should wire-up the routes for a "single-level" tree', async () => {
-    const folderName = 'single-level'
-    const config = await setupConfig(folderName)
+describe('with a default config', async () => {
+  const folderNames = await getFoldersNames()
 
-    await generator(config)
+  it.each(folderNames.map((folder) => [folder]))(
+    'should wire-up the routes for a "%s" tree',
+    async (folderName) => {
+      const config = await setupConfig(folderName)
 
-    const [expectedRouteTree, generatedRouteTree] = await Promise.all([
-      getExpectedRouteTreeFileText(folderName),
-      getRouteTreeFileText(config),
-    ])
+      await generator(config)
 
-    expect(generatedRouteTree).equal(expectedRouteTree)
-  })
+      const [expectedRouteTree, generatedRouteTree] = await Promise.all([
+        getExpectedRouteTreeFileText(folderName),
+        getRouteTreeFileText(config),
+      ])
 
-  it('should wire-up the routes for a "flat" tree', async () => {
-    const folderName = 'flat'
-    const config = await setupConfig(folderName)
-
-    await generator(config)
-
-    const [expectedRouteTree, generatedRouteTree] = await Promise.all([
-      getExpectedRouteTreeFileText(folderName),
-      getRouteTreeFileText(config),
-    ])
-
-    expect(generatedRouteTree).equal(expectedRouteTree)
-  })
-
-  it('should wire-up the routes for a "nested" tree', async () => {
-    const folderName = 'nested'
-    const config = await setupConfig('nested')
-
-    await generator(config)
-
-    const [expectedRouteTree, generatedRouteTree] = await Promise.all([
-      getExpectedRouteTreeFileText(folderName),
-      getRouteTreeFileText(config),
-    ])
-
-    expect(generatedRouteTree).equal(expectedRouteTree)
-  })
-
-  it('should wire-up the routes for a "nested-layouts" tree', async () => {
-    const folderName = 'nested-layouts'
-    const config = await setupConfig(folderName)
-
-    await generator(config)
-
-    const [expectedRouteTree, generatedRouteTree] = await Promise.all([
-      getExpectedRouteTreeFileText(folderName),
-      getRouteTreeFileText(config),
-    ])
-
-    expect(generatedRouteTree).equal(expectedRouteTree)
-  })
+      expect(generatedRouteTree).equal(expectedRouteTree)
+    },
+  )
 })
