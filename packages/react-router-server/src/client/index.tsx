84c1fa3b39efb7d132eaf45d7438c9ae085baa12
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-03-27 15:08:56 +1300

    refactor(react-router-server): createServerFn

--- packages/react-router-server/src/client/index.tsx
+++ packages/react-router-server/src/client/index.tsx
@@ -1,5 +1,17 @@
 export { Asset } from './Asset'
-export * from './createServerFn'
+export {
+  serverFnPayloadTypeHeader,
+  serverFnReturnTypeHeader,
+  createServerFn,
+  type JsonResponse,
+  type FetcherOptionsBase,
+  type FetcherOptions,
+  type FetchFn,
+  type CompiledFetcherFnOptions,
+  type CompiledFetcherFn,
+  type Fetcher,
+  type JsonResponseOrPayload,
+} from './createServerFn'
 export * from './DehydrateRouter'
 export * from './getManifestAssets'
 export * from './json'
