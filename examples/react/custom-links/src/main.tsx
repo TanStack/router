2562469f0fdc5b31a8b9a69544fb4f45bbbd435a
Jason Galea <jason@lecstor.com>
2024-03-29 00:11:13 +1000

    add custom links example

---
+++ examples/react/custom-links/src/main.tsx
@@ -0,0 +1,191 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import {
+  ErrorComponent,
+  type ErrorComponentProps,
+  Link,
+  NotFoundRoute,
+  Outlet,
+  RouterProvider,
+  createLink,
+  createRootRoute,
+  createRoute,
+  createRouter,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+import axios from 'axios'
+
+type PostType = {
+  id: string
+  title: string
+  body: string
+}
+
+type CustomLinkProps = React.ComponentProps<'a'>
+
+function CustomLinkBase({ children, ...props }: CustomLinkProps) {
+  console.log({ props })
+  return (
+    <a style={{ color: 'purple' }} className="text-5xl" {...props}>
+      {children}
+    </a>
+  )
+}
+
+export const CustomLink = createLink(CustomLinkBase)
+
+const fetchPosts = async () => {
+  console.log('Fetching posts...')
+  await new Promise((r) => setTimeout(r, 300))
+  return (
+    axios
+      /* eslint-disable-next-line @typescript-eslint/array-type */
+      .get<PostType[]>('https://jsonplaceholder.typicode.com/posts')
+      .then((r) => r.data.slice(0, 10))
+  )
+}
+
+const fetchPost = async (postId: string) => {
+  console.log(`Fetching post with id ${postId}...`)
+  await new Promise((r) => setTimeout(r, 300))
+  const post = await axios
+    .get<PostType>(`https://jsonplaceholder.typicode.com/posts/${postId}`)
+    .catch((err) => {
+      if (err.response.status === 404) {
+        throw new NotFoundError(`Post with id "${postId}" not found!`)
+      }
+      throw err
+    })
+    .then((r) => r.data)
+
+  return post
+}
+
+const rootRoute = createRootRoute({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <div className="bg-gradient-to-r from-green-700 to-lime-600 text-white">
+      <div className="p-2 flex gap-2 text-lg bg-black/40 shadow-xl">
+        <Link
+          to="/"
+          activeProps={{
+            className: 'font-bold',
+          }}
+          activeOptions={{ exact: true }}
+        >
+          Home
+        </Link>{' '}
+        <CustomLink to={'/posts'}>
+          {({ isActive }) => (
+            <span className={isActive ? 'font-bold' : ''}>Posts</span>
+          )}
+        </CustomLink>
+      </div>
+      <Outlet />
+      <TanStackRouterDevtools position="bottom-right" />
+    </div>
+  )
+}
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+  component: IndexComponent,
+})
+
+function IndexComponent() {
+  return (
+    <div className="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
+
+export const postsRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'posts',
+  loader: () => fetchPosts(),
+}).lazy(() => import('./posts.lazy').then((d) => d.Route))
+
+const postsIndexRoute = createRoute({
+  getParentRoute: () => postsRoute,
+  path: '/',
+  component: PostsIndexComponent,
+})
+
+function PostsIndexComponent() {
+  return <div>Select a post.</div>
+}
+
+class NotFoundError extends Error {}
+
+const postRoute = createRoute({
+  getParentRoute: () => postsRoute,
+  path: '$postId',
+  errorComponent: PostErrorComponent,
+  loader: ({ params }) => fetchPost(params.postId),
+  component: PostComponent,
+})
+
+function PostErrorComponent({ error }: ErrorComponentProps) {
+  if (error instanceof NotFoundError) {
+    return <div>{error.message}</div>
+  }
+
+  return <ErrorComponent error={error} />
+}
+
+function PostComponent() {
+  const post = postRoute.useLoaderData()
+
+  return (
+    <div className="space-y-2">
+      <h4 className="text-xl font-bold">{post.title}</h4>
+      <hr className="opacity-20" />
+      <div className="text-sm">{post.body}</div>
+    </div>
+  )
+}
+
+const notFoundRoute = new NotFoundRoute({
+  getParentRoute: () => rootRoute,
+  component: NotFound,
+})
+
+function NotFound() {
+  return (
+    <div className="p-2">
+      <h3>404 - Not Found</h3>
+    </div>
+  )
+}
+
+const routeTree = rootRoute.addChildren([
+  postsRoute.addChildren([postRoute, postsIndexRoute]),
+  indexRoute,
+])
+
+// Set up a Router instance
+const router = createRouter({
+  routeTree,
+  notFoundRoute,
+  defaultPreload: 'intent',
+  defaultStaleTime: 5000,
+})
+
+// Register things for typesafety
+declare module '@tanstack/react-router' {
+  interface Register {
+    router: typeof router
+  }
+}
+
+const rootElement = document.getElementById('app')!
+
+if (!rootElement.innerHTML) {
+  const root = ReactDOM.createRoot(rootElement)
+
+  root.render(<RouterProvider router={router} />)
+}
