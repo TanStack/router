ee5bb089f2336dc0f411149360f996d04b615ef9
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-04-03 12:09:14 +1300

    fix(react-router): incorrectly splitting the key-values pairs from the querystring (#1405)
    * fix(react-router): incorrect splitting of key-value pairs during decoding
    
    the value in the key-value pair was prematurely being sliced off and as such the full value wasn't being extracted for each key
    
    * chore(react-router): add JSDoc comments
    
    * test(react-router): testing for the qss encode and decode functions
    
    * fix(react-router): a test output that I messed up
    
    * test(react-router): tests for the prefix field in the encode and decode qss functions

--- packages/react-router/src/qss.ts
+++ packages/react-router/src/qss.ts
@@ -2,6 +2,17 @@
 
 // qss has been slightly modified and inlined here for our use cases (and compression's sake). We've included it as a hard dependency for MIT license attribution.
 
+/**
+ * Encodes an object into a query string.
+ * @param obj - The object to encode into a query string.
+ * @param [pfx] - An optional prefix to add before the query string.
+ * @returns The encoded query string.
+ * @example
+ * ```
+ * // Example input: encode({ token: 'foo', key: 'value' })
+ * // Expected output: "token=foo&key=value"
+ * ```
+ */
 export function encode(obj, pfx?: string) {
   let k,
     i,
@@ -25,6 +36,14 @@ export function encode(obj, pfx?: string) {
   return (pfx || '') + str
 }
 
+/**
+ * Converts a string value to its appropriate type (string, number, boolean).
+ * @param mix - The string value to convert.
+ * @returns The converted value.
+ * @example
+ * // Example input: toValue("123")
+ * // Expected output: 123
+ */
 function toValue(mix) {
   if (!mix) return ''
   const str = decodeURIComponent(mix)
@@ -33,18 +52,33 @@ function toValue(mix) {
   return +str * 0 === 0 && +str + '' === str ? +str : str
 }
 
+/**
+ * Decodes a query string into an object.
+ * @param str - The query string to decode.
+ * @param [pfx] - An optional prefix to filter out from the query string.
+ * @returns The decoded key-value pairs in an object format.
+ * @example
+ * // Example input: decode("token=foo&key=value")
+ * // Expected output: { "token": "foo", "key": "value" }
+ */
 export function decode(str, pfx?: string) {
   let tmp, k
   const out = {},
     arr = (pfx ? str.substr(pfx.length) : str).split('&')
 
   while ((tmp = arr.shift())) {
-    tmp = tmp.split('=')
-    k = tmp.shift()
-    if (out[k] !== void 0) {
-      out[k] = [].concat(out[k], toValue(tmp.shift()))
+    const equalIndex = tmp.indexOf('=')
+    if (equalIndex !== -1) {
+      k = tmp.slice(0, equalIndex)
+      const value = tmp.slice(equalIndex + 1)
+      if (out[k] !== void 0) {
+        out[k] = [].concat(out[k], toValue(value))
+      } else {
+        out[k] = toValue(value)
+      }
     } else {
-      out[k] = toValue(tmp.shift())
+      k = tmp
+      out[k] = ''
     }
   }
 
