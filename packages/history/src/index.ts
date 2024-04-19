9677f785187e649eb412554f236902e16e5b2ec8
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-04-19 20:44:41 +1200

    fix(history): update the `state.key` when using memory history on navigation change via `.back(), .forward(), and .go()`

--- packages/history/src/index.ts
+++ packages/history/src/index.ts
@@ -360,12 +360,15 @@ export function createMemoryHistory(
       entries[index] = path
     },
     back: () => {
+      currentState.key = createRandomKey()
       index--
     },
     forward: () => {
+      currentState.key = createRandomKey()
       index = Math.min(index + 1, entries.length - 1)
     },
     go: (n) => {
+      currentState.key = createRandomKey()
       index = Math.min(Math.max(index + n, 0), entries.length - 1)
     },
     createHref: (path) => path,
