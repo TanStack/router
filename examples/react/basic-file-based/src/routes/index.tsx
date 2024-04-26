b7c0c51d6ad07ed772bb59ad40326fb53ee1893b
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-04-26 17:09:18 +1200

    docs(examples): cleanup the basic-file-based example (#1524)
    * chore: remove vanilla-extract
    
    * chore: cleanup vite config
    
    * chore: cleanup the example

--- examples/react/basic-file-based/src/routes/index.tsx
+++ examples/react/basic-file-based/src/routes/index.tsx
@@ -1,9 +1,6 @@
 import * as React from 'react'
 import { createFileRoute } from '@tanstack/react-router'
 
-import * as styles from './style.css'
-import { TEST_DATA } from './test.const'
-
 export const Route = createFileRoute('/')({
   component: Home,
 })
@@ -11,7 +8,7 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <div className="p-2">
-      <h3 className={styles.indexPageTitle}>{TEST_DATA.welcome}</h3>
+      <h3>Welcome Home!</h3>
     </div>
   )
 }
--- examples/react/basic-file-based/vite.config.js
+++ examples/react/basic-file-based/vite.config.js
@@ -1,18 +1,15 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-vite-plugin'
-import { vanillaExtractPlugin } from '@vanilla-extract/vite-plugin'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
-      routeFileIgnorePattern: '.((css|const).ts)',
       experimental: {
         enableCodeSplitting: true,
       },
     }),
     react(),
-    vanillaExtractPlugin(),
   ],
 })
