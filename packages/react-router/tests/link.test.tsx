b482faa3e40148dec71f76950644f152e586b463
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-01 20:37:37 +1200

    test(react-router): make sure this works as exptected

---
+++ packages/react-router/tests/link.test.tsx
@@ -0,0 +1,74 @@
+import React from 'react'
+import { describe, it, expect, afterEach } from 'vitest'
+import { render, cleanup } from '@testing-library/react'
+
+import {
+  createRouter,
+  createRootRoute,
+  createRoute,
+  createMemoryHistory,
+  RouterProvider,
+  createLink,
+  Link,
+} from '../src'
+
+afterEach(() => {
+  cleanup()
+})
+
+describe('Link', () => {
+  it('should NOT pass the disabled prop to the target element', async () => {
+    const rootRoute = createRootRoute()
+    const indexRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/',
+      component: () => (
+        <Link to="/" disabled>
+          Index
+        </Link>
+      ),
+    })
+    const router = createRouter({
+      routeTree: rootRoute.addChildren([indexRoute]),
+      history: createMemoryHistory({ initialEntries: ['/'] }),
+    })
+
+    await router.load()
+
+    const rendered = render(<RouterProvider router={router} />)
+    const customElement = rendered.queryByRole('link')
+    customElement?.hasAttribute('disabled')
+
+    expect(customElement!.hasAttribute('disabled')).toBe(false)
+  })
+})
+
+describe('createLink', () => {
+  it('should pass the disabled prop to the target element', async () => {
+    const CustomLink = createLink('button')
+
+    const rootRoute = createRootRoute()
+    const indexRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/',
+      component: () => (
+        <CustomLink to="/" disabled>
+          Index
+        </CustomLink>
+      ),
+    })
+    const router = createRouter({
+      routeTree: rootRoute.addChildren([indexRoute]),
+      history: createMemoryHistory({ initialEntries: ['/'] }),
+    })
+
+    await router.load()
+
+    const rendered = render(<RouterProvider router={router} />)
+    const customElement = rendered.queryByRole('link')
+    customElement?.hasAttribute('disabled')
+
+    expect(customElement!.hasAttribute('disabled')).toBe(true)
+    expect(customElement!.getAttribute('disabled')).toBe('')
+  })
+})
