93d5e5ff1817b75206cceeacb37b90a65e835926
Ciarán Curley <ciaran.curley.11.11@gmail.com>
2024-05-02 16:53:23 +0100

    test(react-router): more complex tests for multiple params

--- packages/react-router/tests/redirects.test.ts
+++ packages/react-router/tests/redirects.test.ts
@@ -22,25 +22,29 @@ function createTestRouter(initialHistory?: RouterHistory) {
     getParentRoute: () => postsRoute,
     path: '/$slug',
   })
-  // const projectRoute = createRoute({
-  //   getParentRoute: () => rootRoute,
-  //   path: '/project',
-  // })
-  // const projectFrameRoute = createRoute({
-  //   getParentRoute: () => projectRoute,
-  //   path: '/$framework',
-  // })
-  // const projectVersionRoute = createRoute({
-  //   getParentRoute: () => projectFrameRoute,
-  //   path: '/$version',
-  // })
+  const projectRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: '/p',
+  })
+  const projectIdRoute = createRoute({
+    getParentRoute: () => projectRoute,
+    path: '/$projectId',
+  })
+  const projectFrameRoute = createRoute({
+    getParentRoute: () => projectIdRoute,
+    path: '/$framework',
+  })
+  const projectVersionRoute = createRoute({
+    getParentRoute: () => projectFrameRoute,
+    path: '/$version',
+  })
 
   const routeTree = rootRoute.addChildren([
     indexRoute,
     postsRoute.addChildren([postIdRoute]),
-    // projectRoute.addChildren([
-    //   projectFrameRoute.addChildren([projectVersionRoute]),
-    // ]),
+    projectRoute.addChildren([
+      projectIdRoute.addChildren([projectFrameRoute.addChildren([projectVersionRoute])]),
+    ]),
   ])
   const router = createRouter({ routeTree, history })
 
@@ -51,29 +55,7 @@ function createTestRouter(initialHistory?: RouterHistory) {
 }
 
 describe('redirects for simple route with one param', () => {
-  it('router.navigate with `params` and `to`, should transition from "/posts/tanner" to "/posts/tkdodo"', async () => {
-    const { router, routes } = createTestRouter(
-      createMemoryHistory({ initialEntries: ['/posts/tanner'] }),
-    )
-
-    await router.load()
-
-    expect(router.state.location.pathname).toBe('/posts/tanner')
-
-    await router.navigate({
-      from: router.state.resolvedLocation.pathname,
-      to: routes.postIdRoute.to,
-      params: (prev: any) => {
-        console.log(prev)
-        return { ...prev, slug: 'tkdodo' }
-      },
-    })
-    await router.invalidate()
-
-    expect(router.state.location.pathname).toBe('/posts/tkdodo')
-  })
-
-  it('router.navigate with `params` and no `to`, should transition from "/posts/tanner" to "/posts/tkdodo"', async () => {
+  it('"/posts/tanner" to "/posts/tkdodo"', async () => {
     const { router } = createTestRouter(
       createMemoryHistory({ initialEntries: ['/posts/tanner'] }),
     )
@@ -83,15 +65,90 @@ describe('redirects for simple route with one param', () => {
     expect(router.state.resolvedLocation.pathname).toBe('/posts/tanner')
 
     await router.navigate({
-      from: router.state.location.pathname,
       params: (prev: any) => ({ ...prev, slug: 'tkdodo' }),
     })
     await router.invalidate()
 
     // This fails but should succeed
     expect(router.state.location.pathname).toBe('/posts/tkdodo')
-
-    // This succeeds but should fail
-    // expect(router.state.location.pathname).toBe('/posts/tanner')
   })
 })
+
+describe('redirects for complex route with two params', () => {
+  it('"/p/router/react/v1 to /p/router/react/v3"', async () => {
+    const { router, routes } = createTestRouter(
+      createMemoryHistory({ initialEntries: ['/p/router/react/v1'] }),
+    )
+
+    await router.load()
+
+    expect(router.state.location.pathname).toBe('/p/router/react/v1')
+
+    await router.navigate({
+      params: (prev: any) => {
+        return { ...prev, version: 'v3' }
+      },
+    })
+    await router.invalidate()
+
+    expect(router.state.location.pathname).toBe('/p/router/react/v3')
+  })
+
+  it('"/p/router/latest/react to /p/router/latest/vue"', async () => {
+    const { router, routes } = createTestRouter(
+      createMemoryHistory({ initialEntries: ['/p/router/latest/react'] }),
+    )
+
+    await router.load()
+
+    expect(router.state.location.pathname).toBe('/p/router/latest/react')
+
+    await router.navigate({
+      params: (prev: any) => {
+        return { ...prev, framework: 'vue' }
+      },
+    })
+    await router.invalidate()
+
+    expect(router.state.location.pathname).toBe('/p/router/latest/vue')
+  })
+
+  it('"/p/router/latest/react to /p/router/latest/react?framework=vue"', async () => {
+    const { router, routes } = createTestRouter(
+      createMemoryHistory({ initialEntries: ['/p/form/latest/angular'] }),
+    )
+
+    await router.load()
+
+    expect(router.state.location.pathname).toBe('/p/form/latest/angular')
+
+    await router.navigate({
+      params: (prev: any) => {
+        return { ...prev, projectId: 'query' }
+      },
+    });
+    await router.invalidate()
+
+    expect(router.state.location.pathname).toBe('/p/query/latest/angular')
+  })
+
+  it('"/p/table/latest/angular to /p/router/latest/react"', async () => {
+    const { router, routes } = createTestRouter(
+      createMemoryHistory({ initialEntries: ['/p/table/latest/angular'] }),
+    )
+
+    await router.load()
+
+    expect(router.state.location.pathname).toBe('/p/table/latest/angular')
+
+    await router.navigate({
+      params: (prev: any) => {
+        return { ...prev, projectId: 'router', framework: 'react' }
+      },
+    });
+    await router.invalidate()
+
+    expect(router.state.location.pathname).toBe('/p/router/latest/react')
+  })
+})
+
