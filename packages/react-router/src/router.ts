62bb21d381aa110edf9006f28c23d4c96ca5bda6
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-04 17:18:08 +1200

    refactor(react-router): undo the changes I first made

--- packages/react-router/src/router.ts
+++ packages/react-router/src/router.ts
@@ -430,10 +430,6 @@ export class Router<
         onUpdate: () => {
           this.__store.state = {
             ...this.state,
-            status:
-              this.state.isTransitioning || this.state.isLoading
-                ? 'pending'
-                : 'idle',
             cachedMatches: this.state.cachedMatches.filter(
               (d) => !['redirected'].includes(d.status),
             ),
