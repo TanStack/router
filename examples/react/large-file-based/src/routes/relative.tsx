b69e8f88b3f0179ca4faab8863e7a861d37be5ea
Manuel Schiller <manuel.schiller@caligano.de>
2024-05-05 16:39:05 +0200

    chore: eslint

--- examples/react/large-file-based/src/routes/relative.tsx
+++ examples/react/large-file-based/src/routes/relative.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/relative')({
   component: RelativeComponent,
--- packages/react-router/tests/utils.test.ts
+++ packages/react-router/tests/utils.test.ts
@@ -1,10 +1,10 @@
-import { describe, it, expect } from 'vitest'
+import { describe, expect, it } from 'vitest'
 import {
   exactPathTest,
   isPlainArray,
+  removeLayoutSegments,
   removeTrailingSlash,
   replaceEqualDeep,
-  removeLayoutSegments,
 } from '../src/utils'
 
 describe('replaceEqualDeep', () => {
