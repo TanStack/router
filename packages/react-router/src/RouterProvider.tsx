bce8e71652acdfa66594fedaab4bc5f058ccaf6e
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-09 00:50:21 -0600

    fix: eslint

--- packages/react-router/src/RouterProvider.tsx
+++ packages/react-router/src/RouterProvider.tsx
@@ -1,6 +1,7 @@
+// eslint-disable-next-line @typescript-eslint/consistent-type-imports
+import * as React from 'react'
 import { Matches } from './Matches'
 import { getRouterContext } from './routerContext'
-import type * as React from 'react'
 import type { NavigateOptions, ToOptions } from './link'
 import type { ParsedLocation } from './location'
 import type { AnyRoute } from './route'
