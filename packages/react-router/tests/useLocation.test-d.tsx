d5782e0ea3eb1834859d08dcf06b667de082b38e
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-10 21:43:57 +1200

    test: using a test router

--- packages/react-router/tests/useLocation.test-d.tsx
+++ packages/react-router/tests/useLocation.test-d.tsx
@@ -1,17 +1,48 @@
 import { expectTypeOf, test } from 'vitest'
-import { useLocation, type ParsedLocation } from '../src'
+import {
+  createRootRoute,
+  createRoute,
+  createRouter,
+  useLocation,
+  type ParsedLocation,
+  type RouterState,
+} from '../src'
+
+const rootRoute = createRootRoute()
+
+const indexRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: '/',
+})
+
+const invoicesRoute = createRoute({
+  getParentRoute: () => rootRoute,
+  path: 'invoices',
+})
+
+const routeTree = rootRoute.addChildren([invoicesRoute, indexRoute])
+
+const defaultRouter = createRouter({ routeTree })
+
+type DefaultRouter = typeof defaultRouter
 
 test('should have the types for a ParsedLocation in useLocation', () => {
-  const location = useLocation()
+  const location = useLocation<DefaultRouter>()
 
   expectTypeOf(location).toEqualTypeOf<ParsedLocation>()
-  expectTypeOf(location).toHaveProperty('pathname').toEqualTypeOf<string>()
+  expectTypeOf(location)
+    .toHaveProperty('pathname')
+    .toEqualTypeOf<ParsedLocation['pathname']>()
 })
 
 test('should have the type of string for selecting the pathname in useLocation', () => {
-  const pathname = useLocation({
+  const pathname = useLocation<
+    DefaultRouter,
+    RouterState<DefaultRouter['routeTree']>['location'],
+    RouterState<DefaultRouter['routeTree']>['location']['pathname']
+  >({
     select: (state) => state.pathname,
   })
 
-  expectTypeOf(pathname).toEqualTypeOf<string>()
+  expectTypeOf(pathname).toMatchTypeOf<ParsedLocation['pathname']>()
 })
