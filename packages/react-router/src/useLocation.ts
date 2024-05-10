26196efbd476a048228695a3878edd30e7e83423
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-10 20:30:43 +1200

    refactor: useLocation hook using useRouterState and the selector

--- packages/react-router/src/useLocation.ts
+++ packages/react-router/src/useLocation.ts
@@ -1,10 +1,15 @@
-import { useRouter } from "./useRouter";
-import type { ParsedLocation } from "./location";
-import type { FullSearchSchema } from "./routeInfo";
-import type { AnyRoute } from "./route";
+import { useRouterState } from './useRouterState'
+import type { AnyRouter, RegisteredRouter, RouterState } from './router'
 
-
-export function useLocation<TRouteTree extends AnyRoute = AnyRoute>(): ParsedLocation<FullSearchSchema<TRouteTree>> {
-  const router = useRouter();
-  return router.state.location;
+export function useLocation<
+  TRouter extends AnyRouter = RegisteredRouter,
+  TLocationState = RouterState<TRouter['routeTree']>['location'],
+  TSelected = TLocationState,
+>(opts?: { select?: (state: TLocationState) => TSelected }): TSelected {
+  return useRouterState({
+    select: (state) =>
+      opts?.select
+        ? opts.select(state.location as TLocationState)
+        : (state.location as TSelected),
+  })
 }
