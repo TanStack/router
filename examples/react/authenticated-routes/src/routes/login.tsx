1e2885deba8dfe4163f7900458e07d8610b48757
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-05-17 22:01:21 +1200

    chore: put a comment on the sleep used on login

--- examples/react/authenticated-routes/src/routes/login.tsx
+++ examples/react/authenticated-routes/src/routes/login.tsx
@@ -45,7 +45,11 @@ function LoginComponent() {
       await auth.login(username)
 
       await router.invalidate()
+
+      // This is just a hack being used to wait for the auth state to update
+      // in a real app, you'd want to use a more robust solution
       await sleep(1)
+
       await navigate({ to: search.redirect || fallback })
     } catch (error) {
       console.error('Error logging in: ', error)
