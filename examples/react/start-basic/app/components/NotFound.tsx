b1f6d348aee0dd3dc757abb27f857914d6b7e469
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-18 11:36:00 -0600

    fix: full-stack not found

--- examples/react/start-basic/app/components/NotFound.tsx
+++ examples/react/start-basic/app/components/NotFound.tsx
@@ -2,27 +2,24 @@ import { Link } from '@tanstack/react-router'
 
 export function NotFound({ children }: { children?: any }) {
   return (
-    <div className="h-[50vh] flex flex-col items-center justify-center gap-6">
-      <h1 className="text-4xl font-bold">404 Not Found</h1>
-      <p className="text-lg text-gray-600 dark:text-gray-400">
-        The page you are looking for does not exist.
+    <div className="space-y-2 p-2">
+      <div className="text-gray-600 dark:text-gray-400">
+        {children || <p>The page you are looking for does not exist.</p>}
+      </div>
+      <p className="flex items-center gap-2 flex-wrap">
+        <button
+          onClick={() => window.history.back()}
+          className="bg-emerald-500 text-white px-2 py-1 rounded uppercase font-black text-sm"
+        >
+          Go back
+        </button>
+        <Link
+          to="/"
+          className="bg-cyan-600 text-white px-2 py-1 rounded uppercase font-black text-sm"
+        >
+          Start Over
+        </Link>
       </p>
-      {children || (
-        <p className="flex items-center gap-2 flex-wrap">
-          <button
-            onClick={() => window.history.back()}
-            className="bg-emerald-500 text-white p-2 rounded uppercase font-black"
-          >
-            Go back
-          </button>
-          <Link
-            to="/"
-            className="bg-cyan-600 text-white p-2 rounded uppercase font-black"
-          >
-            Start Over
-          </Link>
-        </p>
-      )}
     </div>
   )
 }
