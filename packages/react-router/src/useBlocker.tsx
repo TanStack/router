14e9f75b57470384f81784aff835ee1a3e36ca30
Leonardo Montini <lion.48m@gmail.com>
2024-05-21 22:39:02 +0200

    feat: return async resolver from useBlocker hook

--- packages/react-router/src/useBlocker.tsx
+++ packages/react-router/src/useBlocker.tsx
@@ -3,16 +3,47 @@ import { useRouter } from './useRouter'
 import type { BlockerFn } from '@tanstack/history'
 import type { ReactNode } from './route'
 
-export function useBlocker(
-  blockerFn: BlockerFn,
-  condition: boolean | any = true,
-): void {
+type Resolver = {
+  allow: () => void
+  deny: () => void
+}
+
+export function useBlocker(blockerFn: BlockerFn, condition: boolean = true) {
   const { history } = useRouter()
 
-  React.useEffect(() => {
-    if (!condition) return
-    return history.block(blockerFn)
+  const [resolver, setResolver] = React.useState<Resolver>({
+    allow: () => {},
+    deny: () => {},
   })
+
+  const createPromise = () =>
+    new Promise<boolean>((resolve) => {
+      setResolver({
+        allow: () => resolve(true),
+        deny: () => resolve(false),
+      })
+    })
+
+  const [promise, setPromise] = React.useState(createPromise)
+
+  React.useEffect(() => {
+    const blockerFnComposed = async () => {
+      // Execute the blocker function
+      const canNavigateSync = blockerFn()
+
+      if (canNavigateSync) return true
+
+      const canNavigateAsync = await promise
+
+      if (!canNavigateAsync) setPromise(createPromise)
+
+      return canNavigateAsync
+    }
+
+    return !condition ? undefined : history.block(blockerFnComposed)
+  }, [blockerFn, condition, history, promise])
+
+  return resolver
 }
 
 export function Block({ blocker, condition, children }: PromptProps) {
