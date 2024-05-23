55adc136b932c9fa3cf14c466a158f0f988ddc1c
Tanner Linsley <tannerlinsley@gmail.com>
2024-05-23 17:22:52 -0600

    examples: test vercel link

--- examples/react/start-basic/app/routes/index.tsx
+++ examples/react/start-basic/app/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!</h3>
+      <h3>Welcome Home!!!</h3>
     </div>
   )
 }
