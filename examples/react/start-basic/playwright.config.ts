3479a09de58dc57ae955e75265054495ada0dd87
Aditya Pandey <aditya@autospace.co>
2024-05-27 16:06:17 -0600

    Add CI action

--- examples/react/start-basic/playwright.config.ts
+++ examples/react/start-basic/playwright.config.ts
@@ -24,7 +24,7 @@ export default defineConfig({
 
   use: {
     /* Base URL to use in actions like `await page.goto('/')`. */
-    baseURL: 'http://localhost:3000/',
+    baseURL: process.env.APP_BASE_URL ?? 'http://localhost:3000/',
   },
 
   projects: [
