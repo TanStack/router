f1a6d1ffe70674c47fdb8355e65816b2c4a9e2e3
lithdew <kenta@lithdew.net>
2024-06-06 06:06:10 +0800

    feat: wrap "use server" over callexprs/identifiers for createServerFn
    Wrap identifiers/call expressions that are passed as the 2nd parameter
    to createServerFn with a function expression whose body is prepended
    with the "use server" directive.
    
    This allows for functions that are passed to createServerFn to be
    wrapped with other functions that server as middleware.
    
    router-vite-plugin/tests/test-files/createServerFn.tsx has a Zod
    middleware function example which has the "use server" directive
    properly prepended.

--- packages/router-vite-plugin/tests/snapshots/createServerFn.tsx
+++ packages/router-vite-plugin/tests/snapshots/createServerFn.tsx
@@ -1,4 +1,5 @@
 import { createServerFn } from '@tanstack/start';
+import { z } from 'zod';
 export const withUseServer = createServerFn('GET', async function () {
   "use server";
 
@@ -13,9 +14,27 @@ export const withoutUseServer = createServerFn('GET', async () => {
   await new Promise(r => setTimeout(r, 500));
   return axios.get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts').then(r => r.data.slice(0, 10));
 });
-export const withVariable = createServerFn('GET', abstractedFunction);
+export const withVariable = createServerFn('GET', (...args: Parameters<Parameters<typeof createServerFn>[1]>) => {
+  "use server";
+
+  return abstractedFunction.apply(this, args);
+});
 async function abstractedFunction() {
   console.log('Fetching posts...');
   await new Promise(r => setTimeout(r, 500));
   return axios.get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts').then(r => r.data.slice(0, 10));
 }
+function zodValidator<TSchema extends z.ZodSchema, TResult>(schema: TSchema, fn: (input: z.output<TSchema>) => TResult) {
+  return async (input: unknown) => {
+    return fn(schema.parse(input));
+  };
+}
+export const withZodValidator = createServerFn('GET', (...args: Parameters<Parameters<typeof createServerFn>[1]>) => {
+  "use server";
+
+  return zodValidator(z.number(), input => {
+    return {
+      'you gave': input
+    };
+  }).apply(this, args);
+});
--- packages/router-vite-plugin/tests/snapshots/createServerFn@split.tsx
+++ packages/router-vite-plugin/tests/snapshots/createServerFn@split.tsx
@@ -1,4 +1,5 @@
 import { createServerFn } from '@tanstack/start'
+import { z } from 'zod'
 
 export const withUseServer = createServerFn('GET', async function () {
   console.log('Fetching posts...')
@@ -25,3 +26,19 @@ async function abstractedFunction() {
     .get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts')
     .then((r) => r.data.slice(0, 10))
 }
+
+function zodValidator<TSchema extends z.ZodSchema, TResult>(
+  schema: TSchema,
+  fn: (input: z.output<TSchema>) => TResult,
+) {
+  return async (input: unknown) => {
+    return fn(schema.parse(input))
+  }
+}
+
+export const withZodValidator = createServerFn(
+  'GET',
+  zodValidator(z.number(), (input) => {
+    return { 'you gave': input }
+  }),
+)
