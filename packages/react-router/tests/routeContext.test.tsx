f7d15d09a2a9b05bb39650d1e6f6b4e90a522426
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-09 02:18:47 +1200

    test(react-router): route context breaking for layout routes

--- packages/react-router/tests/routeContext.test.tsx
+++ packages/react-router/tests/routeContext.test.tsx
@@ -12,6 +12,7 @@ import { afterEach, describe, expect, test, vi } from 'vitest'
 
 import {
   Link,
+  Outlet,
   RouterProvider,
   createRootRoute,
   createRoute,
@@ -723,7 +724,7 @@ describe('useRouteContext in the component', () => {
     expect(content).toBeInTheDocument()
   })
 
-  // Check if context that is updated at the root, is the same in the index route
+  // Check if the context that is updated at the root, is the same in the index route
   test('modified route context, present in the index route', async () => {
     const rootRoute = createRootRoute({
       beforeLoad: async ({ context }) => {
@@ -752,7 +753,7 @@ describe('useRouteContext in the component', () => {
     expect(content).toBeInTheDocument()
   })
 
-  // Check if context the context is available after a redirect on first-load
+  // Check if the context that is available after a redirect on first-load
   test('on first-load, route context is present in the /about route after a redirect is thrown in the beforeLoad of the index route', async () => {
     const rootRoute = createRootRoute()
     const indexRoute = createRoute({
@@ -815,7 +816,7 @@ describe('useRouteContext in the component', () => {
     expect(content).toBeInTheDocument()
   })
 
-  // Check if context the context is available after a redirect on navigate
+  // Check if the context that is available after a redirect on navigate
   test('on navigate, route context is present in the /person route after a redirect is thrown in the beforeLoad of the /about route', async () => {
     const rootRoute = createRootRoute()
     const indexRoute = createRoute({
@@ -918,5 +919,227 @@ describe('useRouteContext in the component', () => {
 
     expect(router.state.location.href).toBe('/person')
     expect(window.location.pathname).toBe('/person')
+  })
+
+  // Check if context returned by a layout route, is the same its child route (index route) on first-load
+  test('_layout on first-load, route context in the index route is correctly inherited from the layout parent', async () => {
+    const rootRoute = createRootRoute()
+    const layoutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      id: '_layout',
+      beforeLoad: async ({ context }) => {
+        await sleep(WAIT_TIME)
+        return { ...context, layout: 'layout' }
+      },
+      component: () => <Outlet />,
+    })
+    const indexRoute = createRoute({
+      getParentRoute: () => layoutRoute,
+      path: '/',
+      component: () => {
+        const context = indexRoute.useRouteContext()
+        return <div>{JSON.stringify(context)}</div>
+      },
+    })
+    const routeTree = rootRoute.addChildren([
+      layoutRoute.addChildren([indexRoute]),
+    ])
+    const router = createRouter({ routeTree, context: { foo: 'bar' } })
+
+    render(<RouterProvider router={router} />)
+
+    const content = await screen.findByText(
+      JSON.stringify({ foo: 'bar', layout: 'layout' }),
+    )
+
+    expect(router.state.location.href).toBe('/')
+    expect(window.location.pathname).toBe('/')
+
+    expect(content).toBeInTheDocument()
+  })
+
+  // Check if context returned by a layout route, is the same its child route (about route) on navigate
+  test('_layout on navigate, route context in the /about route is correctly inherited from the layout parent', async () => {
+    const rootRoute = createRootRoute()
+    const indexRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/',
+      component: () => {
+        return (
+          <div>
+            <h1>Index page</h1>
+            <Link to="/about">link to about</Link>
+          </div>
+        )
+      },
+    })
+    const layoutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      id: '_layout',
+      beforeLoad: async ({ context }) => {
+        await sleep(WAIT_TIME)
+        return { ...context, layout: 'layout' }
+      },
+      component: () => <Outlet />,
+    })
+    const aboutRoute = createRoute({
+      getParentRoute: () => layoutRoute,
+      path: '/about',
+      component: () => {
+        const context = aboutRoute.useRouteContext()
+        return <div>{JSON.stringify(context)}</div>
+      },
+    })
+    const routeTree = rootRoute.addChildren([
+      layoutRoute.addChildren([aboutRoute]),
+      indexRoute,
+    ])
+    const router = createRouter({ routeTree, context: { foo: 'bar' } })
+
+    render(<RouterProvider router={router} />)
+
+    const linkToAbout = await screen.findByRole('link', {
+      name: 'link to about',
+    })
+    expect(linkToAbout).toBeInTheDocument()
+    await act(() => fireEvent.click(linkToAbout))
+
+    const content = await screen.findByText(
+      JSON.stringify({ foo: 'bar', layout: 'layout' }),
+    )
+
+    expect(router.state.location.href).toBe('/about')
+    expect(window.location.pathname).toBe('/about')
+
+    expect(content).toBeInTheDocument()
+  })
+
+  // Check if context returned by a layout route, is the same its child route (about route) on after a redirect on navigate
+  test('_layout on navigate, when a redirect is thrown in beforeLoad in /about, the /person route context is correctly inherited from the layout parent', async () => {
+    const rootRoute = createRootRoute()
+    const indexRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/',
+      component: () => {
+        return (
+          <div>
+            <h1>Index page</h1>
+            <Link to="/about">link to about</Link>
+          </div>
+        )
+      },
+    })
+    const aboutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/about',
+      beforeLoad: async () => {
+        await sleep(WAIT_TIME)
+        throw redirect({ to: '/person' })
+      },
+    })
+    const layoutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      id: '_layout',
+      beforeLoad: async ({ context }) => {
+        await sleep(WAIT_TIME)
+        return { ...context, layout: 'layout' }
+      },
+      component: () => <Outlet />,
+    })
+    const personRoute = createRoute({
+      getParentRoute: () => layoutRoute,
+      path: '/person',
+      component: () => {
+        const context = aboutRoute.useRouteContext()
+        return <div>{JSON.stringify(context)}</div>
+      },
+    })
+    const routeTree = rootRoute.addChildren([
+      layoutRoute.addChildren([personRoute]),
+      aboutRoute,
+      indexRoute,
+    ])
+    const router = createRouter({ routeTree, context: { foo: 'bar' } })
+
+    render(<RouterProvider router={router} />)
+
+    const linkToAbout = await screen.findByRole('link', {
+      name: 'link to about',
+    })
+    expect(linkToAbout).toBeInTheDocument()
+    await act(() => fireEvent.click(linkToAbout))
+
+    const content = await screen.findByText(
+      JSON.stringify({ foo: 'bar', layout: 'layout' }),
+    )
+
+    expect(router.state.location.href).toBe('/person')
+    expect(window.location.pathname).toBe('/person')
+
+    expect(content).toBeInTheDocument()
+  })
+
+  test('_layout on navigate, when a redirect is thrown in loader in /about, the /person route context is correctly inherited from the layout parent', async () => {
+    const rootRoute = createRootRoute()
+    const indexRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/',
+      component: () => {
+        return (
+          <div>
+            <h1>Index page</h1>
+            <Link to="/about">link to about</Link>
+          </div>
+        )
+      },
+    })
+    const aboutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      path: '/about',
+      loader: async () => {
+        await sleep(WAIT_TIME)
+        throw redirect({ to: '/person' })
+      },
+    })
+    const layoutRoute = createRoute({
+      getParentRoute: () => rootRoute,
+      id: '_layout',
+      beforeLoad: async ({ context }) => {
+        await sleep(WAIT_TIME)
+        return { ...context, layout: 'layout' }
+      },
+      component: () => <Outlet />,
+    })
+    const personRoute = createRoute({
+      getParentRoute: () => layoutRoute,
+      path: '/person',
+      component: () => {
+        const context = aboutRoute.useRouteContext()
+        return <div>{JSON.stringify(context)}</div>
+      },
+    })
+    const routeTree = rootRoute.addChildren([
+      layoutRoute.addChildren([personRoute]),
+      aboutRoute,
+      indexRoute,
+    ])
+    const router = createRouter({ routeTree, context: { foo: 'bar' } })
+
+    render(<RouterProvider router={router} />)
+
+    const linkToAbout = await screen.findByRole('link', {
+      name: 'link to about',
+    })
+    expect(linkToAbout).toBeInTheDocument()
+    await act(() => fireEvent.click(linkToAbout))
+
+    const content = await screen.findByText(
+      JSON.stringify({ foo: 'bar', layout: 'layout' }),
+    )
+
+    expect(router.state.location.href).toBe('/person')
+    expect(window.location.pathname).toBe('/person')
+
+    expect(content).toBeInTheDocument()
   })
 })
