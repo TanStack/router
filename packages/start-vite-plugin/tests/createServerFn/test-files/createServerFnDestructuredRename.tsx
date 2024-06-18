702ca0c8eee3e28a0638d1ed4c0d6d86f97cc37b
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-18 13:30:31 +1200

    test: test for createServerFn being renamed during the destructured import

---
+++ packages/start-vite-plugin/tests/createServerFn/test-files/createServerFnDestructuredRename.tsx
@@ -0,0 +1,44 @@
+import { createServerFn as serverFn } from '@tanstack/start'
+import { z } from 'zod'
+
+export const withUseServer = serverFn('GET', async function () {
+  console.log('Fetching posts...')
+  await new Promise((r) => setTimeout(r, 500))
+  return axios
+    .get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts')
+    .then((r) => r.data.slice(0, 10))
+})
+
+export const withoutUseServer = serverFn('GET', async () => {
+  console.log('Fetching posts...')
+  await new Promise((r) => setTimeout(r, 500))
+  return axios
+    .get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts')
+    .then((r) => r.data.slice(0, 10))
+})
+
+export const withVariable = serverFn('GET', abstractedFunction)
+
+async function abstractedFunction() {
+  console.log('Fetching posts...')
+  await new Promise((r) => setTimeout(r, 500))
+  return axios
+    .get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts')
+    .then((r) => r.data.slice(0, 10))
+}
+
+function zodValidator<TSchema extends z.ZodSchema, TResult>(
+  schema: TSchema,
+  fn: (input: z.output<TSchema>) => TResult,
+) {
+  return async (input: unknown) => {
+    return fn(schema.parse(input))
+  }
+}
+
+export const withZodValidator = serverFn(
+  'GET',
+  zodValidator(z.number(), (input) => {
+    return { 'you gave': input }
+  }),
+)
