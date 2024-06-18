7e8795460a4ea80f0e1af7cfcd01a0de60472470
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-19 10:10:18 +1200

    style: import order

--- packages/router-plugin/src/combined.ts
+++ packages/router-plugin/src/combined.ts
@@ -1,9 +1,9 @@
 import { createUnplugin } from 'unplugin'
-import type { UnpluginFactory } from 'unplugin'
 
 import { routerCodeSplitterUnpluginFactory } from './code-splitter'
 import { routerGeneratorUnpluginFactory } from './router-generator'
 import type { PluginOptions } from './config'
+import type { UnpluginFactory } from 'unplugin'
 
 export const routerUnpluginFactory: UnpluginFactory<PluginOptions> = () => {
   return [routerCodeSplitterUnpluginFactory, routerGeneratorUnpluginFactory]
