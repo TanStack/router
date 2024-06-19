767051adac0dd1fe2ab4429471bae51430722335
lithdew <kenta@lithdew.net>
2024-06-20 02:40:43 +0800

    feat(start): support streaming using renderToReadableStream
    react-dom/server does not export renderToPipeableStream for JavaScript
    runtimes such as Bun.

--- packages/start/src/server/defaultStreamHandler.tsx
+++ packages/start/src/server/defaultStreamHandler.tsx
@@ -1,7 +1,7 @@
 import { PassThrough } from 'stream'
 import * as React from 'react'
 import { isbot } from 'isbot'
-import { renderToPipeableStream } from 'react-dom/server'
+import ReactDOMServer from 'react-dom/server'
 import { StartServer } from './StartServer'
 import { type RequestHandler } from './createRequestHandler'
 import type { AnyRouter } from '@tanstack/react-router'
@@ -11,30 +11,54 @@ export const defaultStreamHandler: RequestHandler<AnyRouter> = async ({
   router,
   responseHeaders,
 }) => {
-  const passthrough = new PassThrough()
+  if (typeof ReactDOMServer.renderToReadableStream === 'function') {
+    const stream = await ReactDOMServer.renderToReadableStream(
+      <StartServer router={router} />,
+      {
+        signal: request.signal,
+      },
+    )
 
-  const pipeable = renderToPipeableStream(<StartServer router={router} />, {
-    ...(isbot(request.headers.get('User-Agent'))
-      ? {
-          onAllReady() {
-            pipeable.pipe(passthrough)
-          },
-        }
-      : {
-          onShellReady() {
-            pipeable.pipe(passthrough)
-          },
-        }),
-    onShellError(err) {
-      throw err
-    },
-  })
+    if (isbot(request.headers.get('User-Agent'))) {
+      await stream.allReady
+    }
 
-  return new Response(passthrough as any, {
-    status: router.state.statusCode,
-    statusText: `${router.state.statusCode}`.startsWith('5')
-      ? 'Internal Server Error'
-      : 'OK',
-    headers: responseHeaders,
-  })
+    return new Response(stream, {
+      status: router.state.statusCode,
+      headers: responseHeaders,
+    })
+  }
+
+  if (typeof ReactDOMServer.renderToPipeableStream === 'function') {
+    const passthrough = new PassThrough()
+
+    const pipeable = ReactDOMServer.renderToPipeableStream(
+      <StartServer router={router} />,
+      {
+        ...(isbot(request.headers.get('User-Agent'))
+          ? {
+              onAllReady() {
+                pipeable.pipe(passthrough)
+              },
+            }
+          : {
+              onShellReady() {
+                pipeable.pipe(passthrough)
+              },
+            }),
+        onShellError(err) {
+          throw err
+        },
+      },
+    )
+
+    return new Response(passthrough as any, {
+      status: router.state.statusCode,
+      headers: responseHeaders,
+    })
+  }
+
+  throw new Error(
+    'No renderToReadableStream or renderToPipeableStream found in react-dom/server. Ensure you are using a version of react-dom that supports streaming.',
+  )
 }
