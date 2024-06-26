93f2b75ed71cd7e82fb2f5995789c4c7d491a9f6
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-27 09:19:47 +1200

    style(router-generator): prettier

--- packages/router-generator/src/generator.ts
+++ packages/router-generator/src/generator.ts
@@ -351,7 +351,8 @@ export async function generator(config: Config) {
 
     const cleanedPathIsEmpty = (node.cleanedPath || '').length === 0
     const nonPathRoute = node.isRoute && node.isNonPath
-    node.isVirtualParentRequired = (node.isLayout || nonPathRoute) ? !cleanedPathIsEmpty : false
+    node.isVirtualParentRequired =
+      node.isLayout || nonPathRoute ? !cleanedPathIsEmpty : false
     if (!node.isVirtual && node.isVirtualParentRequired) {
       const parentRoutePath = removeLastSegmentFromPath(node.routePath) || '/'
       const parentVariableName = routePathToVariable(parentRoutePath)
