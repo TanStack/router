eb2eaa6b52a41b1e14cb8ad95059448bb0430112
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-06-29 12:56:17 +1200

    feat(router-plugin): initial implementation

--- packages/router-plugin/src/ast.ts
+++ packages/router-plugin/src/ast.ts
@@ -1,4 +1,5 @@
 import * as babel from '@babel/core'
+import '@babel/parser'
 // @ts-expect-error
 import _babelPluginJsx from '@babel/plugin-syntax-jsx'
 // @ts-expect-error
@@ -13,6 +14,32 @@ if (babelPluginJsx.default) {
 
 if (babelPluginTypeScript.default) {
   babelPluginTypeScript = babelPluginTypeScript.default
+}
+
+export type ParseAstOptions = {
+  code: string
+  filename: string
+  root: string
+}
+
+export function parseAst(opts: ParseAstOptions) {
+  const babelPlugins: Array<babel.PluginItem> = [
+    babelPluginJsx,
+    [
+      babelPluginTypeScript,
+      {
+        isTSX: true,
+      },
+    ],
+  ]
+
+  return babel.parse(opts.code, {
+    plugins: babelPlugins,
+    root: opts.root,
+    filename: opts.filename,
+    sourceMaps: true,
+    sourceType: 'module',
+  })
 }
 
 export type CompileAstFn = (compileOpts: {
