08c2f1ea0fabfef9ff2c37640b7467ff86583b90
Honza Hrubý <jan.hruby@mews.com>
2024-06-30 13:34:40 +0200

    basic webpack example

---
+++ examples/react/quickstart-webpack-file-based/webpack.config.js
@@ -0,0 +1,43 @@
+import path from 'path'
+import { fileURLToPath } from 'url'
+import HtmlWebpackPlugin from 'html-webpack-plugin'
+
+const __dirname = fileURLToPath(new URL('.', import.meta.url))
+
+/** @type import('webpack').Configuration */
+export default ({ WEBPACK_SERVE }) => ({
+  target: 'web',
+  mode: WEBPACK_SERVE ? 'development' : 'production',
+  entry: path.resolve(__dirname, './src/index.tsx'),
+  output: {
+    path: path.resolve(__dirname, './dist'),
+    filename: '[name].bundle.js',
+    publicPath: '/',
+  },
+  resolve: {
+    extensions: ['.ts', '.tsx', '.js', '.jsx'],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: path.resolve(__dirname, './public/index.html'),
+      filename: 'index.html',
+    }),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        exclude: /(node_modules)/,
+        use: { loader: 'swc-loader' },
+      },
+    ],
+  },
+  devServer: {
+    open: true,
+    hot: true,
+    historyApiFallback: {
+      rewrites: [{ from: /./, to: '/index.html' }],
+    },
+    static: ['public'],
+  },
+})
