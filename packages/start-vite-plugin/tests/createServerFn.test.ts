c3a2861612bf0b56fa09a8e8be7c71bb3e5e10ca
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-05 09:23:46 +1200

    test(start-vite-plugin): update the tests to use the new compiler function

--- packages/start-vite-plugin/tests/createServerFn.test.ts
+++ packages/start-vite-plugin/tests/createServerFn.test.ts
@@ -2,8 +2,7 @@ import { readFile, readdir } from 'node:fs/promises'
 import path from 'node:path'
 import { describe, expect, test } from 'vitest'
 
-import { compileAst } from '../src/ast'
-import { createServerFnCompiler } from '../src/compilers'
+import { compileCreateServerFnOutput } from '../src/compilers'
 
 async function getFilenames() {
   return await readdir(
@@ -23,15 +22,13 @@ describe('createServerFn compiles correctly', async () => {
     )
     const code = file.toString()
 
-    const result = await createServerFnCompiler({
+    const compiledResult = compileCreateServerFnOutput({
       code,
-      compile: compileAst({
-        root: './createServerFn/test-files',
-      }),
+      root: './createServerFn/test-files',
       filename,
     })
 
-    await expect(result.code).toMatchFileSnapshot(
+    await expect(compiledResult.code).toMatchFileSnapshot(
       `./createServerFn/snapshots/${filename}`,
     )
   })
