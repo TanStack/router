5c88d400e83c66489711a38d59dc982e896d51b1
Tom Ballinger <tom@convex.dev>
2024-07-05 15:48:08 -0700

    Add a Convex database trellaux example

---
+++ examples/react/start-convex-trellaux/app/components/Loader.tsx
@@ -0,0 +1,7 @@
+export function Loader() {
+  return (
+    <div className="flex h-full">
+      <div className="m-auto border-gray-300 h-20 max-h-full aspect-square animate-spin rounded-full border-8 border-t-slate-900" />
+    </div>
+  )
+}
