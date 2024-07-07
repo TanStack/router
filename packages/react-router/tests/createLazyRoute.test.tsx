4afa5f9c1f8e15d9ed2a40de4fb9e3d6ced4821c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-07 12:18:55 +1200

    test(react-router): add a test to "createLazyRoute" to check that the heavy component is rendered in the DOM using testing-library

--- packages/react-router/tests/createLazyRoute.test.tsx
+++ packages/react-router/tests/createLazyRoute.test.tsx
@@ -1,12 +1,21 @@
+import React, { act } from 'react'
 import { afterEach, describe, expect, it, vi } from 'vitest'
-import { cleanup } from '@testing-library/react'
+import '@testing-library/jest-dom/vitest'
+import { cleanup, configure, render, screen } from '@testing-library/react'
 import {
+  Link,
+  RouterProvider,
+  createBrowserHistory,
   createMemoryHistory,
   createRootRoute,
   createRoute,
   createRouter,
 } from '../src'
 import type { RouterHistory } from '../src'
+
+// TODO: Move this setup logic including the '@testing-library/jest-dom/vitest' into its own setup file
+// @ts-expect-error
+global.IS_REACT_ACT_ENVIRONMENT = true
 
 afterEach(() => {
   vi.resetAllMocks()
@@ -18,7 +27,16 @@ function createTestRouter(initialHistory?: RouterHistory) {
     initialHistory ?? createMemoryHistory({ initialEntries: ['/'] })
 
   const rootRoute = createRootRoute({})
-  const indexRoute = createRoute({ getParentRoute: () => rootRoute, path: '/' })
+  const indexRoute = createRoute({
+    getParentRoute: () => rootRoute,
+    path: '/',
+    component: () => (
+      <div>
+        <p>Index Route</p>
+        <Link to="/heavy">Link to heavy</Link>
+      </div>
+    ),
+  })
 
   const heavyRoute = createRoute({
     getParentRoute: () => rootRoute,
@@ -36,6 +54,8 @@ function createTestRouter(initialHistory?: RouterHistory) {
 }
 
 describe('preload: matched routes', { timeout: 20000 }, () => {
+  configure({ reactStrictMode: true })
+
   it('should wait for lazy options to be streamed in before ', async () => {
     const { router } = createTestRouter(
       createMemoryHistory({ initialEntries: ['/'] }),
@@ -53,6 +73,30 @@ describe('preload: matched routes', { timeout: 20000 }, () => {
 
     const lazyRoute = router.routesByPath['/heavy']
 
+    expect(lazyRoute.options.component).toBeDefined()
+  })
+
+  it('should render the heavy/lazy component', async () => {
+    const { router } = createTestRouter(createBrowserHistory())
+
+    await act(() => render(<RouterProvider router={router} />))
+
+    const linkToHeavy = await screen.findByText('Link to heavy')
+    expect(linkToHeavy).toBeInTheDocument()
+
+    expect(router.state.location.pathname).toBe('/')
+    expect(window.location.pathname).toBe('/')
+
+    // click the link to navigate to the heavy route
+    act(() => linkToHeavy.click())
+
+    const heavyElement = await screen.findByText('I am sooo heavy')
+    expect(heavyElement).toBeInTheDocument()
+
+    expect(router.state.location.pathname).toBe('/heavy')
+    expect(window.location.pathname).toBe('/heavy')
+
+    const lazyRoute = router.routesByPath['/heavy']
     expect(lazyRoute.options.component).toBeDefined()
   })
 })
