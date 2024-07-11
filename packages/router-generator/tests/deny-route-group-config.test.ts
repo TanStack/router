63aa48418074b3bcdea66a6897d87f651bad61ba
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-12 11:24:51 +1200

    fix(router-generator): throw an error when a route configuration is found for a grouping identifier (#1947)
    * style(router-generator): stop the tests from screaming about ts-errors and eslint-errors
    
    * test(router-generator): add tests to expect an error on failure
    
    * fix(router-generator): do not allow route-groups to have route-configurations
    
    * chore(router-generator): tsconfig to ignore the test snapshot content
    
    * chore(router-generator): remove unused import

---
+++ packages/router-generator/tests/deny-route-group-config.test.ts
@@ -0,0 +1,62 @@
+import { join } from 'node:path'
+import { describe, expect, it } from 'vitest'
+
+import { generator, getConfig } from '../src'
+import type { Config } from '../src'
+
+function makeFolderDir(folder: string) {
+  return join(process.cwd(), 'tests', 'deny-route-group-config', folder)
+}
+
+async function setupConfig(
+  folder: string,
+  inlineConfig: Partial<Omit<Config, 'routesDirectory'>> = {},
+) {
+  const { generatedRouteTree = '/routeTree.gen.ts', ...rest } = inlineConfig
+  const dir = makeFolderDir(folder)
+
+  const config = await getConfig({
+    disableLogging: true,
+    routesDirectory: dir,
+    generatedRouteTree: dir + generatedRouteTree,
+    ...rest,
+  })
+  return config
+}
+
+type TestCases = Array<{
+  folder: string
+  expectedError: string
+}>
+
+describe('deny-route-group-config throws', () => {
+  it.each([
+    {
+      folder: 'flat-flat',
+      expectedError:
+        'A route configuration for a route group was found at `(group).tsx`. This is not supported. Did you mean to use a layout/pathless route instead?',
+    },
+    {
+      folder: 'nested-flat',
+      expectedError:
+        'A route configuration for a route group was found at `(group).tsx`. This is not supported. Did you mean to use a layout/pathless route instead?',
+    },
+    {
+      folder: 'flat-nested',
+      expectedError:
+        'A route configuration for a route group was found at `nested/(group).tsx`. This is not supported. Did you mean to use a layout/pathless route instead?',
+    },
+    {
+      folder: 'nested-nested',
+      expectedError:
+        'A route configuration for a route group was found at `nested/(group).tsx`. This is not supported. Did you mean to use a layout/pathless route instead?',
+    },
+  ] satisfies TestCases)(
+    'should throw an error for the folder: $folder',
+    async ({ folder, expectedError }) => {
+      const config = await setupConfig(folder)
+
+      expect(() => generator(config)).rejects.toThrowError(expectedError)
+    },
+  )
+})
