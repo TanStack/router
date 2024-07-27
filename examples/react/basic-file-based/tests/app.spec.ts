c83e0d662e08d735a6bfa09a086c10bb6633923d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-07-28 00:07:58 +1200

    test(examples): e2e validation of layout routes

--- examples/react/basic-file-based/tests/app.spec.ts
+++ examples/react/basic-file-based/tests/app.spec.ts
@@ -10,6 +10,20 @@ test('Navigating to a post page', async ({ page }) => {
   await expect(page.getByRole('heading')).toContainText('sunt aut facere')
 })
 
+test('Navigating nested layouts', async ({ page }) => {
+  await page.goto('/')
+  await page.getByRole('link', { name: 'Layout', exact: true }).click()
+
+  await expect(page.locator('#app')).toContainText("I'm a layout")
+  await expect(page.locator('#app')).toContainText("I'm a nested layout")
+
+  await page.getByRole('link', { name: 'Layout A' }).click()
+  await expect(page.locator('#app')).toContainText("I'm layout A!")
+
+  await page.getByRole('link', { name: 'Layout B' }).click()
+  await expect(page.locator('#app')).toContainText("I'm layout B!")
+})
+
 test('Navigating to a not-found route', async ({ page }) => {
   await page.getByRole('link', { name: 'This Route Does Not Exist' }).click()
   await expect(page.getByRole('paragraph')).toContainText(
