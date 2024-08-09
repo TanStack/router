ca401e600ff5850fef9cef01aa82dca0c08b303f
Mikkel Thorbjørn Andersen <73832274+mta-trackunit@users.noreply.github.com>
2024-08-09 13:35:11 +0200

    fix(react-router): `ResolvedSuspense` to always use `React.Suspense` never the `SafeFragment` (#1822)
    * Added test to prove rerender test for hooks is not possible with tanstack router wtih the current state.
    
    * prettier
    
    * Changed implementation to prove it will work
    
    * Navigating from kitchensink app dashboard/invoices to dashboard/users would end up redirecting back to  dashboard/invoices if using this.state.resolvedLocation
    
    * revert and fix navigate by adding a "to" directly in the useEffect since without it tanstack router will navigate back
    
    * cleanup
    
    * ensure lock file was not changed
    
    * now using lock from main
    
    * revert change
    
    * revert fix for search
    
    * fixed formatting
    
    * ci: apply automated fixes
    
    * chore: do not use memoryHistory
    
    ---------
    
    Co-authored-by: Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/react-router/tests/link.test.tsx
+++ packages/react-router/tests/link.test.tsx
@@ -5,6 +5,7 @@ import {
   configure,
   fireEvent,
   render,
+  renderHook,
   screen,
   waitFor,
 } from '@testing-library/react'
@@ -24,6 +25,7 @@ import {
   useMatchRoute,
   useParams,
   useRouteContext,
+  useRouterState,
   useSearch,
 } from '../src'
 import { getIntersectionObserverMock } from './utils'
@@ -46,6 +48,72 @@ afterEach(() => {
 })
 
 describe('Link', () => {
+  test('when using renderHook it returns a hook with same content to prove rerender works', async () => {
+    /**
+     * This is the hook that will be testet.
+     *
+     * @returns custom state
+     */
+    const useLocationFromState = () => {
+      const { location } = useRouterState()
+
+      // could return anything just to prove it will work.
+      const memoLocation = React.useMemo(() => {
+        return {
+          href: location.href,
+          pathname: location.pathname,
+        }
+      }, [location.href, location.pathname])
+
+      return memoLocation
+    }
+
+    const IndexComponent = ({ children }: { children: React.ReactNode }) => {
+      return <h1 data-testid="testId">{children}</h1>
+    }
+    const RouterContainer = ({ children }: { children: React.ReactNode }) => {
+      const childrenRef = React.useRef(children)
+      const memoedRouteTree = React.useMemo(() => {
+        const rootRoute = createRootRoute()
+        const indexRoute = createRoute({
+          getParentRoute: () => rootRoute,
+          path: '/',
+          component: () => (
+            <IndexComponent>{childrenRef.current}</IndexComponent>
+          ),
+        })
+        return rootRoute.addChildren([indexRoute])
+      }, [])
+
+      const memoedRouter = React.useMemo(() => {
+        const router = createRouter({
+          routeTree: memoedRouteTree,
+        })
+
+        return router
+      }, [memoedRouteTree])
+      return <RouterProvider router={memoedRouter} />
+    }
+
+    const { result, rerender } = renderHook(
+      () => {
+        return useLocationFromState()
+      },
+      { wrapper: RouterContainer },
+    )
+    await waitFor(() => expect(screen.getByTestId('testId')).toBeVisible())
+    expect(result.current).toBeTruthy()
+
+    const original = result.current
+
+    rerender()
+
+    await waitFor(() => expect(screen.getByTestId('testId')).toBeVisible())
+    const updated = result.current
+
+    expect(original).toBe(updated)
+  })
+
   test('when a Link is disabled', async () => {
     const rootRoute = createRootRoute()
     const indexRoute = createRoute({
