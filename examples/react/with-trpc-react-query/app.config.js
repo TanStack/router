a1ad375bd623770e99118443d8538924938d1ffd
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-13 18:58:00 +1200

    docs: move the trpc examples to file-based routing (#2121)
    * switch the "with-trpc" example over to vinxi
    
    * move the "with-trpc" example over to file-based routing
    
    * switch the "with-trpc-react-query" over to file-based routing
    
    * cleanup on the "with-trpc" example
    
    * cleanup on the "with-trpc-react-query" example
    
    * remove the manifest generation block flag on the "with-trpc-react-query" example
    
    * remove the manifest generation block flag on the "with-trpc-react" example

---
+++ examples/react/with-trpc-react-query/app.config.js
@@ -0,0 +1,43 @@
+import { createApp } from 'vinxi'
+import reactRefresh from '@vitejs/plugin-react'
+import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
+
+export default createApp({
+  server: {
+    preset: 'vercel', // change to 'node' or 'bun' or anyof the supported presets for nitro (nitro.unjs.io)
+    experimental: {
+      asyncContext: true,
+    },
+  },
+  routers: [
+    {
+      type: 'static',
+      name: 'public',
+      dir: './public',
+    },
+    {
+      type: 'http',
+      name: 'trpc',
+      base: '/trpc',
+      handler: './trpc-server.handler.ts',
+      target: 'server',
+      plugins: () => [reactRefresh()],
+    },
+    {
+      type: 'spa',
+      name: 'client',
+      handler: './index.html',
+      target: 'browser',
+      plugins: () => [
+        TanStackRouterVite({
+          routesDirectory: './app/routes',
+          generatedRouteTree: './app/routeTree.gen.ts',
+          experimental: {
+            enableCodeSplitting: true,
+          },
+        }),
+        reactRefresh(),
+      ],
+    },
+  ],
+})
---
+++ examples/react/with-trpc/app/routes/__root.tsx
@@ -0,0 +1,76 @@
+import {
+  Link,
+  Outlet,
+  createRootRouteWithContext,
+  useRouterState,
+} from '@tanstack/react-router'
+import { TanStackRouterDevtools } from '@tanstack/router-devtools'
+
+import { Spinner } from './-components/spinner'
+import type { trpc } from '../trpc'
+
+export interface RouterAppContext {
+  trpc: typeof trpc
+}
+
+export const Route = createRootRouteWithContext<RouterAppContext>()({
+  component: RootComponent,
+})
+
+function RootComponent() {
+  const isFetching = useRouterState({ select: (s) => s.isLoading })
+
+  return (
+    <>
+      <div className={`min-h-screen flex flex-col`}>
+        <div className={`flex items-center border-b gap-2`}>
+          <h1 className={`text-3xl p-2`}>With tRPC</h1>
+          {/* Show a global spinner when the router is transitioning */}
+          <div
+            className={`text-3xl duration-300 delay-0 opacity-0 ${
+              isFetching ? ` duration-1000 opacity-40` : ''
+            }`}
+          >
+            <Spinner />
+          </div>
+        </div>
+        <div className={`flex-1 flex`}>
+          <div className={`divide-y w-56`}>
+            {(
+              [
+                ['/', 'Home'],
+                ['/dashboard', 'Dashboard'],
+              ] as const
+            ).map(([to, label]) => {
+              return (
+                <div key={to}>
+                  <Link
+                    to={to}
+                    activeOptions={
+                      {
+                        // If the route points to the root of it's parent,
+                        // make sure it's only active if it's exact
+                        // exact: to === '.',
+                      }
+                    }
+                    preload="intent"
+                    className={`block py-2 px-3 text-blue-700`}
+                    // Make "active" links bold
+                    activeProps={{ className: `font-bold` }}
+                  >
+                    {label}
+                  </Link>
+                </div>
+              )
+            })}
+          </div>
+          <div className={`flex-1 border-l border-gray-200`}>
+            {/* Render our first route match */}
+            <Outlet />
+          </div>
+        </div>
+      </div>
+      <TanStackRouterDevtools position="bottom-right" />
+    </>
+  )
+}
