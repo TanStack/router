798d47f52c0bc51c5305ddcbefe29a7a0555b438
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-08-24 02:35:06 +1200

    need to fix this up

--- packages/start/src/server-handler/api/index.ts
+++ packages/start/src/server-handler/api/index.ts
@@ -2,11 +2,23 @@
 import { eventHandler, toWebRequest } from 'vinxi/http'
 import type { HTTPEvent } from 'vinxi/http'
 
-// eslint-disable-next-line @typescript-eslint/require-await
 async function handleServerRequest(event: HTTPEvent) {
   const request = toWebRequest(event)
 
-  return new Response('Hello, world!')
+  const url = new URL(request.url, 'http://localhost:3000')
+
+  // need to figure out a way to resolve this both in dev and in production,
+  // keeping into mind that this hardcoded `app/routes` is not the final path
+  const filepath = `app/routes${url.pathname}`
+  const name = request.method.toUpperCase()
+
+  const requestHandler = (
+    await import.meta.env.MANIFEST[import.meta.env.ROUTER_NAME]!.chunks[
+      filepath
+    ]!.import()
+  )[name]
+
+  return requestHandler({ request })
 }
 
 export default eventHandler(handleServerRequest)
