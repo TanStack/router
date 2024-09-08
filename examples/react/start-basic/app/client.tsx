1857e582d52288d10419d540d1813a546bf8140d
artimath <ryanjhunter@gmail.com>
2024-09-08 11:01:51 -1000

    swap to routerProvider

--- examples/react/start-basic/app/client.tsx
+++ examples/react/start-basic/app/client.tsx
@@ -1,7 +1,11 @@
 import { hydrateRoot } from 'react-dom/client'
 import { StartClient } from '@tanstack/start'
+import { RouterProvider } from '@tanstack/react-router'
 import { createRouter } from './router'
 
 const router = createRouter()
 
-hydrateRoot(document.getElementById('root')!, <StartClient router={router} />)
+hydrateRoot(
+  document.getElementById('root')!,
+  <RouterProvider router={router} />,
+)
