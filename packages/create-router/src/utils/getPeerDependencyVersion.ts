73337ac4ff7bb1bc0f0aed5cd3e840e736cd0123
Manuel Schiller <manuel.schiller@caligano.de>
2024-09-10 00:53:22 +0200

    fix(create-router): delay loading of package.json to runtime

--- packages/create-router/src/utils/getPeerDependencyVersion.ts
+++ packages/create-router/src/utils/getPeerDependencyVersion.ts
@@ -1,5 +1,6 @@
-import packageJson from '../../package.json'
 import type { PeerDependency } from '../types'
+// do not import at build time since router peer dependencies will still be '*'
+const packageJson = require('../../package.json')
 
 export function getDependenciesWithVersion(deps: Array<PeerDependency>) {
   return deps.reduce(
