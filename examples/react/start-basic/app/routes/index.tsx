89ff13de9b73dd3687f35391e9b458987bffeab1
Manuel Schiller <manuel.schiller@caligano.de>
2024-09-13 00:32:11 +0200

    revert index

--- examples/react/start-basic/app/routes/index.tsx
+++ examples/react/start-basic/app/routes/index.tsx
@@ -7,7 +7,7 @@ export const Route = createFileRoute('/')({
 function Home() {
   return (
     <div className="p-2">
-      <h3>Welcome Home!!! 123 123123</h3>
+      <h3>Welcome Home!!!</h3>
     </div>
   )
 }
