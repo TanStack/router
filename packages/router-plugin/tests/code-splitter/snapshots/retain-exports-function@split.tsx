b59a7b49ec2db1fc79e10848e4f590d0746261a0
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-09-14 12:51:15 +1200

    test(router-plugin): check that function exports are also being retained

---
+++ packages/router-plugin/tests/code-splitter/snapshots/retain-exports-function@split.tsx
@@ -0,0 +1,1 @@
+console.warn("These exports from \"retain-exports-function.tsx\" are not being code-split and therefore increasing your bundle size: \n- Layout\n- loaderFn\nYou should remove the export statement from these exports or import them from another file that is not a route.");
