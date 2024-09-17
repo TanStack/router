3d66d7058c1e004354ee751a3005f6d5f660dce4
Christopher Horobin <chrishorobin@hotmail.com>
2024-09-17 08:34:17 +0200

    perf(react-router): apply language service performance to `getRouteApi` (#2354)

--- packages/react-router/tests/routeApi.test-d.tsx
+++ packages/react-router/tests/routeApi.test-d.tsx
@@ -1,6 +1,6 @@
 import { describe, expectTypeOf, test } from 'vitest'
 import { createRootRoute, createRoute, createRouter, getRouteApi } from '../src'
-import type { UseNavigateResult } from '../src'
+import type { MakeRouteMatch, UseNavigateResult } from '../src'
 
 const rootRoute = createRootRoute()
 
@@ -23,6 +23,9 @@ const invoiceRoute = createRoute({
   getParentRoute: () => invoicesRoute,
   path: '$invoiceId',
   validateSearch: () => ({ page: 0 }),
+  beforeLoad: () => ({ beforeLoadContext: 0 }),
+  loaderDeps: () => ({ dep: 0 }),
+  loader: () => ({ data: 0 }),
 })
 
 const routeTree = rootRoute.addChildren([
@@ -40,7 +43,7 @@ type ExtractDefaultFrom<T> =
   T extends UseNavigateResult<infer DefaultFrom> ? DefaultFrom : never
 
 describe('getRouteApi', () => {
-  const invoiceRouteApi = getRouteApi<'/invoices/$invoiceId', DefaultRouter>(
+  const invoiceRouteApi = getRouteApi<DefaultRouter, '/invoices/$invoiceId'>(
     '/invoices/$invoiceId',
   )
   describe('useNavigate', () => {
@@ -51,6 +54,36 @@ describe('getRouteApi', () => {
         ExtractDefaultFrom<typeof navigate>
       >().toEqualTypeOf<'/invoices/$invoiceId'>()
     })
+  })
+  test('useParams', () => {
+    expectTypeOf(invoiceRouteApi.useParams()).toEqualTypeOf<{
+      invoiceId: string
+    }>()
+  })
+  test('useContext', () => {
+    expectTypeOf(invoiceRouteApi.useRouteContext()).toEqualTypeOf<{
+      beforeLoadContext: number
+    }>()
+  })
+  test('useSearch', () => {
+    expectTypeOf(invoiceRouteApi.useSearch()).toEqualTypeOf<{
+      page: number
+    }>()
+  })
+  test('useLoaderData', () => {
+    expectTypeOf(invoiceRouteApi.useLoaderData()).toEqualTypeOf<{
+      data: number
+    }>()
+  })
+  test('useLoaderDeps', () => {
+    expectTypeOf(invoiceRouteApi.useLoaderDeps()).toEqualTypeOf<{
+      dep: number
+    }>()
+  })
+  test('useMatch', () => {
+    expectTypeOf(invoiceRouteApi.useMatch()).toEqualTypeOf<
+      MakeRouteMatch<typeof routeTree, '/invoices/$invoiceId'>
+    >()
   })
 })
 
