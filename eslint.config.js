99e015f0a914609c17da64cbf7acb5f599085bc8
Manuel Schiller <manuel.schiller@caligano.de>
2024-09-19 00:22:40 +0200

    do not (yet) use our own eslint plugin

--- eslint.config.js
+++ eslint.config.js
@@ -3,11 +3,8 @@
 // @ts-expect-error
 import { tanstackConfig } from '@tanstack/config/eslint'
 
-import pluginRouter from '@tanstack/eslint-plugin-router'
-
 export default [
   ...tanstackConfig,
-  ...pluginRouter.configs['flat/recommended'],
   {
     name: 'tanstack/temp',
     rules: {
