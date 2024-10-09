a82bfa1e11a2a7cca9579b31e54643ee1deb8a8c
Manuel Schiller <manuel.schiller@caligano.de>
2024-10-09 22:17:19 +0200

    feat(router-generator): allow virtual file routes without file names (#2500)

--- examples/react/basic-virtual-file-based/src/routes/a.tsx
+++ examples/react/basic-virtual-file-based/src/routes/a.tsx
@@ -1,6 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/_first/_second/layout-a')({
+export const Route = createFileRoute(
+  '/_first/_second-layout/route-without-file/layout-a',
+)({
   component: LayoutAComponent,
 })
 
