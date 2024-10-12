13beb773120f549f647232602147b4eb9f5d1c28
Manuel Schiller <manuel.schiller@caligano.de>
2024-10-12 21:50:10 +0200

    chore: add eslint rule for unused imports

--- eslint.config.js
+++ eslint.config.js
@@ -3,6 +3,8 @@
 // @ts-expect-error
 import { tanstackConfig } from '@tanstack/config/eslint'
 
+import unusedImports from 'eslint-plugin-unused-imports'
+
 export default [
   ...tanstackConfig,
   {
@@ -10,6 +12,24 @@ export default [
     rules: {
       '@typescript-eslint/no-unsafe-function-type': 'off',
       'no-shadow': 'off',
+    },
+  },
+  {
+    plugins: {
+      'unused-imports': unusedImports,
+    },
+    rules: {
+      '@typescript-eslint/no-unused-vars': 'off',
+      'unused-imports/no-unused-imports': 'error',
+      'unused-imports/no-unused-vars': [
+        'warn',
+        {
+          vars: 'all',
+          varsIgnorePattern: '^_',
+          args: 'after-used',
+          argsIgnorePattern: '^_',
+        },
+      ],
     },
   },
 ]
