df6f43c9d3fa2ff34be33748f6b46025919ebb6f
chorobin <chrishorobin@hotmail.com>
2024-10-21 00:58:34 +0200

    chore

---
+++ examples/react/search-validator-adapters/tests/valibot.test-d.tsx
@@ -0,0 +1,15 @@
+import { createRootRoute, createRoute, Link } from '@tanstack/react-router'
+import { expectTypeOf, test } from 'vitest'
+import * as v from 'valibot'
+import { router } from '../src/main'
+import { Route as ValibotRoute } from '../src/routes/users/valibot.index'
+
+test('infers correct input and output type for valibot', () => {
+  expectTypeOf(Link<typeof router, string, '/users/valibot'>)
+    .parameter(0)
+    .toHaveProperty('search')
+    .exclude<boolean | ((...args: any[]) => any)>()
+    .toEqualTypeOf<{ search?: string } | undefined>()
+
+  expectTypeOf(ValibotRoute.useSearch()).toEqualTypeOf<{ search: string }>()
+})
