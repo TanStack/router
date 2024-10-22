0714a156ce69fc5e5daeabf8ac2701417779db07
Logan Anderson <landerson2@upei.ca>
2024-10-21 23:22:17 -0400

    fix typo

--- examples/react/start-basic/app.config.ts
+++ examples/react/start-basic/app.config.ts
@@ -2,9 +2,6 @@ import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
-  tsr: {
-    appDirectory
-  }
   vite: {
     plugins: [
       tsConfigPaths({
