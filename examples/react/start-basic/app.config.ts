1e25dc61bbd02eba73a18fcdf64699b965cb91d0
Logan Anderson <landerson2@upei.ca>
2024-10-21 23:20:39 -0400

    refactor(config): update app directory handling in defineConfig function

--- examples/react/start-basic/app.config.ts
+++ examples/react/start-basic/app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  tsr: {
+    appDirectory
+  }
   vite: {
     plugins: [
       tsConfigPaths({
