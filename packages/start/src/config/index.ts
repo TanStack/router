33cb88d3a16aa2b26dedac23e423641f281eb80b
NekoChan <47508893+nekochan0122@users.noreply.github.com>
2024-11-08 08:36:20 -0800

    feat(start): configurable public router (#2715)

--- packages/start/src/config/index.ts
+++ packages/start/src/config/index.ts
@@ -204,6 +204,12 @@ const routersSchema = z.object({
       vite: viteSchema.optional(),
     })
     .optional(),
+  public: z
+    .object({
+      dir: z.string().optional(),
+      base: z.string().optional(),
+    })
+    .optional(),
 })
 
 const tsrConfig = configSchema.partial().extend({
@@ -290,6 +296,9 @@ export function defineConfig(inlineConfig: TanStackStartInputConfig = {}) {
 
   const apiEntryExists = existsSync(apiEntry)
 
+  const publicDir = opts.routers?.public?.dir || './public'
+  const publicBase = opts.routers?.public?.base || '/'
+
   return createApp({
     server: {
       ...serverOptions,
@@ -303,8 +312,8 @@ export function defineConfig(inlineConfig: TanStackStartInputConfig = {}) {
       {
         name: 'public',
         type: 'static',
-        dir: './public',
-        base: '/',
+        dir: publicDir,
+        base: publicBase,
       },
       withStartPlugins(
         opts,
