508eac2c30b67e78c42659ca39be39df0662e53d
Manuel Schiller <manuel.schiller@caligano.de>
2024-11-09 21:17:06 +0100

    remove `matches` from router state subscription as it is not used

--- packages/react-router/src/Transitioner.tsx
+++ packages/react-router/src/Transitioner.tsx
@@ -7,22 +7,14 @@ import { trimPathRight } from './path'
 export function Transitioner() {
   const router = useRouter()
   const mountLoadForRouter = React.useRef({ router, mounted: false })
-  // CHECK HERE
   const routerState = useRouterState({
     select: (s) =>
-      pick(s, [
-        'isLoading',
-        'location',
-        'resolvedLocation',
-        'isTransitioning',
-        'matches',
-      ]),
-    structuralSharing: true as any,
+      pick(s, ['isLoading', 'location', 'resolvedLocation', 'isTransitioning']),
+    structuralSharing: true,
   })
 
   const [isTransitioning, startReactTransition_] = React.useTransition()
   // Track pending state changes
-  // CHECK HERE
   const hasPendingMatches = useRouterState({
     select: (s) => s.matches.some((d) => d.status === 'pending'),
     structuralSharing: true,
