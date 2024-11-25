5c95b3b0a63483a152fcd7933e04cbbcf61319fe
chorobin <chrishorobin@hotmail.com>
2024-11-25 22:22:25 +0100

    fix(react-router): allow users to override props with their props

--- packages/react-router/src/link.tsx
+++ packages/react-router/src/link.tsx
@@ -30,6 +30,7 @@ import type {
 } from './routeInfo'
 import type { AnyRouter, RegisteredRouter } from './router'
 import type {
+  Assign,
   Constrain,
   Expand,
   MakeDifferenceOptional,
@@ -933,7 +934,7 @@ export type ActiveLinkOptions<
   ActiveLinkOptionProps<TComp>
 
 type ActiveLinkProps<TComp> = Partial<
-  LinkComponentReactProps<TComp> & {
+  UseLinkReactProps<TComp> & {
     [key: `data-${string}`]: unknown
   }
 >
@@ -971,11 +972,6 @@ export interface LinkPropsChildren {
       }) => React.ReactNode)
 }
 
-type LinkComponentReactProps<TComp> = Omit<
-  UseLinkReactProps<TComp>,
-  keyof CreateLinkProps
->
-
 export type LinkComponentProps<
   TComp = 'a',
   TRouter extends AnyRouter = RegisteredRouter,
@@ -983,8 +979,10 @@ export type LinkComponentProps<
   TTo extends string | undefined = '.',
   TMaskFrom extends string = TFrom,
   TMaskTo extends string = '.',
-> = LinkComponentReactProps<TComp> &
-  LinkProps<TComp, TRouter, TFrom, TTo, TMaskFrom, TMaskTo>
+> = Assign<
+  LinkProps<TComp, TRouter, TFrom, TTo, TMaskFrom, TMaskTo>,
+  UseLinkReactProps<TComp>
+>
 
 export type CreateLinkProps = LinkProps<
   any,
