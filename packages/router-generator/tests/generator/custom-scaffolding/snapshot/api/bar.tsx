2cf38ac6fe3e74784f9b8289b9fd3202a3011a75
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-11-27 21:24:42 +1300

    feat(start): change the variable export for API Routes (#2873)

--- packages/router-generator/tests/generator/custom-scaffolding/snapshot/api/bar.tsx
+++ packages/router-generator/tests/generator/custom-scaffolding/snapshot/api/bar.tsx
@@ -1,7 +1,7 @@
 import { json } from '@tanstack/start'
 import { createAPIFileRoute } from '@tanstack/start/api'
 
-export const Route = createAPIFileRoute('/api/bar')({
+export const APIRoute = createAPIFileRoute('/api/bar')({
   GET: ({ request, params }) => {
     return json({ message: 'Hello /api/test' })
   },
