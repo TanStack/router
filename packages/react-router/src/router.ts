b794045151675c8337540be258a1e2826a7a7dd7
Roberto De Lucia <roberto.delucia@sinnefo.eu>
2024-11-27 10:59:42 +0100

    Use consistent quote style

--- packages/react-router/src/router.ts
+++ packages/react-router/src/router.ts
@@ -859,7 +859,7 @@ export class Router<
       })
     }
 
-    if (typeof window !== "undefined" && 'CSS' in window && typeof window.CSS.supports === 'function') {
+    if (typeof window !== 'undefined' && 'CSS' in window && typeof window.CSS.supports === 'function') {
       this.isViewTransitionTypesSupported = window.CSS.supports('selector(:active-view-transition-type(a)')
     }
   }
