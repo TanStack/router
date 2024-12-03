07110fa88910d8e2fc09d159ee54dfe220d6bd0d
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-12-04 10:10:25 +1300

    retrofit all the start e2e sandboxes

--- e2e/start/basic/app/utils/users.tsx
+++ e2e/start/basic/app/utils/users.tsx
@@ -5,6 +5,6 @@ export type User = {
 }
 
 const PORT =
-  import.meta.env.VITE_SERVER_PORT || process.env.VITE_SERVER_PORT || 3002
+  import.meta.env.VITE_SERVER_PORT || process.env.VITE_SERVER_PORT || 3000
 
 export const DEPLOY_URL = `http://localhost:${PORT}`
