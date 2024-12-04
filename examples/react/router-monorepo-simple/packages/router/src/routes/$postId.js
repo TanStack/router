412f6640dcc386bb2aba4b82af02fddbf8fda46d
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-12-04 17:25:11 +0100

    chore: remove generated js from monorepo example (#2922)

--- examples/react/router-monorepo-simple/packages/router/src/routes/$postId.js
+++
@@ -1,5 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { fetchPost } from '../fetch/posts'
-export const Route = createFileRoute('/$postId')({
-  loader: ({ params }) => fetchPost(params.postId),
-})
