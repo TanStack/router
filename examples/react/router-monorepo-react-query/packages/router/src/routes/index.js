89f9b811c03c8086146d07a670a2a82e57a5c389
Nicolas Beaussart <nic.beaussart@gmail.com>
2024-12-04 14:36:14 +0100

    chore: remove generated js from monorepo example

--- examples/react/router-monorepo-react-query/packages/router/src/routes/index.js
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { postsQueryOptions } from '@router-mono-react-query/post-query'
-export const Route = createFileRoute('/')({
-  loader: ({ context: { queryClient } }) => {
-    return queryClient.ensureQueryData(postsQueryOptions)
-  },
-})
