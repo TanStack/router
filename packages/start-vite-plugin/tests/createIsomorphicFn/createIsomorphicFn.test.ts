72051515098dc7571b544da93c4156208b82879d
Ben Durrant <ben.j.durrant@gmail.com>
2024-12-07 04:00:02 +0000

    Add createIsomorphicFn compiler handling

--- packages/start-vite-plugin/tests/createIsomorphicFn/createIsomorphicFn.test.ts
+++ packages/start-vite-plugin/tests/createIsomorphicFn/createIsomorphicFn.test.ts
@@ -1,6 +1,6 @@
 import { readFile, readdir } from 'node:fs/promises'
 import path from 'node:path'
-import { describe, expect, test } from 'vitest'
+import { afterAll, describe, expect, test, vi } from 'vitest'
 
 import { compileStartOutput } from '../../src/compilers'
 
@@ -9,6 +9,22 @@ async function getFilenames() {
 }
 
 describe('createIsomorphicFn compiles correctly', async () => {
+  const noImplWarning =
+    'createIsomorphicFn called without a client or server implementation!'
+
+  const originalConsoleWarn = console.warn
+  const consoleSpy = vi.spyOn(console, 'warn').mockImplementation((...args) => {
+    // we want to avoid sending this warning to the console, we know about it
+    if (args[0] === noImplWarning) {
+      return
+    }
+    originalConsoleWarn(...args)
+  })
+
+  afterAll(() => {
+    consoleSpy.mockRestore()
+  })
+
   const filenames = await getFilenames()
 
   describe.each(filenames)('should handle "%s"', async (filename) => {
@@ -31,6 +47,56 @@ describe('createIsomorphicFn compiles correctly', async () => {
           `./snapshots/${env}/${filename}`,
         )
       },
+    )
+  })
+  test('should error if implementation not provided', () => {
+    expect(() => {
+      compileStartOutput({
+        env: 'client',
+        code: `
+        import { createIsomorphicFn } from '@tanstack/start'
+        const clientOnly = createIsomorphicFn().client()`,
+        root: './test-files',
+        filename: 'no-fn.ts',
+      })
+    }).toThrowError()
+    expect(() => {
+      compileStartOutput({
+        env: 'server',
+        code: `
+        import { createIsomorphicFn } from '@tanstack/start'
+        const serverOnly = createIsomorphicFn().server()`,
+        root: './test-files',
+        filename: 'no-fn.ts',
+      })
+    }).toThrowError()
+  })
+  test('should be assigned to a variable', () => {
+    expect(() => {
+      compileStartOutput({
+        env: 'client',
+        code: `
+        import { createIsomorphicFn } from '@tanstack/start'
+        createIsomorphicFn()`,
+        root: './test-files',
+        filename: 'no-fn.ts',
+      })
+    }).toThrowError()
+  })
+  test('should warn to console if no implementations provided', () => {
+    compileStartOutput({
+      env: 'client',
+      code: `
+      import { createIsomorphicFn } from '@tanstack/start'
+      const noImpl = createIsomorphicFn()`,
+      root: './test-files',
+      filename: 'no-fn.ts',
+    })
+    expect(consoleSpy).toHaveBeenCalledWith(
+      noImplWarning,
+      'This will result in a no-op function.',
+      'Variable name:',
+      'noImpl',
     )
   })
 })
