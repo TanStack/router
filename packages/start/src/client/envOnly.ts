2663b94a818ed19d02075f6e819b0e7d8f2a8165
Ben Durrant <ben.j.durrant@gmail.com>
2024-12-08 22:18:27 +0000

    chore(start): Avoid duplication for envOnly functions. (#2957)

--- packages/start/src/client/envOnly.ts
+++ packages/start/src/client/envOnly.ts
@@ -1,17 +1,9 @@
+type EnvOnlyFn = <TFn extends (...args: Array<any>) => any>(fn: TFn) => TFn
+
 // A function that will only be available in the server build
 // If called on the client, it will throw an error
-
-export function serverOnly<TFn extends (...args: Array<any>) => any>(
-  fn: TFn,
-): TFn {
-  return fn
-}
+export const serverOnly: EnvOnlyFn = (fn) => fn
 
 // A function that will only be available in the client build
 // If called on the server, it will throw an error
-
-export function clientOnly<TFn extends (...args: Array<any>) => any>(
-  fn: TFn,
-): TFn {
-  return fn
-}
+export const clientOnly: EnvOnlyFn = (fn) => fn
