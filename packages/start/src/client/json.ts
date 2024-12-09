586cc7dbdbe4e7f63c0fc56ad95d21fc498ff879
Ben Durrant <ben.durrant@marketdojo.com>
2024-12-09 18:30:34 +0000

    golf json even harder (#2961)

--- packages/start/src/client/json.ts
+++ packages/start/src/client/json.ts
@@ -1,16 +1,15 @@
+import { mergeHeaders } from './headers'
 import type { JsonResponse } from './createServerFn'
 
 export function json<TData>(
   payload: TData,
   init?: ResponseInit,
 ): JsonResponse<TData> {
-  const headers = new Headers(init?.headers)
-
-  if (!headers.has('Content-Type'))
-    headers.set('Content-Type', 'application/json')
-
   return new Response(JSON.stringify(payload), {
     ...init,
-    headers,
+    headers: mergeHeaders(
+      { 'content-type': 'application/json' },
+      init?.headers,
+    ),
   })
 }
