950b80fe666c2cb067e93164a902e0a98f4c5890
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2024-12-10 09:29:11 +1300

    test(e2e): add rsbuild based sandboxed to the end-to-end testing suite (#2965)
    * test(e2e): add rspack basic-file-based sandbox
    
    * test(e2e): add rspack basic-virtual-named-export-config-file-based sandbox
    
    * chore: checkin lockfile
    
    * chore(e2e): switch to referencing the workspace versions
    
    * chore(examples): upgrade rsbuild dependencies to latest
    
    * chore(e2e): remove playwright test results from git history
    
    * chore(root): fix lockfile
    
    * chore(e2e): log out the port being mapped

---
+++ e2e/react-router/rspack-basic-virtual-named-export-config-file-based/src/routes/a.tsx
@@ -0,0 +1,9 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_first/_second/layout-a')({
+  component: LayoutAComponent,
+})
+
+function LayoutAComponent() {
+  return <div>I'm layout A!</div>
+}
