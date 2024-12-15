ee14af9a38c62b3fea9aa955066dbdb0cb874f52
akhmadshin <qwasderfasder@gmail.com>
2024-12-16 01:14:04 +0700

    remove redundant e2e project

--- e2e/react-router/preload-file-based/src/routes/(group)/lazyinside.tsx
+++
@@ -1,7 +0,0 @@
-import { createFileRoute } from '@tanstack/react-router'
-import { z } from 'zod'
-import { zodValidator } from '@tanstack/zod-adapter'
-
-export const Route = createFileRoute('/(group)/lazyinside')({
-  validateSearch: zodValidator(z.object({ hello: z.string().optional() })),
-})
