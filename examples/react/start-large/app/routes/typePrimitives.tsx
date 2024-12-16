a33215a74e5b5f216610ad83f8f3823cd94e60ab
chorobin <chrishorobin@hotmail.com>
2024-12-16 23:07:08 +0100

    feat: add more type primitives

--- examples/react/start-large/app/routes/typePrimitives.tsx
+++ examples/react/start-large/app/routes/typePrimitives.tsx
@@ -1,11 +1,16 @@
-import { redirect } from '@tanstack/react-router'
+import { redirect, useParams, useSearch } from '@tanstack/react-router'
 import type {
   ValidateFrom,
   ValidateId,
   ValidateNavigateOptions,
+  ValidateNavigateOptionsArray,
   ValidateParams,
   ValidateSearch,
   ValidateTo,
+  ValidateUseParamsOptions,
+  ValidateUseParamsResult,
+  ValidateUseSearchOptions,
+  ValidateUseSearchResult,
 } from '@tanstack/react-router'
 
 export const customTo = <TTo extends string>(to: ValidateTo<TTo>) => {
@@ -28,21 +33,43 @@ export const customSearch = <TTo extends string>(
     to: ValidateTo<TTo>
   } & ValidateSearch<TTo>,
 ) => {
-  throw redirect(options as any)
+  throw redirect(options as never)
 }
 
 export const customParams = <TTo extends string>(
   options: { to: ValidateTo<TTo> } & ValidateParams<TTo>,
 ) => {
-  throw redirect(options as any)
+  throw redirect(options as never)
 }
 
-export const customNavigateOptions = <TTo extends string, TFrom extends string>(
-  options: ValidateNavigateOptions<TTo, TFrom>,
+export const customNavigateOptions = <TOptions,>(
+  options: ValidateNavigateOptions<TOptions>,
 ) => {
   throw redirect(options)
 }
 
+export const customNavigateOptionsArray = <
+  TOptions extends ReadonlyArray<unknown>,
+>(
+  ...options: ValidateNavigateOptionsArray<TOptions>
+) => {
+  for (const option of options) {
+    throw redirect(option)
+  }
+}
+
 export const useCustomId = <TId extends string>(id: ValidateId<TId>) => {
   console.log(id)
+}
+
+export const useCustomSearch = <TOptions,>(
+  options: ValidateUseSearchOptions<TOptions>,
+): ValidateUseSearchResult<TOptions> => {
+  return useSearch(options as never)
+}
+
+export const useCustomParams = <TOptions,>(
+  options: ValidateUseParamsOptions<TOptions>,
+): ValidateUseParamsResult<TOptions> => {
+  return useParams(options as never)
 }
--- packages/react-router/src/typePrimitives.ts
+++ packages/react-router/src/typePrimitives.ts
@@ -7,7 +7,9 @@ import type {
 } from './link'
 import type { RouteIds } from './routeInfo'
 import type { AnyRouter, RegisteredRouter } from './router'
-import type { ConstrainLiteral } from './utils'
+import type { UseParamsOptions, UseParamsResult } from './useParams'
+import type { UseSearchOptions, UseSearchResult } from './useSearch'
+import type { Constrain, ConstrainLiteral } from './utils'
 
 export type ValidateFrom<
   TFrom,
@@ -32,15 +34,135 @@ export type ValidateParams<
   TRouter extends AnyRouter = RegisteredRouter,
 > = PathParamOptions<TRouter, TFrom, TTo>
 
+/**
+ * @internal
+ */
+export type InferFrom<TOptions> = TOptions extends {
+  from: infer TFrom extends string
+}
+  ? TFrom
+  : string
+
+/**
+ * @internal
+ */
+export type InferTo<TOptions> = TOptions extends {
+  to: infer TTo extends string
+}
+  ? TTo
+  : undefined
+
+/**
+ * @internal
+ */
+export type InferMaskTo<TOptions> = TOptions extends {
+  mask: { to: infer TTo extends string }
+}
+  ? TTo
+  : string
+
+export type InferMaskFrom<TOptions> = TOptions extends {
+  mask: { from: infer TFrom extends string }
+}
+  ? TFrom
+  : string
+
 export type ValidateNavigateOptions<
-  TTo extends string | undefined = undefined,
-  TFrom extends string = string,
-  TMaskFrom extends string = TFrom,
-  TMaskTo extends string = '.',
+  TOptions,
   TRouter extends AnyRouter = RegisteredRouter,
-> = NavigateOptions<TRouter, TFrom, TTo, TMaskFrom, TMaskTo>
+> = Constrain<
+  TOptions,
+  NavigateOptions<
+    TRouter,
+    InferFrom<TOptions>,
+    InferTo<TOptions>,
+    InferMaskFrom<TOptions>,
+    InferMaskTo<TOptions>
+  >
+>
+
+export type ValidateNavigateOptionsArray<TOptions extends ReadonlyArray<any>> =
+  { [K in keyof TOptions]: ValidateNavigateOptions<TOptions[K]> }
 
 export type ValidateId<
   TId extends string,
   TRouter extends AnyRouter = RegisteredRouter,
 > = ConstrainLiteral<TId, RouteIds<TRouter['routeTree']>>
+
+/**
+ * @internal
+ */
+export type InferStrict<TOptions> = TOptions extends {
+  strict: infer TStrict extends boolean
+}
+  ? TStrict
+  : true
+
+/**
+ * @internal
+ */
+export type InferSelected<TOptions> = TOptions extends {
+  select: (...args: Array<any>) => infer TSelected
+}
+  ? TSelected
+  : unknown
+
+/**
+ * @internal
+ */
+export type InferStructuralSharing<TOptions> = TOptions extends {
+  structuralSharing: infer TStructuralSharing
+}
+  ? TStructuralSharing
+  : unknown
+
+export type ValidateUseSearchOptions<
+  TOptions,
+  TRouter extends AnyRouter = RegisteredRouter,
+> = Constrain<
+  TOptions,
+  UseSearchOptions<
+    TRouter,
+    InferFrom<TOptions>,
+    InferStrict<TOptions>,
+    InferSelected<TOptions>,
+    InferStructuralSharing<TOptions>
+  >
+>
+
+export type ValidateUseSearchResult<
+  TOptions,
+  TRouter extends AnyRouter = RegisteredRouter,
+> = UseSearchResult<
+  TRouter,
+  InferFrom<TOptions>,
+  InferStrict<TOptions>,
+  InferSelected<TOptions>
+>
+
+export type ValidateUseParamsOptions<
+  TOptions,
+  TRouter extends AnyRouter = RegisteredRouter,
+> = Constrain<
+  TOptions,
+  UseParamsOptions<
+    TRouter,
+    InferFrom<TOptions>,
+    InferStrict<TOptions>,
+    InferSelected<TOptions>,
+    InferSelected<TOptions>
+  >
+>
+
+export type ValidateUseParamsResult<
+  TOptions,
+  TRouter extends AnyRouter = RegisteredRouter,
+> = Constrain<
+  TOptions,
+  UseParamsResult<
+    TRouter,
+    InferFrom<TOptions>,
+    InferStrict<TOptions>,
+    InferSelected<TOptions>
+  >
+>
