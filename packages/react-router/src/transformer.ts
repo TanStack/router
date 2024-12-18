04da6791334058e8bda6b6ea0fdf7b7e166177ca
Tanner Linsley <tannerlinsley@gmail.com>
2024-12-18 16:45:17 -0700

    fix: allow errors, undefined from server functions

--- packages/react-router/src/transformer.ts
+++ packages/react-router/src/transformer.ts
@@ -3,6 +3,8 @@ import { isPlainObject } from './utils'
 export interface RouterTransformer {
   stringify: (obj: unknown) => string
   parse: (str: string) => unknown
+  encode: <T>(value: T) => T
+  decode: <T>(value: T) => T
 }
 
 export const defaultTransformer: RouterTransformer = {
@@ -20,32 +22,108 @@ export const defaultTransformer: RouterTransformer = {
   parse: (value: string) =>
     JSON.parse(value, function parser(key, value) {
       const keyVal = this[key]
-      const transformer = transformers.find((t) => t.parseCondition(keyVal))
-
-      if (transformer) {
-        return transformer.parse(keyVal)
+      if (isPlainObject(keyVal)) {
+        const transformer = transformers.find((t) => t.parseCondition(keyVal))
+        if (transformer) {
+          return transformer.parse(keyVal)
+        }
       }
 
       return value
     }),
+  encode: (value: any) => {
+    // When encodign, dive first
+    if (Array.isArray(value)) {
+      return value.map((v) => defaultTransformer.encode(v))
+    }
+
+    if (isPlainObject(value)) {
+      return Object.fromEntries(
+        Object.entries(value).map(([key, v]) => [
+          key,
+          defaultTransformer.encode(v),
+        ]),
+      )
+    }
+
+    const transformer = transformers.find((t) => t.stringifyCondition(value))
+    if (transformer) {
+      return transformer.stringify(value)
+    }
+
+    return value
+  },
+  decode: (value: any) => {
+    // Attempt transform first
+    if (isPlainObject(value)) {
+      const transformer = transformers.find((t) => t.parseCondition(value))
+      if (transformer) {
+        return transformer.parse(value)
+      }
+    }
+
+    if (Array.isArray(value)) {
+      return value.map((v) => defaultTransformer.decode(v))
+    }
+
+    if (isPlainObject(value)) {
+      return Object.fromEntries(
+        Object.entries(value).map(([key, v]) => [
+          key,
+          defaultTransformer.decode(v),
+        ]),
+      )
+    }
+
+    return value
+  },
 }
 
+const createTransformer = <T extends string>(
+  key: T,
+  check: (value: any) => boolean,
+  toValue: (value: any) => any = (v) => v,
+  fromValue: (value: any) => any = (v) => v,
+) => ({
+  key,
+  stringifyCondition: check,
+  stringify: (value: any) => ({ [`$${key}`]: toValue(value) }),
+  parseCondition: (value: any) => value[`$${key}`],
+  parse: (value: any) => fromValue(value[`$${key}`]),
+})
+
+// Keep these ordered by predicted frequency
 const transformers = [
-  {
-    // Dates
-    stringifyCondition: (value: any) => value instanceof Date,
-    stringify: (value: any) => ({ $date: value.toISOString() }),
-    parseCondition: (value: any) => isPlainObject(value) && value.$date,
-    parse: (value: any) => new Date(value.$date),
-  },
-  {
-    // undefined
-    stringifyCondition: (value: any) => value === undefined,
-    stringify: () => ({ $undefined: '' }),
-    parseCondition: (value: any) =>
-      isPlainObject(value) && value.$undefined === '',
-    parse: () => undefined,
-  },
+  createTransformer(
+    // Key
+    'undefined',
+    // Check
+    (v) => v === undefined,
+    // To
+    () => 0,
+    // From
+    () => undefined,
+  ),
+  createTransformer(
+    // Key
+    'date',
+    // Check
+    (v) => v instanceof Date,
+    // To
+    (v) => v.toISOString(),
+    // From
+    (v) => new Date(v),
+  ),
+  createTransformer(
+    // Key
+    'error',
+    // Check
+    (v) => v instanceof Error,
+    // To
+    (v) => ({ ...v, message: v.message, stack: v.stack, cause: v.cause }),
+    // From
+    (v) => Object.assign(new Error(v.message), v),
+  ),
 ] as const
 
 export type TransformerStringify<T, TSerializable> = T extends TSerializable
