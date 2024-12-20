15480475a1b55b656cfb65422aa9d74755d7d895
Lenz Weber-Tronic <lorenz.weber-tronic@apollographql.com>
2024-12-20 15:38:08 +0100

    reduced approach

--- examples/react/start-basic-apollo-client/app/apollo/index.ts
+++ examples/react/start-basic-apollo-client/app/apollo/index.ts
@@ -1,6 +1,9 @@
 import { defaultTransformer } from '@tanstack/react-router'
+import {
+  isTransportedQueryRef,
+  reviveTransportedQueryRef,
+} from '@apollo/client-react-streaming'
 import { createTransportedQueryPreloader } from './createQueryPreloader'
-import { getApolloTransformer } from './getApolloTransformer'
 import { ApolloProvider } from './ApolloProvider'
 import type { PreloadQueryFunction } from '@apollo/client'
 import type { AnyRouter } from '@tanstack/react-router'
@@ -27,9 +30,43 @@ export function routerWithApolloClient<TRouter extends AnyRouter>(
   // @ts-ignore unavoidable due to the ternary in arguments
   router.options.context.preloadQuery =
     createTransportedQueryPreloader(apolloClient)
-  router.options.transformer = defaultTransformer.withTransformers([
-    getApolloTransformer(apolloClient),
-  ])
+
+  // eslint-disable-next-line @typescript-eslint/no-unnecessary-condition
+  const previousTransformer = router.options.transformer || defaultTransformer
+
+  router.options.transformer = {
+    stringify(value) {
+      return previousTransformer.stringify(value)
+    },
+    parse(str) {
+      const transformed = previousTransformer.parse(str)
+
+      JSON.stringify(transformed, (_, value) => {
+        if (isTransportedQueryRef(value)) {
+          // Due to the timing in `afterHydrate`, the stream at this point will still be an empty object
+          // to be replaced by the `extracted` stream value a moment later
+          // Nonetheless, we want to kick off hydration of the queryRef here already,
+          // so query deduplication kicks in.
+          // So we create an intermediate `TransformStream` that is exposed via a getter,
+          // while at the same time, a setter is put into place that will start piping the
+          // incoming stream into our intermediate stream as soon as it is set.
+          // The check for `instanceof ReadableStream` is here just in case that the timing changes
+          // in the future and we don't need to do this anymore.
+          const intermediateStream = new TransformStream()
+          Object.defineProperty(value.$__apollo_queryRef, 'stream', {
+            get: () => intermediateStream.readable,
+            set: (value: ReadableStream) => {
+              value.pipeTo(intermediateStream.writable)
+            },
+          })
+          reviveTransportedQueryRef(value, apolloClient)
+        }
+      })
+
+      return transformed
+    },
+  }
+
   router.options.InnerWrap = ApolloProvider
 
   return router
