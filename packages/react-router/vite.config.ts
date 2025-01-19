b116507b679e5b386836bdbbe79783b2d20dd4e5
Birk Skyum <birk.skyum@pm.me>
2025-01-19 14:51:50 +0100

    add alias to vite config

--- packages/react-router/vite.config.ts
+++ packages/react-router/vite.config.ts
@@ -1,3 +1,4 @@
+import path from 'node:path'
 import { defineConfig, mergeConfig } from 'vitest/config'
 import { tanstackViteConfig } from '@tanstack/config/vite'
 import react from '@vitejs/plugin-react'
@@ -5,7 +6,14 @@ import packageJson from './package.json'
 import type { UserConfig } from 'vitest/config'
 
 const config = defineConfig({
-  plugins: [react()] as UserConfig['plugins'],
+  plugins: [
+    react(),
+  ] as UserConfig['plugins'],
+  resolve: {
+    alias: {
+      '@tanstack/router-core': path.resolve(__dirname, './../node_modules/@tanstack/router-core/src/')
+    }
+  },
   test: {
     name: packageJson.name,
     dir: './tests',
@@ -13,6 +21,12 @@ const config = defineConfig({
     environment: 'jsdom',
     typecheck: { enabled: true },
     setupFiles: ['./tests/setupTests.tsx'],
+    alias: [
+      {
+        find: '@tanstack/router-core',
+        replacement: path.resolve(__dirname, '../router-core/src/index.ts')
+      }
+    ]
   },
 })
 
