da84bbb1b99bfd9a2ddb4ab659677ebefe760780
Manuel Schiller <manuel.schiller@caligano.de>
2025-01-22 19:22:19 +0100

    fix(start): fix script initialization order (#3215)

--- packages/react-router/src/ScriptOnce.tsx
+++ packages/react-router/src/ScriptOnce.tsx
@@ -1,14 +1,40 @@
+import jsesc from 'jsesc'
 import { useRouter } from './useRouter'
 
 export function ScriptOnce({
   children,
   log,
+  sync,
 }: {
   children: string
   log?: boolean
+  sync?: boolean
 }) {
   const router = useRouter()
+  if (typeof document !== 'undefined') {
+    return null
+  }
 
-  router.injectScript(children, { logScript: log })
-  return null
+  if (!sync) {
+    router.injectScript(children, { logScript: log })
+    return null
+  }
+
+  return (
+    <script
+      className="tsr-once"
+      dangerouslySetInnerHTML={{
+        __html: [
+          children,
+          (log ?? true) && process.env.NODE_ENV === 'development'
+            ? `console.info(\`Injected From Server:
+${jsesc(children.toString(), { quotes: 'backtick' })}\`)`
+            : '',
+          'if (typeof __TSR__ !== "undefined") __TSR__.cleanScripts()',
+        ]
+          .filter(Boolean)
+          .join('\n'),
+      }}
+    />
+  )
 }
