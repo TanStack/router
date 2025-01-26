8174f365e199d1c7851056d3b7b78a5373b62a1a
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-01-27 12:36:10 +1300

    chore(e2e): downgrade to tailwindcss 3

--- e2e/react-router/scroll-restoration-sandbox-vite/vite.config.js
+++ e2e/react-router/scroll-restoration-sandbox-vite/vite.config.js
@@ -1,9 +1,8 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
-import tailwindcss from '@tailwindcss/vite'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [TanStackRouterVite(), react(), tailwindcss()],
+  plugins: [TanStackRouterVite(), react()],
 })
