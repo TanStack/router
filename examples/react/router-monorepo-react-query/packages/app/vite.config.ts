61959a332b0c29371ea2295c9c0d7b13500e23e4
Nicolas Beaussart <nic.beaussart@gmail.com>
2025-01-26 23:10:40 +0100

    chore(examples): tailwind v4 on monorepo based examples (#3247)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- examples/react/router-monorepo-react-query/packages/app/vite.config.ts
+++ examples/react/router-monorepo-react-query/packages/app/vite.config.ts
@@ -3,6 +3,7 @@ import dts from 'vite-plugin-dts'
 import * as path from 'node:path'
 import { fileURLToPath } from 'node:url'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const __filename = fileURLToPath(import.meta.url)
 const __dirname = path.dirname(__filename)
@@ -26,6 +27,7 @@ export default defineConfig({
       tsconfigPath: path.join(__dirname, 'tsconfig.json'),
     }),
     react(),
+    tailwindcss(),
   ],
   build: {
     outDir: './dist',
