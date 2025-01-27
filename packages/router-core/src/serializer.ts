61ccb0ca01a4a54504fd969da716aec89ccf1b6c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-01-28 12:17:39 +1300

    fix(router-core): allow return `Response`

--- packages/router-core/src/serializer.ts
+++ packages/router-core/src/serializer.ts
@@ -17,7 +17,7 @@ export type SerializerParseBy<T, TSerializable> = T extends TSerializable
     ? ReadableStream
     : { [K in keyof T]: SerializerParseBy<T[K], TSerializable> }
 
-export type Serializable = Date | undefined | Error | FormData
+export type Serializable = Date | undefined | Error | FormData | Response
 
 export type SerializerStringify<T> = SerializerStringifyBy<T, Serializable>
 
