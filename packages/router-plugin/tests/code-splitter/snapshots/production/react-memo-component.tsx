1b5704e5a1b0a3eab9eee7ae50176edac625132c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-01-30 10:29:22 +1300

    test(router-plugin): update snapshots to expect the dummy component export

--- packages/router-plugin/tests/code-splitter/snapshots/production/react-memo-component.tsx
+++ packages/router-plugin/tests/code-splitter/snapshots/production/react-memo-component.tsx
@@ -7,3 +7,6 @@ export const Route = createFileRoute('/')({
   component: lazyRouteComponent($$splitComponentImporter, 'component', () => Route.ssr),
   loader: lazyFn($$splitLoaderImporter, 'loader')
 });
+export function TSRDummyComponent() {
+  return null;
+}
