5cfe9f276f9691a72910fdf8cade7ac269af1a14
Birk Skyum <birk.skyum@pm.me>
2025-01-30 02:42:13 +0100

    remove react lint from solid-router

--- packages/solid-router/eslint.config.ts
+++ packages/solid-router/eslint.config.ts
@@ -1,24 +1,8 @@
-import pluginSolid from '@eslint-react/eslint-plugin'
-// @ts-expect-error
-import pluginSolidHooks from 'eslint-plugin-react-hooks'
 import rootConfig from '../../eslint.config.js'
 
 export default [
   ...rootConfig,
   {
     files: ['src/**/*.{ts,tsx}', 'tests/**/*.{ts,tsx}'],
-  },
-  {
-    plugins: {
-      'react-hooks': pluginSolidHooks,
-      '@eslint-react': pluginSolid,
-    },
-    rules: {
-      '@eslint-react/no-unstable-context-value': 'off',
-      '@eslint-react/no-unstable-default-props': 'off',
-      '@eslint-react/dom/no-missing-button-type': 'off',
-      'react-hooks/exhaustive-deps': 'error',
-      'react-hooks/rules-of-hooks': 'error',
-    },
   },
 ]
