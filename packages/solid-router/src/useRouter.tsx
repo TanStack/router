c8dbd298be6a92d5dfba63c2444f11f598363c5a
Birk Skyum <birk.skyum@pm.me>
2025-01-30 01:46:35 +0100

    fix imports

--- packages/solid-router/src/useRouter.tsx
+++ packages/solid-router/src/useRouter.tsx
@@ -1,4 +1,4 @@
-import * as Solid from 'react'
+import * as Solid from 'solid-js'
 import warning from 'tiny-warning'
 import { getRouterContext } from './routerContext'
 import type { AnyRouter, RegisteredRouter } from './router'
@@ -6,7 +6,7 @@ import type { AnyRouter, RegisteredRouter } from './router'
 export function useRouter<TRouter extends AnyRouter = RegisteredRouter>(opts?: {
   warn?: boolean
 }): TRouter {
-  const value = Solid.useContext(getRouterContext())
+  const value = Solid.useContext(getRouterContext() as any)
   warning(
     !((opts?.warn ?? true) && !value),
     'useRouter must be used inside a <RouterProvider> component!',
