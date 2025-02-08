3b158d0ab08f9ea4d4490d707470ca73cd9288a4
Manuel Schiller <manuel.schiller@caligano.de>
2025-02-08 22:36:40 +0100

    feat(react-router): add `shouldThrow` option to `useParams` and `useSearch` (#3368)

--- packages/react-router/src/route.ts
+++ packages/react-router/src/route.ts
@@ -648,19 +648,21 @@ export class RouteApi<TId, TRouter extends AnyRouter = RegisteredRouter> {
   }
 
   useSearch: UseSearchRoute<TId> = (opts) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     return useSearch({
       select: opts?.select,
       structuralSharing: opts?.structuralSharing,
       from: this.id,
-    } as any)
+    } as any) as any
   }
 
   useParams: UseParamsRoute<TId> = (opts) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     return useParams({
       select: opts?.select,
       structuralSharing: opts?.structuralSharing,
       from: this.id,
-    } as any)
+    } as any) as any
   }
 
   useLoaderDeps: UseLoaderDepsRoute<TId> = (opts) => {
@@ -1056,19 +1058,21 @@ export class Route<
   }
 
   useSearch: UseSearchRoute<TId> = (opts) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     return useSearch({
       select: opts?.select,
       structuralSharing: opts?.structuralSharing,
       from: this.id,
-    } as any)
+    } as any) as any
   }
 
   useParams: UseParamsRoute<TId> = (opts) => {
+    // eslint-disable-next-line @typescript-eslint/no-unnecessary-type-assertion
     return useParams({
       select: opts?.select,
       structuralSharing: opts?.structuralSharing,
       from: this.id,
-    } as any)
+    } as any) as any
   }
 
   useLoaderDeps: UseLoaderDepsRoute<TId> = (opts) => {
