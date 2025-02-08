dab006fa0ce35fe22491801adc7c9bbad9d94b50
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-02-08 16:26:57 +1300

    test(router-plugin): add more test files

--- packages/router-plugin/tests/code-splitter/shared/imported.tsx
+++ packages/router-plugin/tests/code-splitter/shared/imported.tsx
@@ -4,6 +4,18 @@ export const importedComponent = () => {
   return <div>I am imported</div>
 }
 
+export const importedErrorComponent = () => {
+  return <div>I am an error imported</div>
+}
+
+export const importedNotFoundComponent = () => {
+  return <div>I am a not found imported</div>
+}
+
+export const importedPendingComponent = () => {
+  return <div>I am a pending imported</div>
+}
+
 export const importedLoader = () => {
   return {
     randomNumber: Math.random(),
---
+++ packages/router-plugin/tests/code-splitter/snapshots/1-default/development/imported-errorComponent.tsx
@@ -0,0 +1,11 @@
+const $$splitErrorComponentImporter = () => import('imported-errorComponent.tsx?tsr-split=errorComponent');
+const $$splitComponentImporter = () => import('imported-errorComponent.tsx?tsr-split=component');
+import { lazyRouteComponent } from '@tanstack/react-router';
+import { createFileRoute } from '@tanstack/react-router';
+export const Route = createFileRoute('/')({
+  component: lazyRouteComponent($$splitComponentImporter, 'component', () => Route.ssr),
+  errorComponent: lazyRouteComponent($$splitErrorComponentImporter, 'errorComponent')
+});
+export function TSRDummyComponent() {
+  return null;
+}
