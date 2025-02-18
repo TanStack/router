0277309b1bc46f5ae7d68117a18716e34b2de3a9
Manuel Schiller <manuel.schiller@caligano.de>
2025-02-18 02:10:05 +0100

    fix(react-router): only navigate once in StrictMode (#3465)

--- packages/react-router/src/useNavigate.tsx
+++ packages/react-router/src/useNavigate.tsx
@@ -54,13 +54,22 @@ export function Navigate<
   const TMaskFrom extends string = TFrom,
   const TMaskTo extends string = '',
 >(props: NavigateOptions<TRouter, TFrom, TTo, TMaskFrom, TMaskTo>): null {
-  const { navigate } = useRouter()
+  const router = useRouter()
 
+  const previousPropsRef = React.useRef<NavigateOptions<
+    TRouter,
+    TFrom,
+    TTo,
+    TMaskFrom,
+    TMaskTo
+  > | null>(null)
   React.useEffect(() => {
-    navigate({
-      ...props,
-    } as any)
-  }, [navigate, props])
-
+    if (previousPropsRef.current !== props) {
+      router.navigate({
+        ...props,
+      })
+      previousPropsRef.current = props
+    }
+  }, [router, props])
   return null
 }
