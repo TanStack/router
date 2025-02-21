cdf34d850765c17aba5a37970971569da02c9185
Tanner Linsley <tannerlinsley@gmail.com>
2025-02-21 10:51:06 -0700

    rebased on fix:globalmiddleware

--- scripts/publish.js
+++ scripts/publish.js
@@ -145,14 +145,6 @@ await publish({
       packageDir: 'packages/react-start-server',
     },
     {
-      name: '@tanstack/start-config',
-      packageDir: 'packages/start-config',
-    },
-    {
-      name: '@tanstack/react-start-config',
-      packageDir: 'packages/react-start-config',
-    },
-    {
       name: '@tanstack/react-start-api-routes',
       packageDir: 'packages/react-start-api-routes',
     },
