75d1a9811852a9444433d69745fc7eacf5711abb
Birk Skyum <birk.skyum@pm.me>
2025-02-22 13:21:28 +0100

    tanstack solid-start

---
+++ packages/solid-start-server-functions-fetcher/vite.config.ts
@@ -0,0 +1,22 @@
+import { defineConfig, mergeConfig } from 'vitest/config'
+import { tanstackViteConfig } from '@tanstack/config/vite'
+import solid from 'vite-plugin-solid'
+import packageJson from './package.json'
+import type { ViteUserConfig } from 'vitest/config'
+
+const config = defineConfig({
+  plugins: [solid()] as ViteUserConfig['plugins'],
+  test: {
+    name: packageJson.name,
+    watch: false,
+    environment: 'jsdom',
+  },
+})
+
+export default mergeConfig(
+  config,
+  tanstackViteConfig({
+    entry: './src/index.tsx',
+    srcDir: './src',
+  }),
+)
