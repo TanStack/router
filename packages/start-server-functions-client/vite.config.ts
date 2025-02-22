28b7d59b9f7c0cb8c73b4248b8af6d511fb90f38
Birk Skyum <birk.skyum@pm.me>
2025-02-22 04:42:26 +0100

    make package agnostic

--- packages/start-server-functions-client/vite.config.ts
+++ packages/start-server-functions-client/vite.config.ts
@@ -1,11 +1,10 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 import { tanstackViteConfig } from '@tanstack/config/vite'
-import react from '@vitejs/plugin-react'
 import packageJson from './package.json'
 import type { ViteUserConfig } from 'vitest/config'
 
 const config = defineConfig({
-  plugins: [react()] as ViteUserConfig['plugins'],
+  plugins: [] as ViteUserConfig['plugins'],
   test: {
     name: packageJson.name,
     watch: false,
