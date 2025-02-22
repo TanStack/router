581bef0e315e62a2022660db16991059ce0146f3
Birk Skyum <birk.skyum@pm.me>
2025-02-22 13:21:28 +0100

    tanstack solid-start

---
+++ e2e/solid-start/basic/app/components/CustomMessage.tsx
@@ -0,0 +1,9 @@
+
+export function CustomMessage({ message }: { message: string }) {
+  return (
+    <div className="py-2">
+      <div className="italic">This is a custom message:</div>
+      <p>{message}</p>
+    </div>
+  )
+}
