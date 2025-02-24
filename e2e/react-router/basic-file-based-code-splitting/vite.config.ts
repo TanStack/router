8706737ae3436b42a5f61ce1249f378ea6915f7c
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-02-24 20:15:43 +1300

    test(e2e): update e2e

--- e2e/react-router/basic-file-based-code-splitting/vite.config.ts
+++ e2e/react-router/basic-file-based-code-splitting/vite.config.ts
@@ -6,6 +6,7 @@ import { TanStackRouterVite } from '@tanstack/router-plugin/vite'
 export default defineConfig({
   plugins: [
     TanStackRouterVite({
+      target: 'react',
       autoCodeSplitting: true,
       codeSplittingOptions: {
         splitBehavior: ({ routeId }) => {
