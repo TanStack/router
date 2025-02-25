7a8c94c3ea05eeedcdb89ebdfdaae8c40312ae65
Manuel Schiller <manuel.schiller@caligano.de>
2025-02-26 00:34:58 +0100

    fix merge, format

--- packages/react-start-plugin/src/nitro/options.ts
+++ packages/react-start-plugin/src/nitro/options.ts
@@ -1,53 +1,53 @@
-import { PrerenderRoute } from 'nitropack';
+import { PrerenderRoute } from 'nitropack'
 
 export interface Options {
-  ssr?: boolean;
-  ssrBuildDir?: string;
+  ssr?: boolean
+  ssrBuildDir?: string
   /**
    * Prerender the static pages without producing the server output.
    */
-  static?: boolean;
-  prerender?: PrerenderOptions;
-  entryServer?: string;
-  index?: string;
-  workspaceRoot?: string;
+  static?: boolean
+  prerender?: PrerenderOptions
+  entryServer?: string
+  index?: string
+  workspaceRoot?: string
   /**
    * Additional page paths to include
    */
-  additionalPagesDirs?: string[];
+  additionalPagesDirs?: string[]
   /**
    * Additional API paths to include
    */
-  additionalAPIDirs?: string[];
-  apiPrefix?: string;
+  additionalAPIDirs?: string[]
+  apiPrefix?: string
 
   /**
    * Toggles internal API middleware.
    * If disabled, a proxy request is used to route /api
    * requests to / in the production server build.
    */
-  useAPIMiddleware?: boolean;
+  useAPIMiddleware?: boolean
 }
 
 export interface PrerenderOptions {
   /**
    * Add additional routes to prerender through crawling page links.
    */
-  discover?: boolean;
+  discover?: boolean
 
   /**
    * List of routes to prerender resolved statically or dynamically.
    */
   routes?:
     | (string | PrerenderContentDir)[]
-    | (() => Promise<(string | PrerenderContentDir | undefined)[]>);
-  sitemap?: SitemapConfig;
+    | (() => Promise<(string | PrerenderContentDir | undefined)[]>)
+  sitemap?: SitemapConfig
   /** List of functions that run for each route after pre-rendering is complete. */
-  postRenderingHooks?: ((routes: PrerenderRoute) => Promise<void>)[];
+  postRenderingHooks?: ((routes: PrerenderRoute) => Promise<void>)[]
 }
 
 export interface SitemapConfig {
-  host: string;
+  host: string
 }
 
 export interface PrerenderContentDir {
@@ -55,14 +55,14 @@ export interface PrerenderContentDir {
    * The directory where files should be grabbed from.
    * @example `/src/contents/blog`
    */
-  contentDir: string;
+  contentDir: string
   /**
    * Transform the matching content files path into a route.
    * The function is called for each matching content file within the specified contentDir.
    * @param file information of the matching file (`path`, `name`, `extension`, `attributes`)
    * @returns a string with the route should be returned (e. g. `/blog/<slug>`) or the value `false`, when the route should not be prerendered.
    */
-  transform: (file: PrerenderContentFile) => string | false;
+  transform: (file: PrerenderContentFile) => string | false
 }
 
 /**
@@ -73,8 +73,8 @@ export interface PrerenderContentDir {
  * @returns a string with the route should be returned (e. g. `/blog/<slug>`) or the value `false`, when the route should not be prerendered.
  */
 export interface PrerenderContentFile {
-  path: string;
-  attributes: Record<string, any>;
-  name: string;
-  extension: string;
+  path: string
+  attributes: Record<string, any>
+  name: string
+  extension: string
 }
