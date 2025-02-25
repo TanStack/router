049a8f769c7501a2308ba12fd9d0e1406d343f5c
Brenley Dueck <brenleydueck@gmail.com>
2025-02-25 17:36:26 -0600

    Fix streaming and hydration

--- e2e/solid-start/basic/app/client.tsx
+++ e2e/solid-start/basic/app/client.tsx
@@ -1,10 +1,8 @@
 /// <reference types="vinxi/types/client" />
-import { StartClient } from '@tanstack/solid-start'
 import { createRouter } from './router'
 import { hydrate } from 'solid-js/web'
+import { RouterProvider } from '@tanstack/solid-router'
 
 const router = createRouter()
 
-const appDiv = document.getElementById('app')!
-
-hydrate(() => <StartClient router={router} />, appDiv)
+hydrate(() => <RouterProvider router={router} />, document)
