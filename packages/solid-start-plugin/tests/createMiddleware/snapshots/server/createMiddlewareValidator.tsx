a3a297b99f3e492dde746dcdb33e01e99c25f1d6
Birk Skyum <birk.skyum@pm.me>
2025-02-25 22:13:39 +0100

    add solid-start-plugin

---
+++ packages/solid-start-plugin/tests/createMiddleware/snapshots/server/createMiddlewareValidator.tsx
@@ -0,0 +1,7 @@
+import { createMiddleware } from '@tanstack/solid-start';
+import { z } from 'zod';
+export const withUseServer = createMiddleware({
+  id: 'test'
+}).validator(z.number()).server(({
+  input
+}) => input + 1);
