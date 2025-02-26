1b0b61e1c9b2b107fa3ce37a152def04bfefb956
Manuel Schiller <manuel.schiller@caligano.de>
2025-02-26 22:55:06 +0100

    add example again

--- e2e/react-start/basic/app/routes/index.tsx
+++ e2e/react-start/basic/app/routes/index.tsx
@@ -1,15 +1,27 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { CustomMessage } from '~/components/CustomMessage'
 
 export const Route = createFileRoute('/')({
+  ssr: false,
+  beforeLoad: () => {
+    console.log('beforeLoad')
+  },
+  loader: async () => {
+    console.log('loader')
+    await new Promise((resolve) => setTimeout(resolve, 500))
+    return {
+      message: 'Hello from the server!',
+    }
+  },
+  pendingComponent: () => <div className="p-2">Loading...</div>,
   component: Home,
 })
 
 function Home() {
+  const { message } = Route.useLoaderData()
   return (
     <div className="p-2">
       <h3>Welcome Home!!!</h3>
-      <CustomMessage message="Hello from a custom component!" />
+      <p>{message}</p>
     </div>
   )
 }
