01ae9b75550775ef17275e4b38594ddb4c8be2dd
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-02-27 13:54:18 +1300

    chore(examples): add quickstart-file-based for solid-router

---
+++ examples/solid/quickstart-file-based/src/routes/index.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react'
+import { createFileRoute } from '@tanstack/solid-router'
+
+export const Route = createFileRoute('/')({
+  component: HomeComponent,
+})
+
+function HomeComponent() {
+  return (
+    <div class="p-2">
+      <h3>Welcome Home!</h3>
+    </div>
+  )
+}
