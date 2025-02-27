a4d2e81b695f3badfc31a5d261908afb21c84a0a
Birk Skyum <birk.skyum@pm.me>
2025-02-27 18:16:18 +0100

    add e2e/solid-start/webbsite

--- e2e/solid-start/basic/app/routes/__root.tsx
+++ e2e/solid-start/basic/app/routes/__root.tsx
@@ -120,13 +120,3 @@ function RootComponent() {
     </>
   )
 }
-
-// const RouterDevtools =
-//   process.env.NODE_ENV === 'production'
-//     ? () => null // Render nothing in production
-//     : React.lazy(() =>
-//         // Lazy load in development
-//         import('@tanstack/router-devtools').then((res) => ({
-//           default: res.TanStackRouterDevtools,
-//         })),
-//       )
