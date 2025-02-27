04ea5b1ee2a4932f9439218b1240f42f82fe22ec
Birk Skyum <birk.skyum@pm.me>
2025-02-27 18:37:42 +0100

    add basic-tsr-config

---
+++ e2e/solid-start/basic-tsr-config/src/app/routes/index.tsx
@@ -0,0 +1,36 @@
+import { createFileRoute, useRouter } from '@tanstack/solid-router'
+import { createServerFn } from '@tanstack/solid-start'
+
+let count = 0
+
+const getCount = createServerFn({ method: 'GET' }).handler(() => {
+  return count
+})
+
+const updateCount = createServerFn({ method: 'POST' })
+  .validator((addBy: number) => addBy)
+  .handler(({ data: addBy }) => {
+    count += addBy
+  })
+
+export const Route = createFileRoute('/')({
+  component: Home,
+  loader: async () => await getCount(),
+})
+
+function Home() {
+  const router = useRouter()
+  const state = Route.useLoaderData()
+
+  return (
+    <button
+      onClick={() => {
+        updateCount({ data: 1 }).then(() => {
+          router.invalidate()
+        })
+      }}
+    >
+      Add 1 to {state()}?
+    </button>
+  )
+}
