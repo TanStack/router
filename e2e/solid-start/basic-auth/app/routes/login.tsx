08fef4347b10770f31422ef6ea46910d37f3d78f
Birk Skyum <birk.skyum@pm.me>
2025-02-27 19:21:48 +0100

    solid-start basic auth

---
+++ e2e/solid-start/basic-auth/app/routes/login.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/solid-router'
+
+import { Login } from '~/components/Login'
+
+export const Route = createFileRoute('/login')({
+  component: LoginComp,
+})
+
+function LoginComp() {
+  return <Login />
+}
