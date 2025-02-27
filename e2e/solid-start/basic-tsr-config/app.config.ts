61d86c2e04d0aff23f642fcfb1f34bcb016b3e70
Birk Skyum <birk.skyum@pm.me>
2025-02-27 22:42:39 +0100

    defer tsr config issue to react-router

--- e2e/solid-start/basic-tsr-config/app.config.ts
+++
@@ -1,8 +0,0 @@
-// app.config.ts
-import { defineConfig } from '@tanstack/solid-start/config'
-
-export default defineConfig({
-  tsr: {
-    appDirectory: './src/app',
-  },
-})
--- e2e/solid-start/basic-tsr-config/tests/app.spec.ts
+++
@@ -1,9 +0,0 @@
-import { expect, test } from '@playwright/test'
-
-test('opening the app', async ({ page }) => {
-  await page.goto('/')
-
-  await expect(page.getByText('Add 1 to 0?')).toContainText('Add 1 to 0?')
-  await page.click('button')
-  await expect(page.getByText('Add 1 to 1?')).toContainText('Add 1 to 1?')
-})
