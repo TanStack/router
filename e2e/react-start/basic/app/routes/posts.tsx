ca056fbe80bdc05a8c06e9bfb5f7b19e98726c7b
Birk Skyum <birk.skyum@pm.me>
2025-02-28 18:45:21 +0100

    test(react-start): check change to meta tags on client navigation

--- e2e/react-start/basic/app/routes/posts.tsx
+++ e2e/react-start/basic/app/routes/posts.tsx
@@ -3,6 +3,13 @@ import { Link, Outlet, createFileRoute } from '@tanstack/react-router'
 import { fetchPosts } from '~/utils/posts'
 
 export const Route = createFileRoute('/posts')({
+  head: () => ({
+    meta: [{
+        title:
+          'Posts page',
+      },
+    ]
+  }),
   loader: async () => fetchPosts(),
   component: PostsComponent,
 })
