0259f06abae76dc269656a47bc4556271f76ac81
Birk Skyum <birk.skyum@pm.me>
2025-02-28 23:21:42 +0100

    test(basic-tsr-config): make test more strict

--- e2e/react-start/basic-tsr-config/tests/app.spec.ts
+++ e2e/react-start/basic-tsr-config/tests/app.spec.ts
@@ -3,7 +3,7 @@ import { expect, test } from '@playwright/test'
 test('opening the app', async ({ page }) => {
   await page.goto('/')
 
-  await expect(page.getByText('Add 1 to 0?')).toBeTruthy()
+  await expect(page.getByText('Add 1 to 0?')).toContainText('Add 1 to 0?')
   await page.click('button')
-  await expect(page.getByText('Add 1 to 1?')).toBeTruthy()
+  await expect(page.getByText('Add 1 to 1?')).toContainText('Add 1 to 1?')
 })
