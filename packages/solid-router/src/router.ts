a5c9ca7a8ec262801cef54e8dbdee326593ca4ca
JP Rosevear <jp@codalio.com>
2025-03-01 14:58:41 -0500

    correct

--- packages/solid-router/src/router.ts
+++ packages/solid-router/src/router.ts
@@ -394,7 +394,7 @@ export class Router<
     if (
       typeof window !== 'undefined' &&
       'CSS' in window &&
-      typeof window.CSS.supports === 'function'
+      typeof window.CSS?.supports === 'function'
     ) {
       this.isViewTransitionTypesSupported = window.CSS.supports(
         'selector(:active-view-transition-type(a)',
