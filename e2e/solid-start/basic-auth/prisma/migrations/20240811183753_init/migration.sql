0a2eac5cff2dd36df921b662aa2658810f24222f
Birk Skyum <birk.skyum@pm.me>
2025-03-01 04:33:01 +0100

    remove basic auth

--- e2e/solid-start/basic-auth/prisma/migrations/20240811183753_init/migration.sql
+++
@@ -1,8 +0,0 @@
--- CreateTable
-CREATE TABLE "User" (
-    "email" TEXT NOT NULL PRIMARY KEY,
-    "password" TEXT NOT NULL
-);
-
--- CreateIndex
-CREATE UNIQUE INDEX "User_email_key" ON "User"("email");
