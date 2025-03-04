64c2acdd6ee4cd20a63d1d07bd3518590e3b4e15
Hoseong Lee <hoseong.a.lee@gmail.com>
2025-03-03 18:38:58 -0600

    split single unit test for the parseLocation into multiple focused cases

--- packages/history/tests/createHashHistory.test.ts
+++ packages/history/tests/createHashHistory.test.ts
@@ -3,22 +3,36 @@ import { describe, expect, test } from 'vitest'
 import {createHashHistory} from '../src'
 
 describe('createHashHistory', () => {
-    test('parseLocation', () => {
-        const history = createHashHistory()
-        window.history.pushState({}, "", "/")
-        expect(history.location.pathname).toBe('/')
-        expect(history.location.search).toBe('')
-        window.history.pushState({}, "", "/#hello")
-        expect(history.location.pathname).toBe('/hello')
-        expect(history.location.search).toBe('')
-        window.history.pushState({}, "", "/?search=params")
-        expect(history.location.pathname).toBe('/')
-        expect(history.location.search).toBe('?search=params')
-        window.history.pushState({}, "", "/#hello?search=params")
-        expect(history.location.pathname).toBe('/hello')
-        expect(history.location.search).toBe('?search=params')
-        window.history.pushState({}, "", "/?search=params#hello")
-        expect(history.location.pathname).toBe('/hello')
-        expect(history.location.search).toBe('?search=params')
+    describe('parseLocation', () => {
+        test('neither search params nor hash', () => {
+            const history = createHashHistory()
+            window.history.pushState({}, "", "/")
+            expect(history.location.pathname).toBe('/')
+            expect(history.location.search).toBe('')
+        })
+        test('hash present, no search params', () => {
+            const history = createHashHistory()
+            window.history.pushState({}, "", "/#hello")
+            expect(history.location.pathname).toBe('/hello')
+            expect(history.location.search).toBe('')
+        })
+        test('search params present, no hash', () => {
+            const history = createHashHistory()
+            window.history.pushState({}, "", "/?search=params")
+            expect(history.location.pathname).toBe('/')
+            expect(history.location.search).toBe('?search=params')
+        })
+        test('both search params and hash present, in that order', () => {
+            const history = createHashHistory()
+            window.history.pushState({}, "", "/#hello?search=params")
+            expect(history.location.pathname).toBe('/hello')
+            expect(history.location.search).toBe('?search=params')
+        })
+        test('both hash and search params present, in that order', () => {
+            const history = createHashHistory()
+            window.history.pushState({}, "", "/?search=params#hello")
+            expect(history.location.pathname).toBe('/hello')
+            expect(history.location.search).toBe('?search=params')
+        })
     })
 })
