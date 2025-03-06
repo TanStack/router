cc93485b19540c7df22f63f1e3a6dc8a1d6e2873
Birk Skyum <birk.skyum@pm.me>
2025-03-06 16:26:16 +0100

    fix solid-query-devtools build

--- packages/solid-router-devtools/vite.config.ts
+++ packages/solid-router-devtools/vite.config.ts
@@ -10,7 +10,7 @@ const config = defineConfig({
 export default mergeConfig(
   config,
   tanstackViteConfig({
-    entry: './src/index.tsx',
+    entry: './src/index.ts',
     srcDir: './src',
   }),
 )
