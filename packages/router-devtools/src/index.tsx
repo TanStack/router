e232bca18e7aa96964d3942b592301093424f505
Birk Skyum <birk.skyum@pm.me>
2025-03-07 18:42:03 +0100

    add migration note on router-devtools

--- packages/router-devtools/src/index.tsx
+++ packages/router-devtools/src/index.tsx
@@ -1,2 +1,6 @@
+console.warn(
+  '[@tanstack/router-devtools] Warning: This package has moved to @tanstack/react-router-devtools. Please switch to the new package, as this package will be dropped soon.',
+)
+
 export { TanStackRouterDevtools } from './TanStackRouterDevtools'
 export { TanStackRouterDevtoolsPanel } from './TanStackRouterDevtoolsPanel'
