12ee34a157e2f211b0bbbbc45bb3baff828819c2
Tanner Linsley <tannerlinsley@gmail.com>
2025-03-07 12:11:26 -0700

    It's working!

--- examples/react/start-basic/app/ssr.tsx
+++ examples/react/start-basic/app/ssr.tsx
@@ -2,14 +2,11 @@ import {
   createStartHandler,
   defaultStreamHandler,
   defineEventHandler,
-  getWebRequest,
 } from '@tanstack/react-start/server'
 
 import { createRouter } from './router'
 
 export default defineEventHandler((event) => {
-  console.log(getWebRequest(event)?.url)
-
   const startHandler = createStartHandler({
     createRouter,
   })(defaultStreamHandler)
