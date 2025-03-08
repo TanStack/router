22ddc762f9f56816249e7fb5fd1350649c2f8a9f
Birk Skyum <birk.skyum@pm.me>
2025-03-08 02:06:50 +0100

    Revert "forward router-devtools to new packgage"
    This reverts commit 8156c43029b304da7ffaa16b07f499dad208b85d.

--- packages/router-devtools/src/index.tsx
+++ packages/router-devtools/src/index.tsx
@@ -2,5 +2,5 @@ console.warn(
   '[@tanstack/router-devtools] This package has moved to @tanstack/react-router-devtools. Please switch to the new package at your earliest convenience, as this package will be dropped in the next major version release.',
 )
 
-export { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+export { TanStackRouterDevtools } from './TanStackRouterDevtools'
 export { TanStackRouterDevtoolsPanel } from './TanStackRouterDevtoolsPanel'
