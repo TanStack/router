c6dbfe8269bfc53ead67b7142aa24f9f4b50caa9
Birk Skyum <birk.skyum@pm.me>
2025-03-08 02:30:14 +0100

    retain existing behavior

--- packages/router-devtools/src/index.tsx
+++ packages/router-devtools/src/index.tsx
@@ -2,5 +2,5 @@ console.warn(
   '[@tanstack/router-devtools] This package has moved to @tanstack/react-router-devtools. Please switch to the new package at your earliest convenience, as this package will be dropped in the next major version release.',
 )
 
-export { TanStackRouterDevtools } from '@tanstack/react-router-devtools'
+export { TanStackRouterDevtoolsInProd as TanStackRouterDevtools } from '@tanstack/react-router-devtools'
 export { TanStackRouterDevtoolsPanel } from './TanStackRouterDevtoolsPanel'
