88a45d5289f333da3a3675dbaf3beee48e1a04f1
Birk Skyum <birk.skyum@pm.me>
2025-03-08 20:28:11 +0100

    revert changes

--- packages/router-devtools/src/index.tsx
+++ packages/router-devtools/src/index.tsx
@@ -3,4 +3,4 @@ console.warn(
 )
 
 export { TanStackRouterDevtoolsInProd as TanStackRouterDevtools } from '@tanstack/react-router-devtools'
-// export { TanStackRouterDevtoolsPanelInProd as TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
+export { TanStackRouterDevtoolsPanelInProd as TanStackRouterDevtoolsPanel } from '@tanstack/react-router-devtools'
