0ce42e2d0def46b9fad0ebb622d2b3b9ec09b0bf
Birk Skyum <birk.skyum@pm.me>
2025-03-09 23:13:50 +0100

    examples/react/start-large

--- examples/react/start-large/src/createRoutes.mjs
+++ examples/react/start-large/src/createRoutes.mjs
@@ -4,45 +4,45 @@ import { existsSync } from 'fs'
 const length = 100
 
 const main = async () => {
-  const absolute = (await readFile('./app/routes/absolute.tsx')).toString()
-  const relative = (await readFile('./app/routes/relative.tsx')).toString()
+  const absolute = (await readFile('./src/routes/absolute.tsx')).toString()
+  const relative = (await readFile('./src/routes/relative.tsx')).toString()
   const searchRoute = (
-    await readFile('./app/routes/search/route.tsx')
+    await readFile('./src/routes/search/route.tsx')
   ).toString()
   const search = (
-    await readFile('./app/routes/search/searchPlaceholder.tsx')
+    await readFile('./src/routes/search/searchPlaceholder.tsx')
   ).toString()
   const paramsRoute = (
-    await readFile('./app/routes/params/route.tsx')
+    await readFile('./src/routes/params/route.tsx')
   ).toString()
   const params = await (
-    await readFile('./app/routes/params/$paramsPlaceholder.tsx')
+    await readFile('./src/routes/params/$paramsPlaceholder.tsx')
   ).toString()
 
-  if (!existsSync('./app/routes/(gen)')) {
-    await mkdir('./app/routes/(gen)')
+  if (!existsSync('./src/routes/(gen)')) {
+    await mkdir('./src/routes/(gen)')
   }
 
-  if (!existsSync('./app/routes/(gen)/search')) {
-    await mkdir('./app/routes/(gen)/search')
+  if (!existsSync('./src/routes/(gen)/search')) {
+    await mkdir('./src/routes/(gen)/search')
   }
 
-  if (!existsSync('./app/routes/(gen)/params')) {
-    await mkdir('./app/routes/(gen)/params')
+  if (!existsSync('./src/routes/(gen)/params')) {
+    await mkdir('./src/routes/(gen)/params')
   }
 
-  await writeFile('./app/routes/(gen)/search/route.tsx', searchRoute)
-  await writeFile('./app/routes/(gen)/params/route.tsx', paramsRoute)
+  await writeFile('./src/routes/(gen)/search/route.tsx', searchRoute)
+  await writeFile('./src/routes/(gen)/params/route.tsx', paramsRoute)
 
   for (let y = 0; y < length; y = y + 1) {
     const replacedAbsolute = absolute.replaceAll('/absolute', `/absolute${y}`)
     const replacedRelative = relative.replaceAll('/relative', `/relative${y}`)
     const replacedSearch = search.replaceAll('searchPlaceholder', `search${y}`)
     const replacedParams = params.replaceAll('paramsPlaceholder', `param${y}`)
-    await writeFile(`./app/routes/(gen)/absolute${y}.tsx`, replacedAbsolute)
-    await writeFile(`./app/routes/(gen)/relative${y}.tsx`, replacedRelative)
-    await writeFile(`./app/routes/(gen)/search/search${y}.tsx`, replacedSearch)
-    await writeFile(`./app/routes/(gen)/params/$param${y}.tsx`, replacedParams)
+    await writeFile(`./src/routes/(gen)/absolute${y}.tsx`, replacedAbsolute)
+    await writeFile(`./src/routes/(gen)/relative${y}.tsx`, replacedRelative)
+    await writeFile(`./src/routes/(gen)/search/search${y}.tsx`, replacedSearch)
+    await writeFile(`./src/routes/(gen)/params/$param${y}.tsx`, replacedParams)
   }
 }
 
