afb716a44fc611977c56b3d99fe795bc841525f3
Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-03-09 20:39:43 +1300

    revert(react-router,solid-router): revert #3700 (#3702)
    #3700 introduced an unintended side-effect with the scroll restoration
    process in TanStack Start.
    
    This revets the code changes in `@tanstack/react-router` and
    `@tanstack/solid-router` and short-circuits the tests relevant test
    scripts introduced in the offending PR.

--- packages/solid-router/src/scroll-restoration.tsx
+++ packages/solid-router/src/scroll-restoration.tsx
@@ -7,7 +7,6 @@ import type {
   NonNullableUpdater,
   ParsedLocation,
 } from '@tanstack/router-core'
-import type { RouterHistory } from '@tanstack/history'
 
 export type ScrollRestorationEntry = { scrollX: number; scrollY: number }
 
@@ -94,12 +93,11 @@ let ignoreScroll = false
 // toString() it into a script tag to execute as early as possible in the browser
 // during SSR. Additionally, we also call it from within the router lifecycle
 export function restoreScroll(
-  routerHistory: RouterHistory,
   storageKey: string,
-  key?: string,
-  behavior?: ScrollToOptions['behavior'],
-  shouldScrollRestoration?: boolean,
-  scrollToTopSelectors?: Array<string>,
+  key: string | undefined,
+  behavior: ScrollToOptions['behavior'] | undefined,
+  shouldScrollRestoration: boolean | undefined,
+  scrollToTopSelectors: Array<string> | undefined,
 ) {
   let byKey: ScrollRestorationByKey
 
@@ -145,7 +143,7 @@ export function restoreScroll(
     // Which means we've never seen this location before,
     // we need to check if there is a hash in the URL.
     // If there is, we need to scroll it's ID into view.
-    const hash = routerHistory.location.hash.split('#')[1]
+    const hash = window.location.hash.split('#')[1]
 
     if (hash) {
       const hashScrollIntoViewOptions =
@@ -307,12 +305,11 @@ export function setupScrollRestoration(router: AnyRouter, force?: boolean) {
     }
 
     restoreScroll(
-      router.history,
       storageKey,
       cacheKey,
-      router.options.scrollRestorationBehavior,
-      router.isScrollRestoring,
-      router.options.scrollToTopSelectors,
+      router.options.scrollRestorationBehavior || undefined,
+      router.isScrollRestoring || undefined,
+      router.options.scrollToTopSelectors || undefined,
     )
 
     if (router.isScrollRestoring) {
