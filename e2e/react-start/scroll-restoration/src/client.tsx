18f91ad125e73038e7710c65e34be59ef2f687db
Birk Skyum <birk.skyum@pm.me>
2025-03-10 00:09:14 +0100

    remove unwanted client/ssr files

--- e2e/react-start/scroll-restoration/src/client.tsx
+++
@@ -1,9 +0,0 @@
-// @ts-nocheck
-
-import { hydrateRoot } from 'react-dom/client'
-import { StartClient } from '@tanstack/react-start'
-import { createRouter } from './router'
-
-const router = createRouter()
-
-hydrateRoot(document, <StartClient router={router} />)
