81fdc097afa5ee130e8f8edb985c194456ae6e5d
Birk Skyum <birk.skyum@pm.me>
2025-03-09 22:55:57 +0100

    e2e/react-start/basic

--- e2e/react-start/basic/app.config.ts
+++ e2e/react-start/basic/app.config.ts
@@ -2,6 +2,9 @@ import { defineConfig } from '@tanstack/start/config'
 import tsConfigPaths from 'vite-tsconfig-paths'
 
 export default defineConfig({
+  tsr: {
+    appDirectory: 'src',
+  },
   vite: {
     plugins: [
       tsConfigPaths({
