73af05551ea8c76fc253f1cc874e2d8df85b3940
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2025-03-10 10:40:34 +0100

    refactor(start-client-core): move createServerFn to core (#3727)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/solid-start-client/src/index.tsx
+++ packages/solid-start-client/src/index.tsx
@@ -18,7 +18,7 @@ export {
   type ClientOnlyFn,
   type IsomorphicFnBase,
 } from '@tanstack/start-client-core'
-export { createServerFn } from './createServerFn'
+export { createServerFn } from '@tanstack/start-client-core'
 export {
   type ServerFn as FetchFn,
   type ServerFnCtx as FetchFnCtx,
