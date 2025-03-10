f7c8ed187406aee47d4253ac32df9f0ec8bbc385
Birk Skyum <birk.skyum@pm.me>
2025-03-10 10:32:52 +0100

    refactor(start-client-core): move createServerFn to core

--- packages/start-client-core/src/index.tsx
+++ packages/start-client-core/src/index.tsx
@@ -19,7 +19,10 @@ export {
   type IsomorphicFnBase,
 } from './createIsomorphicFn'
 export { serverOnly, clientOnly } from './envOnly'
-export { type JsonResponse } from './createServerFn'
+export {
+  type JsonResponse,
+  createServerFn,
+} from './createServerFn'
 export { json } from './json'
 export {
   createMiddleware,
