17a4e272fe552fee33e470e3e0f0ad1771575853
Birk Skyum <birk.skyum@pm.me>
2025-03-12 15:44:27 +0100

    fix imports

--- examples/solid/start-basic/src/utils/posts.tsx
+++ examples/solid/start-basic/src/utils/posts.tsx
@@ -1,5 +1,5 @@
 import { notFound } from '@tanstack/solid-router'
-import { createServerFn } from '@tanstack/react-start'
+import { createServerFn } from '@tanstack/solid-start'
 import axios from 'redaxios'
 
 export type PostType = {
