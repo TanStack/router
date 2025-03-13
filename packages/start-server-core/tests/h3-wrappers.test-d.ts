629995879e5257f07bb082dc5f0af9a898a4efb5
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2025-03-13 13:43:23 +0100

    fix(start-server-core): move h3 wrapper tests to shared packages and enable them (#3765)

--- packages/start-server-core/tests/h3-wrappers.test-d.ts
+++ packages/start-server-core/tests/h3-wrappers.test-d.ts
@@ -1,6 +1,6 @@
 import { test } from 'vitest'
 import * as h3 from 'h3'
-import * as tssServer from '../src'
+import * as h3Wrappers from '../src/h3'
 
 interface Foo {
   a: string
@@ -14,7 +14,7 @@ type PrependOverload<
   TOverload extends (...args: Array<any>) => any,
 > = TOverload & TOriginal
 
-type SharedKeys = keyof typeof tssServer & keyof typeof h3
+type SharedKeys = keyof typeof h3Wrappers & keyof typeof h3
 // things we're not wrapping
 type KeysToIgnore = Satisfies<
   | 'H3Error'
@@ -92,259 +92,259 @@ test('wrappers match original', () => {
   expectWrappersToMatchH3({
     readRawBody: {
       h3: h3.readRawBody<'hex'>,
-      tss: tssServer.readRawBody<'hex'>,
+      tss: h3Wrappers.readRawBody<'hex'>,
     },
     readBody: {
       h3: h3.readBody<Foo>,
-      tss: tssServer.readBody<Foo>,
+      tss: h3Wrappers.readBody<Foo>,
     },
     getQuery: {
       h3: h3.getQuery<Foo>,
-      tss: tssServer.getQuery<Foo>,
+      tss: h3Wrappers.getQuery<Foo>,
     },
     isMethod: {
       h3: h3.isMethod,
-      tss: tssServer.isMethod,
+      tss: h3Wrappers.isMethod,
     },
     isPreflightRequest: {
       h3: h3.isPreflightRequest,
-      tss: tssServer.isPreflightRequest,
+      tss: h3Wrappers.isPreflightRequest,
     },
     getValidatedQuery: {
       h3: h3.getValidatedQuery<Foo>,
-      tss: tssServer.getValidatedQuery<Foo>,
+      tss: h3Wrappers.getValidatedQuery<Foo>,
     },
     getRouterParams: {
       h3: h3.getRouterParams,
-      tss: tssServer.getRouterParams,
+      tss: h3Wrappers.getRouterParams,
     },
     getRouterParam: {
       h3: h3.getRouterParam,
-      tss: tssServer.getRouterParam,
+      tss: h3Wrappers.getRouterParam,
     },
     getValidatedRouterParams: {
       h3: h3.getValidatedRouterParams<Foo>,
-      tss: tssServer.getValidatedRouterParams<Foo>,
+      tss: h3Wrappers.getValidatedRouterParams<Foo>,
     },
     assertMethod: {
       h3: h3.assertMethod,
-      tss: tssServer.assertMethod,
+      tss: h3Wrappers.assertMethod,
     },
     getRequestHeaders: {
       h3: h3.getRequestHeaders,
-      tss: tssServer.getRequestHeaders,
+      tss: h3Wrappers.getRequestHeaders,
     },
     getRequestHeader: {
       h3: h3.getRequestHeader,
-      tss: tssServer.getRequestHeader,
+      tss: h3Wrappers.getRequestHeader,
     },
     getRequestURL: {
       h3: h3.getRequestURL,
-      tss: tssServer.getRequestURL,
+      tss: h3Wrappers.getRequestURL,
     },
     getRequestHost: {
       h3: h3.getRequestHost,
-      tss: tssServer.getRequestHost,
+      tss: h3Wrappers.getRequestHost,
     },
     getRequestProtocol: {
       h3: h3.getRequestProtocol,
-      tss: tssServer.getRequestProtocol,
+      tss: h3Wrappers.getRequestProtocol,
     },
     getRequestIP: {
       h3: h3.getRequestIP,
-      tss: tssServer.getRequestIP,
+      tss: h3Wrappers.getRequestIP,
     },
     send: {
       h3: h3.send,
-      tss: tssServer.send,
+      tss: h3Wrappers.send,
     },
     sendNoContent: {
       h3: h3.sendNoContent,
-      tss: tssServer.sendNoContent,
+      tss: h3Wrappers.sendNoContent,
     },
     setResponseStatus: {
       h3: h3.setResponseStatus,
-      tss: tssServer.setResponseStatus,
+      tss: h3Wrappers.setResponseStatus,
     },
     getResponseStatus: {
       h3: h3.getResponseStatus,
-      tss: tssServer.getResponseStatus,
+      tss: h3Wrappers.getResponseStatus,
     },
     getResponseStatusText: {
       h3: h3.getResponseStatusText,
-      tss: tssServer.getResponseStatusText,
+      tss: h3Wrappers.getResponseStatusText,
     },
     getResponseHeaders: {
       h3: h3.getResponseHeaders,
-      tss: tssServer.getResponseHeaders,
+      tss: h3Wrappers.getResponseHeaders,
     },
     getResponseHeader: {
       h3: h3.getResponseHeader,
-      tss: tssServer.getResponseHeader,
+      tss: h3Wrappers.getResponseHeader,
     },
     setResponseHeaders: {
       h3: h3.setResponseHeaders,
-      tss: tssServer.setResponseHeaders,
+      tss: h3Wrappers.setResponseHeaders,
     },
     setResponseHeader: {
       h3: h3.setResponseHeader<'Age'>,
-      tss: tssServer.setResponseHeader<'Age'>,
+      tss: h3Wrappers.setResponseHeader<'Age'>,
     },
     appendResponseHeaders: {
       h3: h3.appendResponseHeaders,
-      tss: tssServer.appendResponseHeaders,
+      tss: h3Wrappers.appendResponseHeaders,
     },
     appendResponseHeader: {
       h3: h3.appendResponseHeader<'Age'>,
-      tss: tssServer.appendResponseHeader<'Age'>,
+      tss: h3Wrappers.appendResponseHeader<'Age'>,
     },
     defaultContentType: {
       h3: h3.defaultContentType,
-      tss: tssServer.defaultContentType,
+      tss: h3Wrappers.defaultContentType,
     },
     sendRedirect: {
       h3: h3.sendRedirect,
-      tss: tssServer.sendRedirect,
+      tss: h3Wrappers.sendRedirect,
     },
     sendStream: {
       h3: h3.sendStream,
-      tss: tssServer.sendStream,
+      tss: h3Wrappers.sendStream,
     },
     writeEarlyHints: {
       h3: h3.writeEarlyHints,
-      tss: tssServer.writeEarlyHints,
+      tss: h3Wrappers.writeEarlyHints,
     },
     sendError: {
       h3: h3.sendError,
-      tss: tssServer.sendError,
+      tss: h3Wrappers.sendError,
     },
     sendProxy: {
       h3: h3.sendProxy,
-      tss: tssServer.sendProxy,
+      tss: h3Wrappers.sendProxy,
     },
     proxyRequest: {
       h3: h3.proxyRequest,
-      tss: tssServer.proxyRequest,
+      tss: h3Wrappers.proxyRequest,
     },
     fetchWithEvent: {
       h3: h3.fetchWithEvent<Foo, Foo, typeof fetch>,
-      tss: tssServer.fetchWithEvent<Foo, Foo, typeof fetch>,
+      tss: h3Wrappers.fetchWithEvent<Foo, Foo, typeof fetch>,
     },
     getProxyRequestHeaders: {
       h3: h3.getProxyRequestHeaders,
-      tss: tssServer.getProxyRequestHeaders,
+      tss: h3Wrappers.getProxyRequestHeaders,
     },
     parseCookies: {
       h3: h3.parseCookies,
-      tss: tssServer.parseCookies,
+      tss: h3Wrappers.parseCookies,
     },
     getCookie: {
       h3: h3.getCookie,
-      tss: tssServer.getCookie,
+      tss: h3Wrappers.getCookie,
     },
     setCookie: {
       h3: h3.setCookie,
-      tss: tssServer.setCookie,
+      tss: h3Wrappers.setCookie,
     },
     deleteCookie: {
       h3: h3.deleteCookie,
-      tss: tssServer.deleteCookie,
+      tss: h3Wrappers.deleteCookie,
     },
     useSession: {
       h3: h3.useSession<Foo>,
-      tss: tssServer.useSession<Foo>,
+      tss: h3Wrappers.useSession<Foo>,
     },
     getSession: {
       h3: h3.getSession<Foo>,
-      tss: tssServer.getSession<Foo>,
+      tss: h3Wrappers.getSession<Foo>,
     },
     updateSession: {
       h3: h3.updateSession<Foo>,
-      tss: tssServer.updateSession<Foo>,
+      tss: h3Wrappers.updateSession<Foo>,
     },
     sealSession: {
       h3: h3.sealSession<Foo>,
-      tss: tssServer.sealSession<Foo>,
+      tss: h3Wrappers.sealSession<Foo>,
     },
     unsealSession: {
       h3: h3.unsealSession,
-      tss: tssServer.unsealSession,
+      tss: h3Wrappers.unsealSession,
     },
     clearSession: {
       h3: h3.clearSession,
-      tss: tssServer.clearSession,
+      tss: h3Wrappers.clearSession,
     },
     handleCacheHeaders: {
       h3: h3.handleCacheHeaders,
-      tss: tssServer.handleCacheHeaders,
+      tss: h3Wrappers.handleCacheHeaders,
     },
     handleCors: {
       h3: h3.handleCors,
-      tss: tssServer.handleCors,
+      tss: h3Wrappers.handleCors,
     },
     appendCorsHeaders: {
       h3: h3.appendCorsHeaders,
-      tss: tssServer.appendCorsHeaders,
+      tss: h3Wrappers.appendCorsHeaders,
     },
     appendCorsPreflightHeaders: {
       h3: h3.appendCorsPreflightHeaders,
-      tss: tssServer.appendCorsPreflightHeaders,
+      tss: h3Wrappers.appendCorsPreflightHeaders,
     },
     sendWebResponse: {
       h3: h3.sendWebResponse,
-      tss: tssServer.sendWebResponse,
+      tss: h3Wrappers.sendWebResponse,
     },
     appendHeader: {
       h3: h3.appendHeader<'Age'>,
-      tss: tssServer.appendHeader<'Age'>,
+      tss: h3Wrappers.appendHeader<'Age'>,
     },
     appendHeaders: {
       h3: h3.appendHeaders,
-      tss: tssServer.appendHeaders,
+      tss: h3Wrappers.appendHeaders,
     },
     setHeader: {
       h3: h3.setHeader<'Age'>,
-      tss: tssServer.setHeader<'Age'>,
+      tss: h3Wrappers.setHeader<'Age'>,
     },
     setHeaders: {
       h3: h3.setHeaders,
-      tss: tssServer.setHeaders,
+      tss: h3Wrappers.setHeaders,
     },
     getHeader: {
       h3: h3.getHeader,
-      tss: tssServer.getHeader,
+      tss: h3Wrappers.getHeader,
     },
     getHeaders: {
       h3: h3.getHeaders,
-      tss: tssServer.getHeaders,
+      tss: h3Wrappers.getHeaders,
     },
     getRequestFingerprint: {
       h3: h3.getRequestFingerprint,
-      tss: tssServer.getRequestFingerprint,
+      tss: h3Wrappers.getRequestFingerprint,
     },
     getRequestWebStream: {
       h3: h3.getRequestWebStream,
-      tss: tssServer.getRequestWebStream,
+      tss: h3Wrappers.getRequestWebStream,
     },
     readFormData: {
       h3: h3.readFormData,
-      tss: tssServer.readFormData,
+      tss: h3Wrappers.readFormData,
     },
     readMultipartFormData: {
       h3: h3.readMultipartFormData,
-      tss: tssServer.readMultipartFormData,
+      tss: h3Wrappers.readMultipartFormData,
     },
     readValidatedBody: {
       h3: h3.readValidatedBody<Foo>,
-      tss: tssServer.readValidatedBody<Foo>,
+      tss: h3Wrappers.readValidatedBody<Foo>,
     },
     removeResponseHeader: {
       h3: h3.removeResponseHeader,
-      tss: tssServer.removeResponseHeader,
+      tss: h3Wrappers.removeResponseHeader,
     },
     clearResponseHeaders: {
       h3: h3.clearResponseHeaders,
-      tss: tssServer.clearResponseHeaders,
+      tss: h3Wrappers.clearResponseHeaders,
     },
   })
 })
