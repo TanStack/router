TanStack/router

commit 418fd958276c4f4b315af1966c10eca1ec79b5c5

fix(history): `parseLocation` in createHashHistory to handle search params correctly (#3644)
`parseLocation` in `createHashHistory` failed to parse search params when they appeared before the hash. This PR updates the parsing logic so that search params are correctly extracted and processed, regardless of their position.
fixes #3595

packages/history/src/index.ts
@@ -546,7 +546,9 @@ export function createHashHistory(opts?: { window?: any }): RouterHistory {
   return createBrowserHistory({
     window: win,
     parseLocation: () => {
-      const hashHref = win.location.hash.split('#').slice(1).join('#') ?? '/'
+      const search = win.location.search
+      const pathname = win.location.hash.substring(1)
+      const hashHref = search ? `/${pathname}${search}` : `/${pathname}`
       return parseHref(hashHref, win.history.state)
     },
     createHref: (href) =>

packages/history/tests/createHashHistory.test.ts
@@ -1,0 +1,44 @@
+import { describe, expect, test } from 'vitest'
+
+import { createHashHistory } from '../src'
+
+describe('createHashHistory', () => {
+  describe('parseLocation', () => {
+    describe.each([
+      ['/', { pathname: '/', search: '' }, 'neither search params nor hash'],
+      [
+        '/#hello',
+        { pathname: '/hello', search: '' },
+        'hash present, no search params',
+      ],
+      [
+        '/?search=params',
+        { pathname: '/', search: '?search=params' },
+        'search params present, no hash',
+      ],
+      [
+        '/#hello?search=params',
+        { pathname: '/hello', search: '?search=params' },
+        'both hash and search params present, in that order',
+      ],
+      [
+        '/?search=params#hello',
+        { pathname: '/hello', search: '?search=params' },
+        'both search params and hash present, in that order',
+      ],
+    ])('check for %s', (...[path, exp, desc]) => {
+      test(`onLoad with ${path} (${desc})`, () => {
+        window.history.replaceState({}, '', path)
+        const history = createHashHistory()
+        expect(history.location.pathname).toBe(exp.pathname)
+        expect(history.location.search).toBe(exp.search)
+      })
+      test(`onNavigate with ${path} (${desc})`, () => {
+        const history = createHashHistory()
+        window.history.pushState({}, '', path)
+        expect(history.location.pathname).toBe(exp.pathname)
+        expect(history.location.search).toBe(exp.search)
+      })
+    })
+  })
+})

commit 185a324061a1efa641161c811f4a5c3f8073931f

fix(history): Fix double-slash regression introduced by #3644
This more cleanly and safely constructs a href with search parameters and prevents incorrectly adding a double slash to the start of the URL.

Fixes #3739
Fixes #3699

packages/history/src/index.ts
@@ -546,9 +546,12 @@ export function createHashHistory(opts?: { window?: any }): RouterHistory {
   return createBrowserHistory({
     window: win,
     parseLocation: () => {
-      const search = win.location.search
-      const pathname = win.location.hash.substring(1)
-      const hashHref = search ? `/${pathname}${search}` : `/${pathname}`
+      const hashSplit = win.location.hash.split("#").slice(1)
+      const pathPart = hashSplit[0] ?? "/"
+      const searchPart = win.location.search
+      const hashEntries = hashSplit.slice(1)
+      const hashPart = hashEntries.length === 0 ? "" : `#${hashEntries.join("#")}`
+      const hashHref = `${pathPart}${searchPart}${hashPart}`
       return parseHref(hashHref, win.history.state)
     },
     createHref: (href) =>