6ad6ee18010c0b0f2d06014ff713ef6d1e1138f2
Thibaut Rousseau <contact@thibaut-rousseau.com>
2025-03-17 00:13:30 +0100

    refactor(router-core): decorate with JSDoc comments for `NavigateOptionProps`, `ActiveOptions`, `MatchRouteOptions` and `Redirect` (#3792)
    Following up on https://github.com/TanStack/router/discussions/3790 I added some JSDoc on a few uncommented types I encountered in my project.
    
    ---------
    
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- packages/router-core/src/redirect.ts
+++ packages/router-core/src/redirect.ts
@@ -5,6 +5,9 @@ import type { PickAsRequired } from './utils'
 
 export type AnyRedirect = Redirect<any, any, any, any, any>
 
+/**
+ * @link [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RedirectType)
+ */
 export type Redirect<
   TRouter extends AnyRouter = RegisteredRouter,
   TFrom extends RoutePaths<TRouter['routeTree']> | string = '/',
@@ -17,8 +20,20 @@ export type Redirect<
    * @deprecated Use `statusCode` instead
    **/
   code?: number
+  /**
+   * The HTTP status code to use when redirecting.
+   * @link [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RedirectType#statuscode-property)
+   */
   statusCode?: number
+  /**
+   * If provided, will throw the redirect object instead of returning it. This can be useful in places where `throwing` in a function might cause it to have a return type of `never`. In that case, you can use `redirect({ throw: true })` to throw the redirect object instead of returning it.
+   * @link [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RedirectType#throw-property)
+   */
   throw?: any
+  /**
+   * The HTTP headers to use when redirecting.
+   * @link [API Docs](https://tanstack.com/router/latest/docs/framework/react/api/router/RedirectType#headers-property)
+   */
   headers?: HeadersInit
 } & NavigateOptions<TRouter, TFrom, TTo, TMaskFrom, TMaskTo>
 
