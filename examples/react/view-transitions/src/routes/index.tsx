a9aabd9b898bcc96bbd04785d9ae5aa2fb7f97b4
alex-delia <adelia3@uwo.ca>
2025-03-16 15:22:40 -0500

    docs: add view-transitions example

---
+++ examples/react/view-transitions/src/routes/index.tsx
@@ -0,0 +1,23 @@
+import * as React from 'react';
+import { Link, createFileRoute } from '@tanstack/react-router';
+
+export const Route = createFileRoute('/')({
+  component: Home,
+});
+
+function Home() {
+  return (
+    <div className="p-2 [view-transition-name:main-content]">
+      <h3>Welcome To The View Transitions Example!</h3>
+      <div className="flex justify-center mt-4">
+        <Link
+          to={'/how-it-works'}
+          viewTransition={{ types: ['slide-left'] }}
+          className="font-bold"
+        >
+          Next Page -&gt;
+        </Link>
+      </div>
+    </div>
+  );
+}
