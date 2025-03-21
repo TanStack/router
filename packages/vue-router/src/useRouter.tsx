4e2646951f40aeee5cef7b45d2db9d2e56b522fe
Birk Skyum <birk.skyum@pm.me>
2025-03-21 12:45:26 +0100

    use getRouterContext

--- packages/vue-router/src/useRouter.tsx
+++ packages/vue-router/src/useRouter.tsx
@@ -1,19 +1,15 @@
+import * as Vue from 'vue'
 import warning from 'tiny-warning'
-import { injectRouter } from './routerContext'
+import { getRouterContext } from './routerContext'
 import type { AnyRouter, RegisteredRouter } from '@tanstack/router-core'
 
 export function useRouter<TRouter extends AnyRouter = RegisteredRouter>(opts?: {
   warn?: boolean
 }): TRouter {
-  try {
-    return injectRouter() as TRouter
-  } catch (err) {
-    if (opts?.warn ?? true) {
-      warning(
-        false,
-        'useRouter must be used inside a <RouterProvider> component!',
-      )
-    }
-    return null as unknown as TRouter
-  }
+  const value = Vue.inject(getRouterContext() as any, null)
+  warning(
+    !((opts?.warn ?? true) && !value),
+    'useRouter must be used inside a <RouterProvider> component!',
+  )
+  return value as any
 }
