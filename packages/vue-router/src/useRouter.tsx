e2f113dc2b0c6c21c24cd58413067404a45c33b0
Birk Skyum <birk.skyum@pm.me>
2025-03-21 11:50:07 +0100

    align vue to solid/react

--- packages/vue-router/src/useRouter.tsx
+++ packages/vue-router/src/useRouter.tsx
@@ -6,18 +6,10 @@ import type { AnyRouter, RegisteredRouter } from '@tanstack/router-core'
 export function useRouter<TRouter extends AnyRouter = RegisteredRouter>(opts?: {
   warn?: boolean
 }): TRouter {
-  try {
-    const value = Vue.inject(getRouterContext() as any, null)
-    warning(
-      !((opts?.warn ?? true) && !value),
-      'useRouter must be used inside a <RouterProvider> component!',
-    )
-    return value as any
-  } catch (err) {
-    warning(
-      opts?.warn ?? true,
-      'Error injecting router context. Make sure useRouter is used inside a <RouterProvider> component!',
-    )
-    return null as any
-  }
+  const value = Vue.inject(getRouterContext() as any, null)
+  warning(
+    !((opts?.warn ?? true) && !value),
+    'useRouter must be used inside a <RouterProvider> component!',
+  )
+  return value as any
 }
