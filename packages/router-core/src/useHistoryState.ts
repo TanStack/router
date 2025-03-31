cf7ac4d3f1f0af23f7edf4967a3cb340dc16ae5b
naoya7076 <naoya7076@gmail.com>
2025-03-31 09:46:20 +0900

    refactor(router): delete unused type

--- packages/router-core/src/useHistoryState.ts
+++ packages/router-core/src/useHistoryState.ts
@@ -1,18 +1,6 @@
-import type { RouteById, RouteIds } from './routeInfo'
-import type { AnyRouter, RegisteredRouter } from './router'
-import type { Constrain, Expand } from './utils'
-
-export type UseHistoryStateOptions<
-  TRouter extends AnyRouter,
-  TFrom extends string | undefined,
-  TStrict extends boolean,
-  TState,
-  TSelected,
-> = {
-  select?: (state: TState) => TSelected
-  from?: Constrain<TFrom, RouteIds<TRouter['routeTree']>>
-  strict?: TStrict
-}
+import type { RouteById } from './routeInfo'
+import type { AnyRouter } from './router'
+import type { Expand } from './utils'
 
 export type UseHistoryStateResult<
   TRouter extends AnyRouter,
@@ -30,14 +18,3 @@ export type ResolveUseHistoryState<
 > = TStrict extends false
   ? Expand<Partial<Record<string, unknown>>>
   : Expand<RouteById<TRouter['routeTree'], TFrom>['types']['fullStateSchema']>
-
-export type UseHistoryStateRoute<out TFrom> = <
-  TRouter extends AnyRouter = RegisteredRouter,
-  TSelected = RouteById<TRouter['routeTree'], TFrom>['types']['stateSchema'],
-  TStructuralSharing extends boolean = boolean,
->(opts?: {
-  select?: (
-    state: RouteById<TRouter['routeTree'], TFrom>['types']['stateSchema'],
-  ) => TSelected
-  structuralSharing?: TStructuralSharing
-}) => UseHistoryStateResult<TRouter, TFrom, true, TSelected>
