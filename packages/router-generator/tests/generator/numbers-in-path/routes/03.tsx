dd51b04ac2cdf275953b5eba572e94acd8cdadf4
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-04-04 14:26:19 +1300

    test(router-generator): update snapshots for the numbers in filename test cases

--- packages/router-generator/tests/generator/numbers-in-path/routes/03.tsx
+++ packages/router-generator/tests/generator/numbers-in-path/routes/03.tsx
@@ -1,6 +1,6 @@
-import { createFileRoute } from '@tanstack/react-router'
 
-export const Route = createFileRoute('/03')({
+
+export const Route = createFileRoute({
   component: RouteComponent,
 })
 
