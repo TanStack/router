78e03f236e3691d2e51da156514e1f7c84624772
Brandon Roberts <robertsbt@gmail.com>
2025-04-07 22:26:48 -0500

    chore: use ng-packagr for the build

--- packages/angular-router/vite.config.ts
+++ packages/angular-router/vite.config.ts
@@ -1,33 +1,13 @@
-import { defineConfig, mergeConfig } from 'vitest/config'
-import { tanstackViteConfig } from '@tanstack/config/vite'
-import angular from '@analogjs/vite-plugin-angular'
+import { defineConfig } from 'vitest/config'
 import packageJson from './package.json'
-import type { UserConfig } from 'vitest/config'
 
-const isTest = process.env['NODE_ENV'] === 'test';
-
-const config = defineConfig({
-  root: __dirname,
-  plugins: [
-    angular({ tsconfig: `${__dirname}/tsconfig.${isTest ? 'spec' : 'lib.prod' }.json`}),
-  ] as UserConfig['plugins'],
+export default defineConfig({
   test: {
-    globals: true,
     name: packageJson.name,
     dir: './tests',
     watch: false,
     environment: 'jsdom',
     setupFiles: ['./tests/setup-tests.ts'],
+    globals: true,
   },
 })
-
-export default mergeConfig(
-  config,
-  tanstackViteConfig({
-    tsconfigPath: 'tsconfig.lib.prod.json',
-    outDir: 'dist/fesm2022',
-    entry: './src/index.ts',
-    srcDir: './src',
-    cjs: false
-  }),
-)
