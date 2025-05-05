86fc5cb13f1f8f1a42380a6a2880e4703538e2d9
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-06 09:47:43 +1200

    switch e2e code-based tests to use object syntax

--- e2e/react-router/basic/tests/params.spec.ts
+++ e2e/react-router/basic/tests/params.spec.ts
@@ -8,14 +8,30 @@ test.describe('params operations + prefix/suffix', () => {
   test.describe('named params', () => {
     const NAMED_PARAMS_PAIRS = [
       // Test ID | Expected href
-      ['l-to-named-foo', '/params/named/foo', { foo: 'foo' }],
-      ['l-to-named-prefixfoo', '/params/named/prefixfoo', { foo: 'foo' }],
-      ['l-to-named-foosuffix', '/params/named/foosuffix', { foo: 'foo' }],
-    ] satisfies Array<[string, string, Record<string, string>]>
+      {
+        id: 'l-to-named-foo',
+        pathname: '/params/named/foo',
+        params: { foo: 'foo' },
+      },
+      {
+        id: 'l-to-named-prefixfoo',
+        pathname: '/params/named/prefixfoo',
+        params: { foo: 'foo' },
+      },
+      {
+        id: 'l-to-named-foosuffix',
+        pathname: '/params/named/foosuffix',
+        params: { foo: 'foo' },
+      },
+    ] satisfies Array<{
+      id: string
+      pathname: string
+      params: Record<string, string>
+    }>
 
     test.describe('Link', () => {
-      NAMED_PARAMS_PAIRS.forEach(([id, pathname]) => {
-        test(`interpolation for testid=${id} has href=${pathname}`, async ({
+      NAMED_PARAMS_PAIRS.forEach(({ id, pathname }) => {
+        test(`interpolation for testid="${id}" has href="${pathname}"`, async ({
           page,
         }) => {
           const link = page.getByTestId(id)
@@ -23,8 +39,8 @@ test.describe('params operations + prefix/suffix', () => {
         })
       })
 
-      NAMED_PARAMS_PAIRS.forEach(([id, pathname]) => {
-        test(`navigation for testid=${id} succeeds to href=${pathname}`, async ({
+      NAMED_PARAMS_PAIRS.forEach(({ id, pathname }) => {
+        test(`navigation for testid="${id}" succeeds to href="${pathname}"`, async ({
           page,
         }) => {
           const link = page.getByTestId(id)
@@ -36,8 +52,8 @@ test.describe('params operations + prefix/suffix', () => {
       })
     })
 
-    NAMED_PARAMS_PAIRS.forEach(([_, pathname, params]) => {
-      test(`on first-load to ${pathname} has correct params`, async ({
+    NAMED_PARAMS_PAIRS.forEach(({ pathname, params }) => {
+      test(`on first-load to "${pathname}" has correct params`, async ({
         page,
       }) => {
         await page.goto(pathname)
@@ -56,26 +72,30 @@ test.describe('params operations + prefix/suffix', () => {
   test.describe('wildcard param', () => {
     const WILDCARD_PARAM_PAIRS = [
       // Test ID | Expected href
-      [
-        'l-to-wildcard-foo',
-        '/params/wildcard/foo',
-        { '*': 'foo', _splat: 'foo' },
-      ],
-      [
-        'l-to-wildcard-prefixfoo',
-        '/params/wildcard/prefixfoo',
-        { '*': 'foo', _splat: 'foo' },
-      ],
-      [
-        'l-to-wildcard-foosuffix',
-        '/params/wildcard/foosuffix',
-        { '*': 'foo', _splat: 'foo' },
-      ],
-    ] satisfies Array<[string, string, Record<string, string>]>
+      {
+        id: 'l-to-wildcard-foo',
+        pathname: '/params/wildcard/foo',
+        params: { '*': 'foo', _splat: 'foo' },
+      },
+      {
+        id: 'l-to-wildcard-prefixfoo',
+        pathname: '/params/wildcard/prefixfoo',
+        params: { '*': 'foo', _splat: 'foo' },
+      },
+      {
+        id: 'l-to-wildcard-foosuffix',
+        pathname: '/params/wildcard/foosuffix',
+        params: { '*': 'foo', _splat: 'foo' },
+      },
+    ] satisfies Array<{
+      id: string
+      pathname: string
+      params: Record<string, string>
+    }>
 
     test.describe('Link', () => {
-      WILDCARD_PARAM_PAIRS.forEach(([id, pathname]) => {
-        test(`interpolation for testid=${id} has href=${pathname}`, async ({
+      WILDCARD_PARAM_PAIRS.forEach(({ id, pathname }) => {
+        test(`interpolation for testid="${id}" has href="${pathname}"`, async ({
           page,
         }) => {
           const link = page.getByTestId(id)
@@ -83,8 +103,8 @@ test.describe('params operations + prefix/suffix', () => {
         })
       })
 
-      WILDCARD_PARAM_PAIRS.forEach(([id, pathname]) => {
-        test(`navigation for testid=${id} succeeds to href=${pathname}`, async ({
+      WILDCARD_PARAM_PAIRS.forEach(({ id, pathname }) => {
+        test(`navigation for testid="${id}" succeeds to href="${pathname}"`, async ({
           page,
         }) => {
           const link = page.getByTestId(id)
@@ -96,8 +116,8 @@ test.describe('params operations + prefix/suffix', () => {
       })
     })
 
-    WILDCARD_PARAM_PAIRS.forEach(([_, pathname, params]) => {
-      test(`on first-load to ${pathname} has correct params`, async ({
+    WILDCARD_PARAM_PAIRS.forEach(({ pathname, params }) => {
+      test(`on first-load to "${pathname}" has correct params`, async ({
         page,
       }) => {
         await page.goto(pathname)
