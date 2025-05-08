1d87237a8e32401bf59c9f332aff7e120784f792
Manuel Schiller <manuel.schiller@caligano.de>
2025-05-09 01:27:09 +0200

    upgrade start-bare example

---
+++ examples/react/start-bare/vite.config.ts
@@ -0,0 +1,15 @@
+import { TanStackStartVitePlugin } from '@tanstack/react-start/plugin'
+import { defineConfig } from 'vite'
+import tsConfigPaths from 'vite-tsconfig-paths'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+  },
+  plugins: [
+    tsConfigPaths({
+      projects: ['./tsconfig.json'],
+    }),
+    TanStackStartVitePlugin(),
+  ],
+})
