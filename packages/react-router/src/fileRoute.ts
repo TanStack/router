f6a1b1c671570c470c5b3e096366ed10c42f9dcc
Manuel Schiller <manuel.schiller@caligano.de>
2025-05-09 22:18:43 +0200

    fix runtime of createLazyFileRoute for usage without id

--- packages/react-router/src/fileRoute.ts
+++ packages/react-router/src/fileRoute.ts
@@ -241,6 +241,10 @@ export function createLazyRoute<
 export function createLazyFileRoute<
   TFilePath extends keyof FileRoutesByPath,
   TRoute extends FileRoutesByPath[TFilePath]['preLoaderRoute'],
->(id: TFilePath) {
+>(id: TFilePath) : (opts: LazyRouteOptions) => LazyRoute<TRoute> {
+  if (typeof id === 'object') {
+    return new LazyRoute<TRoute>(id) as any
+  }
+
   return (opts: LazyRouteOptions) => new LazyRoute<TRoute>({ id, ...opts })
 }
