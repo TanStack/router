3d1323136dd8f55a8595e3624868b2eb2c295519
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-09 16:04:14 +1200

    sync e2e:solid-start/website

--- e2e/solid-start/website/src/routes/$project.$version.docs.index.tsx
+++ e2e/solid-start/website/src/routes/$project.$version.docs.index.tsx
@@ -1,7 +1,7 @@
 import { redirect } from '@tanstack/solid-router'
 
 export const Route = createFileRoute({
-  loader: () => {
+  beforeLoad: () => {
     throw redirect({
       from: '/$project/$version/docs',
       to: '/$project/$version/docs/framework/$framework/$',
