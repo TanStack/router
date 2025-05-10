fdc48d82f55c7152a60357da32afc2a810acb317
Brenley Dueck <brenleydueck@gmail.com>
2025-05-10 12:22:17 -0500

    chore: fix build

--- e2e/react-start/basic/src/utils/test-env.tsx
+++ e2e/react-start/basic/src/utils/test-env.tsx
@@ -1,4 +1,4 @@
-import { createServerFn } from '@tanstack/start'
+import { createServerFn } from '@tanstack/react-start'
 
 export const fetchDatabaseUrl = createServerFn({ method: 'GET' }).handler(
   () => {
