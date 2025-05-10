4a66b31a187d4c7b362ab67f2818a535d1e2de9e
Brenley Dueck <brenley@crowdbotics.com>
2025-05-10 13:25:03 -0500

    chore: fix test name

--- e2e/react-start/basic/tests/env-variable.spec.ts
+++ e2e/react-start/basic/tests/env-variable.spec.ts
@@ -1,6 +1,6 @@
 import { expect, test } from '@playwright/test'
 
-test('Navigating to post', async ({ page }) => {
+test('Ability to access env variable', async ({ page }) => {
   await page.goto('/')
 
   await expect(page.getByText('DATABASE_URL: test123')).toBeVisible()
