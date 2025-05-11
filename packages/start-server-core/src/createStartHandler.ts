a9e4f402e0fe1b5b19ad468e731f55e1e272fd68
MoeedDar <moeed.dar@gmail.com>
2025-05-11 21:12:56 +0100

    comment fix

--- packages/start-server-core/src/createStartHandler.ts
+++ packages/start-server-core/src/createStartHandler.ts
@@ -81,7 +81,7 @@ export function createStartHandler<TRouter extends AnyRouter>({
           }
 
           // First, let's attempt to handle server functions
-          // Add trailing slash to sanitise user defined TSS_SERVER_FN
+          // Add trailing slash to sanitise user defined TSS_SERVER_FN_BASE
           const serverFnBase = process.env.TSS_SERVER_FN_BASE.startsWith("/")
             ? process.env.TSS_SERVER_FN_BASE
             : "/" + process.env.TSS_SERVER_FN_BASE;
@@ -209,7 +209,7 @@ export function createStartHandler<TRouter extends AnyRouter>({
       const body = response.status === 204 ? null : (await response.clone().blob());
       const headers = new Headers(response.headers);
       headers.append("Access-Control-Expose-Headers", tsrRedirectHeaderKey);
-      
+
       return new Response(body, {
         status: response.status,
         statusText: response.statusText,
