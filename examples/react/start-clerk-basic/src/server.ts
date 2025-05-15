bd05a7df47d654aa697ebe34c69d618640f0f98f
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-15 15:29:21 +1200

    chore: fix clerk example

--- examples/react/start-clerk-basic/src/server.ts
+++ examples/react/start-clerk-basic/src/server.ts
@@ -2,15 +2,14 @@ import {
   createStartHandler,
   defaultStreamHandler,
 } from '@tanstack/react-start/server'
-import { getRouterManifest } from '@tanstack/react-start/router-manifest'
 import { createClerkHandler } from '@clerk/tanstack-react-start/server'
 import { createRouter } from './router'
 
 const handler = createStartHandler({
   createRouter,
-  getRouterManifest,
 })
 
+// @ts-expect-error
 const clerkHandler = createClerkHandler(handler)
 
 export default clerkHandler(defaultStreamHandler)
