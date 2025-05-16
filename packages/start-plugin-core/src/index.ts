30863dad8d4646cf75bc11f9ee2fa6866a51abd9
Manuel Schiller <manuel.schiller@caligano.de>
2025-05-16 19:42:41 +0200

    export less from start-plugin-core

--- packages/start-plugin-core/src/index.ts
+++ packages/start-plugin-core/src/index.ts
@@ -1,10 +1,3 @@
-export type { CompileOptions, IdentifierConfig } from './compilers'
-
-export { compileStartOutputFactory } from './compilers'
-
-export type { PagesJson } from './nitro/build-sitemap'
-export { buildSitemap } from './nitro/build-sitemap'
-
 export {
   createTanStackConfig,
   createTanStackStartOptionsSchema,
