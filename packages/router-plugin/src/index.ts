5d5ab2e98b85f322a41cfab4009cc74f8a25d3ad
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-21 20:09:52 +1200

    feat(router-plugin): expose `getConfig`

--- packages/router-plugin/src/index.ts
+++ packages/router-plugin/src/index.ts
@@ -1,4 +1,4 @@
-export { configSchema } from './core/config'
+export { configSchema, getConfig } from './core/config'
 export { unpluginRouterCodeSplitterFactory } from './core/router-code-splitter-plugin'
 export { unpluginRouterGeneratorFactory } from './core/router-generator-plugin'
 export type { Config, ConfigInput, ConfigOutput } from './core/config'
