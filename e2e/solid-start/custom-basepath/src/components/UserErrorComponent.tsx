150c371dbe1e0c13e8ded03dcd628f7b8f418486
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-05-27 19:58:53 +1200

    cleanup `custom-basepath` sandboxes

--- e2e/solid-start/custom-basepath/src/components/UserErrorComponent.tsx
+++ e2e/solid-start/custom-basepath/src/components/UserErrorComponent.tsx
@@ -1,4 +1,5 @@
-import { ErrorComponent, ErrorComponentProps } from '@tanstack/solid-router'
+import { ErrorComponent } from '@tanstack/solid-router'
+import type { ErrorComponentProps } from '@tanstack/solid-router'
 
 export function UserErrorComponent({ error }: ErrorComponentProps) {
   return <ErrorComponent error={error} />
