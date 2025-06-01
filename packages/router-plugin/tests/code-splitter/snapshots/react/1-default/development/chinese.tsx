7c2bc2f334d2100be45a5179799bd3aeff6f6f4a
Manuel Schiller <manuel.schiller@caligano.de>
2025-06-01 18:14:56 +0200

    fix: make route HMR handling robust against route deletion (#4291)
    fixes #4289

--- packages/router-plugin/tests/code-splitter/snapshots/react/1-default/development/chinese.tsx
+++ packages/router-plugin/tests/code-splitter/snapshots/react/1-default/development/chinese.tsx
@@ -10,7 +10,7 @@ interface DemoProps {
 }
 if (import.meta.hot) {
   import.meta.hot.accept(newModule => {
-    if (newModule.Route && typeof newModule.Route.clone === 'function') {
+    if (newModule && newModule.Route && typeof newModule.Route.clone === 'function') {
       newModule.Route.clone(Route);
     }
   });
