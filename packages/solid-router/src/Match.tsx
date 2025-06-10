0245d3734c5a3b835e43a48f6021c08f07d6d651
naoya7076 <naoya7076@gmail.com>
2025-06-10 22:28:24 +0900

    Rename 'key' in markdown and comments to __key

--- packages/solid-router/src/Match.tsx
+++ packages/solid-router/src/Match.tsx
@@ -131,7 +131,7 @@ export const Match = (props: { matchId: string }) => {
 // On Rendered can't happen above the root layout because it actually
 // renders a dummy dom element to track the rendered state of the app.
 // We render a script tag with a key that changes based on the current
-// location state.key. Also, because it's below the root layout, it
+// location state.__key. Also, because it's below the root layout, it
 // allows us to fire onRendered events even after a hydration mismatch
 // error that occurred above the root layout (like bad head/link tags,
 // which is common).
