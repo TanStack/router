c92920dadab0fd4568432c3d445d0c48cc25e8fc
SeanCassiere <33615041+SeanCassiere@users.noreply.github.com>
2025-06-13 21:47:47 +1200

    add for solid

--- e2e/solid-router/basic-file-based-code-splitting/vite.config.ts
+++ e2e/solid-router/basic-file-based-code-splitting/vite.config.ts
@@ -2,13 +2,16 @@ import { defineConfig } from 'vite'
 import solid from 'vite-plugin-solid'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
 
+const isVerboseFileRoutes = process.env.VERBOSE_FILE_ROUTES === '1' || false
+console.info(`Verbose file routes is set to: ${isVerboseFileRoutes}.`)
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
     tanstackRouter({
       target: 'solid',
       autoCodeSplitting: true,
-      verboseFileRoutes: false,
+      verboseFileRoutes: isVerboseFileRoutes,
       codeSplittingOptions: {
         splitBehavior: ({ routeId }) => {
           if (routeId === '/posts') {
