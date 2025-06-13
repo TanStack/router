09f0b6dcb0e18a33c73b0bf066ca362af8c411b8
wookhyung <qpflapffhs76@gmail.com>
2025-06-13 14:45:52 +0900

    refactor: simplify script handling in Asset component

--- packages/solid-router/src/Asset.tsx
+++ packages/solid-router/src/Asset.tsx
@@ -1,6 +1,4 @@
 import { Meta, Style, Title } from '@solidjs/meta'
-import { createEffect, onCleanup } from 'solid-js'
-import { useRouter } from './useRouter'
 import type { RouterManagedTag } from '@tanstack/router-core'
 
 export function Asset({ tag, attrs, children }: RouterManagedTag): any {
@@ -14,58 +12,13 @@ export function Asset({ tag, attrs, children }: RouterManagedTag): any {
     case 'style':
       return <Style {...attrs} innerHTML={children} />
     case 'script':
-      return <ScriptAsset attrs={attrs} children={children} />
+      if (attrs?.src) {
+        return <script {...attrs} />
+      }
+      if (typeof children === 'string')
+        return <script {...attrs} innerHTML={children} />
+      return null
     default:
       return null
   }
-}
-
-function ScriptAsset({ attrs, children }: { attrs: any; children?: string }) {
-  const router = useRouter()
-
-  if (router.isServer) {
-    if (attrs?.src) {
-      return <script {...attrs} />
-    } else if (typeof children === 'string') {
-      return <script {...attrs} innerHTML={children} />
-    }
-    return null
-  }
-
-  createEffect(() => {
-    if (attrs?.src) {
-      const script = document.createElement('script')
-
-      Object.keys(attrs).forEach((key) => {
-        script.setAttribute(key, attrs[key])
-      })
-
-      document.head.appendChild(script)
-
-      onCleanup(() => {
-        if (script.parentNode) {
-          script.parentNode.removeChild(script)
-        }
-      })
-    } else if (typeof children === 'string') {
-      const script = document.createElement('script')
-      script.textContent = children
-
-      if (attrs) {
-        Object.keys(attrs).forEach((key) => {
-          script.setAttribute(key, attrs[key])
-        })
-      }
-
-      document.head.appendChild(script)
-
-      onCleanup(() => {
-        if (script.parentNode) {
-          script.parentNode.removeChild(script)
-        }
-      })
-    }
-  })
-
-  return null
 }
