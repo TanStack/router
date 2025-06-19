5cb9b77b6a8de5c184aa733a348c3541fd586604
Jan Vincent Liwanag <jvliwanag@gmail.com>
2025-06-19 15:36:08 +0800

    fix: export RedirectOptions type

--- packages/router-core/src/index.ts
+++ packages/router-core/src/index.ts
@@ -372,7 +372,12 @@ export type { UseLoaderDepsResult, ResolveUseLoaderDeps } from './useLoaderDeps'
 
 export type { UseLoaderDataResult, ResolveUseLoaderData } from './useLoaderData'
 
-export type { Redirect, ResolvedRedirect, AnyRedirect } from './redirect'
+export type {
+  Redirect,
+  RedirectOptions,
+  ResolvedRedirect,
+  AnyRedirect,
+} from './redirect'
 
 export {
   redirect,
