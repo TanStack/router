62c0fd3a10c476e3378772fea21cc2ac0555b36d
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-06-21 16:20:27 +0200

    create router-core-ssr package

---
+++ packages/router-core-ssr/vite.config.ts
@@ -0,0 +1,25 @@
+import { defineConfig, mergeConfig  } from 'vitest/config'
+import { tanstackViteConfig } from '@tanstack/config/vite'
+import packageJson from './package.json'
+import minifyScriptPlugin from './vite-minify-plugin'
+import type {ViteUserConfig} from 'vitest/config';
+
+const config = defineConfig({
+  plugins: [minifyScriptPlugin()] as ViteUserConfig['plugins'],
+  test: {
+    name: packageJson.name,
+    watch: false,
+    environment: 'jsdom',
+  },
+})
+
+export default mergeConfig(
+  config,
+  tanstackViteConfig({
+    srcDir: './src',
+    entry: [
+      './src/client/index.ts',
+      './src/server/index.ts'
+    ],
+  }),
+)
