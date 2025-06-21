c738087349478c9d8ac6087419954144f84f7de0
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-06-21 17:27:30 +0200

    re-export client and server functions from router-core-ssr

--- packages/react-router-ssr/src/client/index.ts
+++ packages/react-router-ssr/src/client/index.ts
@@ -1,1 +1,2 @@
 export {Client} from './Client'
+export * from '@tanstack/router-core-ssr/client'
--- packages/react-router-ssr/src/server/index.ts
+++ packages/react-router-ssr/src/server/index.ts
@@ -1,3 +1,4 @@
 export {Server} from './Server'
 export {defaultRenderHandler} from './defaultRenderHandler'
 export {defaultStreamHandler} from './defaultStreamHandler'
+export * from '@tanstack/router-core-ssr/server'
