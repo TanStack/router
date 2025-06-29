f527a5c1c3117b71fc980a5c3865c461152dbee6
Dane Grant <danecando@gmail.com>
2025-06-29 10:07:31 -0400

    add sitemap vite plugin to router basic example

--- examples/react/basic/vite.config.js
+++ examples/react/basic/vite.config.js
@@ -1,7 +1,35 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { sitemapPlugin } from '@tanstack/router-sitemap/vite-plugin'
+import { fetchPosts } from './src/posts'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    sitemapPlugin({
+      sitemap: {
+        siteUrl: 'http://localhost:3000',
+        priority: 0.5,
+        changefreq: 'weekly',
+        routes: [
+          '/',
+          '/posts',
+          [
+            '/posts/$postId',
+            async () => {
+              const posts = await fetchPosts()
+              return posts.map((post) => ({
+                path: `/posts/${post.id}`,
+                priority: 0.8,
+                changefreq: 'daily',
+              }))
+            },
+          ],
+          '/route-a',
+          '/route-b',
+        ],
+      },
+    }),
+  ],
 })
