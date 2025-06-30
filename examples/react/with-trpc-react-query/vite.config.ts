9d6c854e9366b3c824e3e530c59215510a461479
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-06-30 03:29:54 +0200

    update to use vite more closely

--- examples/react/with-trpc-react-query/vite.config.ts
+++ examples/react/with-trpc-react-query/vite.config.ts
@@ -1,22 +1,52 @@
-import { defineConfig } from 'vite'
+import url from 'node:url'
+import path from 'node:path'
+import {  defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import { tanstackRouter } from '@tanstack/router-plugin/vite'
+import type {BuildEnvironmentOptions} from 'vite';
 
-// https://vitejs.dev/config/
-export default defineConfig({
-  server: {
-    proxy: {
-      '/trpc': 'http://localhost:3000',
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
+
+// Client Build Configuration
+const clientBuildConfig: BuildEnvironmentOptions = {
+  outDir: 'dist/client',
+  emitAssets: true,
+  copyPublicDir: true,
+  emptyOutDir: true,
+}
+
+// Server Build Configuration
+const serverBuildConfig: BuildEnvironmentOptions = {
+  ssr: true,
+  outDir: 'dist/server',
+  copyPublicDir: false,
+  emptyOutDir: true,
+  rollupOptions: {
+    input: path.resolve(__dirname, 'src/server/server.ts'),
+    output: {
+      entryFileNames: '[name].js',
+      chunkFileNames: 'assets/[name]-[hash].js',
+      assetFileNames: 'assets/[name]-[hash][extname]',
     },
   },
-  plugins: [
-    tanstackRouter({
-      target: 'react',
-      autoCodeSplitting: true,
-    }),
-    react(),
-  ],
-  build: {
-    outDir: 'dist/client',
-  },
+}
+
+// https://vitejs.dev/config/
+export default defineConfig((configEnv) => {
+  return {
+    server: {
+      proxy: {
+        '/trpc': 'http://localhost:3000',
+      },
+    },
+    plugins: [
+      tanstackRouter({
+        target: 'react',
+        autoCodeSplitting: true,
+      }),
+      react(),
+    ],
+    build: configEnv.mode === 'server' ? serverBuildConfig : clientBuildConfig
+  }
 })
--- examples/react/with-trpc/src/server/server.ts
+++ examples/react/with-trpc/src/server/server.ts
@@ -1,26 +1,77 @@
 import path from 'node:path'
 import url from 'node:url'
+import * as fs from 'node:fs'
 import express from 'express'
 import { trpcMiddleWare } from './trpc'
 
-const isProd = process.env.NODE_ENV === 'production'
+const isTest = process.env.NODE_ENV === 'test' || !!process.env.VITE_TEST_BUILD
 
-const app = express()
+const __filename = url.fileURLToPath(import.meta.url)
+const __dirname = path.dirname(__filename)
 
-app.use('/trpc', trpcMiddleWare)
+export const createServer = async (
+  root = process.cwd(),
+  isProd = process.env.NODE_ENV === 'production',
+  hmrPort: number = 3001,
+) => {
+  const app = express()
 
-if (isProd) {
-  const __filename = url.fileURLToPath(import.meta.url)
-  const __dirname = path.dirname(__filename)
+  app.use('/trpc', trpcMiddleWare)
+
+  if (!isProd) {
+    const vite = await (await import('vite')).createServer({
+      root,
+      logLevel: isTest ? 'error' : 'info',
+      server: {
+        middlewareMode: true,
+        watch: {
+          // During tests we edit the files too fast and sometimes chokidar
+          // misses change events, so enforce polling for consistency
+          usePolling: true,
+          interval: 100,
+        },
+        hmr: {
+          port: hmrPort,
+        },
+      },
+      appType: 'custom',
+    })
+
+    // Use vite's connect instance as middleware
+    app.use(vite.middlewares)
+    console.log(path.resolve(root, 'index.html'))
+
+    // Handle any requests that don't match an API route by serving the React app's index.html
+    app.get('*', async (req, res, next) => {
+      try {
+        let html = fs.readFileSync(path.resolve(root, 'index.html'), 'utf-8')
+
+        // Transform HTML using Vite plugins.
+        html = await vite.transformIndexHtml(req.url, html)
+
+        res.send(html)
+      } catch (e) {
+        return next(e)
+      }
+    })
+
+    return { app }
+  }
 
   app.use(express.static(path.resolve(__dirname, '../client')))
 
   // Handle any requests that don't match an API route by serving the React app's index.html
-  app.get('/{*any}', (req, res) => {
+  app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../client', 'index.html'))
   })
+
+  return {app}
 }
 
-app.listen(3000, () => {
-  console.info('Client Server: http://localhost:3000')
-})
+if (!isTest) {
+  createServer().then(async ({ app }) =>
+    app.listen(3000, () => {
+      console.info('Client Server: http://localhost:3000')
+    }),
+  )
+}
