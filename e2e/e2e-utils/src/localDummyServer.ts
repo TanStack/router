1f36ef81cccf7a578798299e61904f6c0c355b4a
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-07-19 20:03:12 +0200

    add cors and stop endpoint

--- e2e/e2e-utils/src/localDummyServer.ts
+++ e2e/e2e-utils/src/localDummyServer.ts
@@ -6,16 +6,53 @@ import http from 'node:http'
 
 export async function localDummyServer(port: number) {
   const server = http.createServer((req, res) => {
-    res.writeHead(200, { 'Content-Type': 'text/html' })
-    res.end('Hello World')
+    // Set CORS headers
+    res.setHeader('Access-Control-Allow-Origin', '*')
+    res.setHeader('Access-Control-Request-Method', '*')
+    res.setHeader('Access-Control-Allow-Methods', 'OPTIONS, GET, POST')
+    res.setHeader('Access-Control-Allow-Headers', '*')
+
+    if (req.method === 'OPTIONS') {
+      res.writeHead(200)
+      res.end()
+      return
+    }
   })
-  const promise = new Promise<void>((resolve) => {
+
+  server.on('request', async (req, res) => {
+    const url = new URL(req.url!, 'http://localhost')
+
+    if (res.req.method === 'GET' && url.pathname === '/') {
+      res.writeHead(200, { 'Content-Type': 'text/html' })
+      res.end('Hello World')
+    }
+  })
+
+  server.on('request', async (req, res) => {
+    const url = new URL(req.url!, `http://localhost`)
+
+    if (res.req.method === 'POST' && url.pathname === '/stop') {
+      server.close()
+      console.log(
+        `stopped local dummy server running at http://localhost:${port}`,
+      )
+
+      res.writeHead(200)
+      res.end()
+    }
+  })
+
+  const startServer = new Promise<void>((resolve) => {
     server.listen(port, 'localhost', () => {
-      console.log(`local dummy server running at http://localhost:${port}`)
+      console.log(
+        `started local dummy server running at http://localhost:${port}`,
+      )
       resolve()
     })
   })
-  await promise
 
+  await startServer
+
+  await new Promise((r) => setTimeout(r, 500))
   return server
 }
