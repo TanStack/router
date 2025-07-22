d485cc23d658504fbe07bed40a41e8d0e5144b4c
Dimitris Karagiannis <mkarajohn@users.noreply.github.com>
2025-07-22 18:32:16 +0300

    fix(router-core): type mismatch for navigate function (#4625)
    According to the docs this should only return a promise
    https://tanstack.com/router/latest/docs/framework/react/api/router/RouterType#navigate-method
    Makes sense otherwise you cannot .then() it
    
    ---------
    
    Co-authored-by: Nico Lynzaad <44094871+nlynzaad@users.noreply.github.com>

--- packages/router-core/src/router.ts
+++ packages/router-core/src/router.ts
@@ -1755,7 +1755,7 @@ export class RouterCore<
       } else {
         window.location.href = href
       }
-      return
+      return Promise.resolve()
     }
 
     return this.buildAndCommitLocation({
