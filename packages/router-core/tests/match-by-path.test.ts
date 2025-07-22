72183fc48eb2609eb884a9f83118001e472fdea1
Sheraff <fpellet@ensc.fr>
2025-07-22 17:01:13 +0200

    cleanup

--- packages/router-core/tests/match-by-path.test.ts
+++ packages/router-core/tests/match-by-path.test.ts
@@ -1,246 +1,158 @@
-import { describe, expect, it } from 'vitest'
-import { matchByPath } from '../src'
+import { describe, expect, it } from "vitest"
+import { matchByPath } from "../src"
 
 describe('default path matching', () => {
-  it('should match a static path', () => {
-    expect(matchByPath('', '', { to: '' })).toEqual({})
-    expect(matchByPath('', '/', { to: '' })).toEqual({})
-    expect(matchByPath('', '', { to: '/' })).toEqual({})
-    expect(matchByPath('', '/', { to: '/' })).toEqual({})
-    expect(matchByPath('/', '/', { to: '/' })).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/a' })).toEqual({})
-    expect(matchByPath('/', '/a/b', { to: '/a/b' })).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/a/' })).toEqual({})
-    expect(matchByPath('/', '/a/', { to: '/a/' })).toEqual({})
-    expect(matchByPath('/', '/a/', { to: '/a' })).toBeFalsy()
-    expect(matchByPath('/', '/b', { to: '/a' })).toBeFalsy()
+  it.each([
+    ['', '', '', {}],
+    ['', '/', '', {}],
+    ['', '', '/', {}],
+    ['', '/', '/', {}],
+    ['/', '/', '/', {}],
+    ['/', '/a', '/a', {}],
+    ['/', '/a/b', '/a/b', {}],
+    ['/', '/a', '/a/', {}],
+    ['/', '/a/', '/a/', {}],
+    ['/', '/a/', '/a', undefined],
+    ['/', '/b', '/a', undefined],
+  ])('static %s %s => %s', (base, from, to, result) => {
+    expect(matchByPath(base, from, { to, caseSensitive: false })).toEqual(result)
   })
-  it('should match path with params', () => {
-    expect(matchByPath('/', '/a/1', { to: '/a/$id' })).toEqual({ id: '1' })
-    expect(matchByPath('/', '/a/1/b', { to: '/a/$id/b' })).toEqual({ id: '1' })
-    expect(matchByPath('/', '/a/1/b/2', { to: '/a/$id/b/$other' })).toEqual({
-      id: '1',
-      other: '2',
-    })
-    expect(matchByPath('/', '/a/1/b/2', { to: '/a/$id/b/$id' })).toEqual({
-      id: '2',
-    }) // not sure this is needed / intentional / can even happen
+
+  it.each([
+    ['/a/1', '/a/$id', { id: '1' }],
+    ['/a/1/b', '/a/$id/b', { id: '1' }],
+    ['/a/1/b/2', '/a/$id/b/$other', { id: '1', other: '2' }],
+    ['/a/1/b/2', '/a/$id/b/$id', { id: '2' }]
+  ])('params %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to })).toEqual(result)
   })
-  it('should match path with optional params', () => {
-    expect(matchByPath('/', '/a/1', { to: '/a/{-$id}' })).toEqual({ id: '1' })
-    expect(matchByPath('/', '/a', { to: '/a/{-$id}' })).toEqual({})
-    expect(matchByPath('/', '/a/1/b', { to: '/a/{-$id}/b' })).toEqual({
-      id: '1',
-    })
-    expect(matchByPath('/', '/a/b', { to: '/a/{-$id}/b' })).toEqual({})
-    expect(
-      matchByPath('/', '/a/1/b/2', { to: '/a/{-$id}/b/{-$other}' }),
-    ).toEqual({ id: '1', other: '2' })
-    expect(matchByPath('/', '/a/b/2', { to: '/a/{-$id}/b/{-$other}' })).toEqual(
-      { other: '2' },
-    )
-    expect(matchByPath('/', '/a/1/b', { to: '/a/{-$id}/b/{-$other}' })).toEqual(
-      { id: '1' },
-    )
-    expect(matchByPath('/', '/a/b', { to: '/a/{-$id}/b/{-$other}' })).toEqual(
-      {},
-    )
-    expect(matchByPath('/', '/a/1/b/2', { to: '/a/{-$id}/b/{-$id}' })).toEqual({
-      id: '2',
-    }) // not sure this is needed / intentional / can even happen
+
+  it.each([
+    ['/a/1', '/a/{-$id}', { id: '1' }],
+    ['/a', '/a/{-$id}', {}],
+    ['/a/1/b', '/a/{-$id}/b', { id: '1' }],
+    ['/a/b', '/a/{-$id}/b', {}],
+    ['/a/1/b/2', '/a/{-$id}/b/{-$other}', { id: '1', other: '2' }],
+    ['/a/b/2', '/a/{-$id}/b/{-$other}', { other: '2' }],
+    ['/a/1/b', '/a/{-$id}/b/{-$other}', { id: '1', }],
+    ['/a/b', '/a/{-$id}/b/{-$other}', {}],
+    ['/a/1/b/2', '/a/{-$id}/b/{-$id}', { id: '2' }],
+  ])('optional %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to })).toEqual(result)
   })
-  it('should match path with wildcard params', () => {
-    expect(matchByPath('/', '/a/b/c', { to: '/a/$' })).toEqual({
-      _splat: 'b/c',
-      '*': 'b/c',
-    })
-    expect(matchByPath('/', '/a/', { to: '/a/$' })).toEqual({
-      _splat: '/',
-      '*': '/',
-    }) // is this intended? since the above match doesn't include the leading slash, this feels weird
-    expect(matchByPath('/', '/a', { to: '/a/$' })).toEqual({
-      _splat: '',
-      '*': '',
-    })
-    expect(matchByPath('/', '/a/b/c', { to: '/a/$/foo' })).toEqual({
-      _splat: 'b/c',
-      '*': 'b/c',
-    })
+
+  it.each([
+    ['/a/b/c', '/a/$', { _splat: 'b/c', '*': 'b/c' }],
+    ['/a/', '/a/$', { _splat: '/', '*': '/' }],
+    ['/a', '/a/$', { _splat: '', '*': '' }],
+    ['/a/b/c', '/a/$/foo', { _splat: 'b/c', '*': 'b/c' }],
+  ])('wildcard %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to })).toEqual(result)
   })
 })
 
 describe('case insensitive path matching', () => {
-  it('should match a static path case insensitively', () => {
-    expect(matchByPath('', '', { to: '', caseSensitive: false })).toEqual({})
-    expect(matchByPath('', '/', { to: '', caseSensitive: false })).toEqual({})
-    expect(matchByPath('', '', { to: '/', caseSensitive: false })).toEqual({})
-    expect(matchByPath('', '/', { to: '/', caseSensitive: false })).toEqual({})
-    expect(matchByPath('/', '/', { to: '/', caseSensitive: false })).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/A', caseSensitive: false })).toEqual(
-      {},
-    )
-    expect(
-      matchByPath('/', '/a/b', { to: '/A/B', caseSensitive: false }),
-    ).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/A/', caseSensitive: false })).toEqual(
-      {},
-    )
-    expect(
-      matchByPath('/', '/a/', { to: '/A/', caseSensitive: false }),
-    ).toEqual({})
-    expect(
-      matchByPath('/', '/a/', { to: '/A', caseSensitive: false }),
-    ).toBeFalsy()
-    expect(
-      matchByPath('/', '/b', { to: '/A', caseSensitive: false }),
-    ).toBeFalsy()
+  it.each([
+    ['', '', '', {}],
+    ['', '/', '', {}],
+    ['', '', '/', {}],
+    ['', '/', '/', {}],
+    ['/', '/', '/', {}],
+    ['/', '/a', '/A', {}],
+    ['/', '/a/b', '/A/B', {}],
+    ['/', '/a', '/A/', {}],
+    ['/', '/a/', '/A/', {}],
+    ['/', '/a/', '/A', undefined],
+    ['/', '/b', '/A', undefined],
+  ])('static %s %s => %s', (base, from, to, result) => {
+    expect(matchByPath(base, from, { to, caseSensitive: false })).toEqual(result)
   })
-  it('should match path with params case insensitively', () => {
-    expect(
-      matchByPath('/', '/a/1', { to: '/A/$id', caseSensitive: false }),
-    ).toEqual({ id: '1' })
-    expect(
-      matchByPath('/', '/a/1/b', { to: '/A/$id/B', caseSensitive: false }),
-    ).toEqual({ id: '1' })
-    expect(
-      matchByPath('/', '/a/1/b/2', {
-        to: '/A/$id/B/$other',
-        caseSensitive: false,
-      }),
-    ).toEqual({ id: '1', other: '2' })
-    expect(
-      matchByPath('/', '/a/1/b/2', {
-        to: '/A/$id/B/$id',
-        caseSensitive: false,
-      }),
-    ).toEqual({ id: '2' }) // not sure this is needed / intentional / can even happen
+
+  it.each([
+    ['/a/1', '/A/$id', { id: '1' }],
+    ['/a/1/b', '/A/$id/B', { id: '1' }],
+    ['/a/1/b/2', '/A/$id/B/$other', { id: '1', other: '2' }],
+    ['/a/1/b/2', '/A/$id/B/$id', { id: '2' }]
+  ])('params %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, caseSensitive: false })).toEqual(result)
   })
-  it('should match path with optional params case insensitively', () => {
-    expect(
-      matchByPath('/', '/a/1', { to: '/A/{-$id}', caseSensitive: false }),
-    ).toEqual({ id: '1' })
-    expect(
-      matchByPath('/', '/a', { to: '/A/{-$id}', caseSensitive: false }),
-    ).toEqual({})
-    expect(
-      matchByPath('/', '/a/1/b', { to: '/A/{-$id}/B', caseSensitive: false }),
-    ).toEqual({ id: '1' })
-    // expect(matchByPath('/', '/a/b', { to: '/A/{-$id}/B', caseSensitive: false })).toEqual({})
-    expect(
-      matchByPath('/', '/a/1/b/2', {
-        to: '/A/{-$id}/B/{-$other}',
-        caseSensitive: false,
-      }),
-    ).toEqual({ id: '1', other: '2' })
-    // expect(matchByPath('/', '/a/b/2', { to: '/A/{-$id}/B/{-$other}', caseSensitive: false })).toEqual({ other: '2' })
-    expect(
-      matchByPath('/', '/a/1/b', {
-        to: '/A/{-$id}/B/{-$other}',
-        caseSensitive: false,
-      }),
-    ).toEqual({ id: '1' })
-    // expect(matchByPath('/', '/a/b', { to: '/A/{-$id}/B/{-$other}', caseSensitive: false })).toEqual({})
-    expect(
-      matchByPath('/', '/a/1/b/2', {
-        to: '/A/{-$id}/B/{-$id}',
-        caseSensitive: false,
-      }),
-    ).toEqual({ id: '2' }) // not sure this is needed / intentional / can even happen
+
+  it.each([
+    ['/a/1', '/A/{-$id}', { id: '1' }],
+    ['/a', '/A/{-$id}', {}],
+    ['/a/1/b', '/A/{-$id}/B', { id: '1' }],
+    // ['/a/b', '/A/{-$id}/B', {}],
+    ['/a/1/b/2', '/A/{-$id}/B/{-$other}', { id: '1', other: '2' }],
+    // ['/a/b/2', '/A/{-$id}/B/{-$other}', { other: '2' }],
+    ['/a/1/b', '/A/{-$id}/B/{-$other}', { id: '1' }],
+    // ['/a/b', '/A/{-$id}/B/{-$other}', {}],
+    ['/a/1/b/2', '/A/{-$id}/B/{-$id}', { id: '2' }],
+  ])('optional %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, caseSensitive: false })).toEqual(result)
   })
-  it('should match path with wildcard params case insensitively', () => {
-    expect(
-      matchByPath('/', '/a/b/c', { to: '/A/$', caseSensitive: false }),
-    ).toEqual({ _splat: 'b/c', '*': 'b/c' })
-    expect(
-      matchByPath('/', '/a/', { to: '/A/$', caseSensitive: false }),
-    ).toEqual({ _splat: '/', '*': '/' }) // is this intended? since the above match doesn't include the leading slash, this feels weird
-    expect(
-      matchByPath('/', '/a', { to: '/A/$', caseSensitive: false }),
-    ).toEqual({ _splat: '', '*': '' })
-    expect(
-      matchByPath('/', '/a/b/c', { to: '/A/$/foo', caseSensitive: false }),
-    ).toEqual({ _splat: 'b/c', '*': 'b/c' })
+
+  it.each([
+    ['/a/b/c', '/A/$', { _splat: 'b/c', '*': 'b/c' }],
+    ['/a/', '/A/$', { _splat: '/', '*': '/' }],
+    ['/a', '/A/$', { _splat: '', '*': '' }],
+    ['/a/b/c', '/A/$/foo', { _splat: 'b/c', '*': 'b/c' }],
+  ])('wildcard %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, caseSensitive: false })).toEqual(result)
   })
 })
 
 describe('fuzzy path matching', () => {
-  it('should match a static path fuzzily', () => {
-    expect(matchByPath('', '', { to: '', fuzzy: true })).toEqual({})
-    expect(matchByPath('', '/', { to: '', fuzzy: true })).toEqual({})
-    expect(matchByPath('', '', { to: '/', fuzzy: true })).toEqual({})
-    expect(matchByPath('', '/', { to: '/', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/', { to: '/', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/a', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/a', { to: '/a/', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/a/', { to: '/a/', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/a/', { to: '/a', fuzzy: true })).toEqual({
-      '**': '/',
-    })
-    expect(matchByPath('/', '/a/b', { to: '/a/b', fuzzy: true })).toEqual({})
-    expect(matchByPath('/', '/a/b', { to: '/a', fuzzy: true })).toEqual({
-      '**': 'b',
-    })
-    expect(matchByPath('/', '/a/b/', { to: '/a', fuzzy: true })).toEqual({
-      '**': 'b/',
-    })
-    expect(matchByPath('/', '/a/b/c', { to: '/a', fuzzy: true })).toEqual({
-      '**': 'b/c',
-    })
-    expect(matchByPath('/', '/a', { to: '/a/b', fuzzy: true })).toBeFalsy()
-    expect(matchByPath('/', '/b', { to: '/a', fuzzy: true })).toBeFalsy()
-    expect(matchByPath('/', '/a', { to: '/b', fuzzy: true })).toBeFalsy()
+  it.each([
+    ['', '', '', {}],
+    ['', '/', '', {}],
+    ['', '', '/', {}],
+    ['', '/', '/', {}],
+    ['/', '/', '/', {}],
+    ['/', '/a', '/a', {}],
+    ['/', '/a', '/a/', {}],
+    ['/', '/a/', '/a/', {}],
+    ['/', '/a/', '/a', { "**": "/" }],
+    ['/', '/a/b', '/a/b', {}],
+    ['/', '/a/b', '/a', { "**": "b" }],
+    ['/', '/a/b/', '/a', { "**": "b/" }],
+    ['/', '/a/b/c', '/a', { "**": "b/c" }],
+    ['/', '/a', '/a/b', undefined],
+    ['/', '/b', '/a', undefined],
+    ['/', '/a', '/b', undefined],
+  ])('static %s %s => %s', (base, from, to, result) => {
+    expect(matchByPath(base, from, { to, fuzzy: true })).toEqual(result)
   })
 
-  it('should match path with params fuzzily', () => {
-    expect(matchByPath('/', '/a/1', { to: '/a/$id', fuzzy: true })).toEqual({
-      id: '1',
-    })
-    expect(matchByPath('/', '/a/1/b', { to: '/a/$id', fuzzy: true })).toEqual({
-      id: '1',
-      '**': 'b',
-    })
-    expect(matchByPath('/', '/a/1/', { to: '/a/$id/', fuzzy: true })).toEqual({
-      id: '1',
-    })
-    expect(
-      matchByPath('/', '/a/1/b/2', { to: '/a/$id/b/$other', fuzzy: true }),
-    ).toEqual({ id: '1', other: '2' })
-    expect(
-      matchByPath('/', '/a/1/b/2/c', { to: '/a/$id/b/$other', fuzzy: true }),
-    ).toEqual({ id: '1', other: '2', '**': 'c' })
+  it.each([
+    ['/a/1', '/a/$id', { id: '1' }],
+    ['/a/1/b', '/a/$id', { id: '1', "**": "b" }],
+    ['/a/1/', '/a/$id/', { id: '1' }],
+    ['/a/1/b/2', '/a/$id/b/$other', { id: '1', other: '2' }],
+    ['/a/1/b/2/c', '/a/$id/b/$other', { id: '1', other: '2', "**": "c" }],
+  ])('params %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, fuzzy: true })).toEqual(result)
   })
 
-  it('should match path with optional params fuzzily', () => {
-    expect(matchByPath('/', '/a/1', { to: '/a/{-$id}', fuzzy: true })).toEqual({
-      id: '1',
-    })
-    expect(matchByPath('/', '/a', { to: '/a/{-$id}', fuzzy: true })).toEqual({})
-    expect(
-      matchByPath('/', '/a/1/b', { to: '/a/{-$id}', fuzzy: true }),
-    ).toEqual({ '**': 'b', id: '1' })
-    expect(
-      matchByPath('/', '/a/1/b', { to: '/a/{-$id}/b', fuzzy: true }),
-    ).toEqual({ id: '1' })
-    expect(
-      matchByPath('/', '/a/b', { to: '/a/{-$id}/b', fuzzy: true }),
-    ).toEqual({})
-    expect(
-      matchByPath('/', '/a/b/c', { to: '/a/{-$id}/b', fuzzy: true }),
-    ).toEqual({ '**': 'c' })
-    expect(
-      matchByPath('/', '/a/b', { to: '/a/{-$id}/b/{-$other}', fuzzy: true }),
-    ).toEqual({})
-    expect(
-      matchByPath('/', '/a/b/2/d', {
-        to: '/a/{-$id}/b/{-$other}',
-        fuzzy: true,
-      }),
-    ).toEqual({ other: '2', '**': 'd' })
-    expect(
-      matchByPath('/', '/a/1/b/2/c', {
-        to: '/a/{-$id}/b/{-$other}',
-        fuzzy: true,
-      }),
-    ).toEqual({ id: '1', other: '2', '**': 'c' })
+  it.each([
+    ['/a/1', '/a/{-$id}', { id: '1' }],
+    ['/a', '/a/{-$id}', {}],
+    ['/a/1/b', '/a/{-$id}', { '**': 'b', id: '1' }],
+    ['/a/1/b', '/a/{-$id}/b', { id: '1' }],
+    ['/a/b', '/a/{-$id}/b', {}],
+    ['/a/b/c', '/a/{-$id}/b', { '**': 'c' }],
+    ['/a/b', '/a/{-$id}/b/{-$other}', {}],
+    ['/a/b/2/d', '/a/{-$id}/b/{-$other}', { 'other': '2', '**': 'd' }],
+    ['/a/1/b/2/c', '/a/{-$id}/b/{-$other}', { id: '1', other: '2', "**": "c" }],
+  ])('optional %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, fuzzy: true })).toEqual(result)
+  })
+
+  it.each([
+    ['/a/b/c', '/a/$', { _splat: 'b/c', '*': 'b/c' }],
+    ['/a/', '/a/$', { _splat: '/', '*': '/' }],
+    ['/a', '/a/$', { _splat: '', '*': '' }],
+    ['/a/b/c/d', '/a/$/foo', { _splat: 'b/c/d', '*': 'b/c/d' }],
+  ])('wildcard %s => %s', (from, to, result) => {
+    expect(matchByPath('/', from, { to, fuzzy: true })).toEqual(result)
   })
 })
