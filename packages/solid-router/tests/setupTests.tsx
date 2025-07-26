92f294a587de5731f1e2eb3f70a80f8b374fffd2
Manuel Schiller <manuel.schiller@caligano.de>
2025-07-26 22:54:15 +0200

    chore: silence error in test

--- packages/solid-router/tests/setupTests.tsx
+++ packages/solid-router/tests/setupTests.tsx
@@ -1,4 +1,8 @@
 import '@testing-library/jest-dom/vitest'
+import { vi } from 'vitest';
 
 // @ts-expect-error
 global.IS_REACT_ACT_ENVIRONMENT = true
+
+// Mock window.scrollTo to silence errors in tests
+window.scrollTo = vi.fn();
