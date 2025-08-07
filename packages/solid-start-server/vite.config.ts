630ef2cf228fa05834d744dc105e425c819b0116
Manuel Schiller <manuel.schiller@caligano.de>
2025-08-08 00:41:01 +0200

    cleanup

--- packages/solid-start-server/vite.config.ts
+++ packages/solid-start-server/vite.config.ts
@@ -2,14 +2,11 @@ import { defineConfig, mergeConfig } from 'vitest/config'
 import { tanstackViteConfig } from '@tanstack/config/vite'
 import solid from 'vite-plugin-solid'
 import packageJson from './package.json'
-import minifyScriptPlugin from './vite-minify-plugin'
-import type { ViteUserConfig } from 'vitest/config'
 
 const config = defineConfig({
   plugins: [
-    minifyScriptPlugin(),
     solid({ solid: { generate: 'ssr' } }),
-  ] as ViteUserConfig['plugins'],
+  ],
   test: {
     name: packageJson.name,
     watch: false,
