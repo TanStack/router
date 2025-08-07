570f3db4da6d690bbb876fd7d1fb7610aa07bd52
Manuel Schiller <manuel.schiller@caligano.de>
2025-08-08 00:41:01 +0200

    merge react/solid-start-vite with react/solid-start packages
    avoids a circular dep

--- packages/solid-start/vite.config.ts
+++ packages/solid-start/vite.config.ts
@@ -1,5 +1,6 @@
 import { defineConfig, mergeConfig } from 'vitest/config'
 import { tanstackViteConfig } from '@tanstack/config/vite'
+import { copyFilesPlugin } from '@tanstack/router-utils'
 import packageJson from './package.json'
 
 const config = defineConfig({
@@ -8,6 +9,13 @@ const config = defineConfig({
     watch: false,
     environment: 'jsdom',
   },
+  plugins: [
+    copyFilesPlugin({
+      pattern: ['*.ts', '*.tsx', '!*.d.ts'],
+      fromDir: 'src/plugin/default-entry',
+      toDir: 'dist/plugin/default-entry',
+    }),
+  ],
 })
 
 export default mergeConfig(
@@ -17,16 +25,17 @@ export default mergeConfig(
     entry: [
       './src/client.tsx',
       './src/server.tsx',
-      './src/plugin-vite.ts',
+      './src/plugin/vite.ts',
       './src/server-functions-client.tsx',
       './src/server-functions-server.tsx',
     ],
     externalDeps: [
       '@tanstack/solid-start-client',
       '@tanstack/solid-start-server',
-      '@tanstack/solid-start-plugin',
       '@tanstack/start-server-functions-client',
       '@tanstack/start-server-functions-server',
     ],
+    cjs: false,
+    exclude: ['./src/plugin/default-entry'],
   }),
 )
