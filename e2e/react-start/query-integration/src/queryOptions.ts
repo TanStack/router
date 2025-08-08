90adfe919e725372dc6334903e98717a75457be1
Manuel Schiller <manuel.schiller@caligano.de>
2025-08-08 23:52:29 +0200

    feat: restructure query-integration (#4643)
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

---
+++ e2e/react-start/query-integration/src/queryOptions.ts
@@ -0,0 +1,16 @@
+import { queryOptions } from '@tanstack/react-query'
+
+export const makeQueryOptions = (key: string) =>
+  queryOptions({
+    queryKey: ['e2e-test-query-integration', key],
+    queryFn: async () => {
+      console.log('fetching query data')
+      await new Promise<void>((resolve) => {
+        setTimeout(resolve, 500)
+      })
+      const result = typeof window !== 'undefined' ? 'client' : 'server'
+      console.log('query data result', result)
+      return result
+    },
+    staleTime: Infinity,
+  })
