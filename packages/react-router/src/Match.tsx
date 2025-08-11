94ea50ff19b60ff7372d7017815d68dbccd2a207
Flo <fpellet@ensc.fr>
2025-08-11 15:58:39 +0200

    refactor(react-router,solid-router): Minor simplification of MatchInner match logic in select (#4921)
    We don't need the `matchIndex` in `Match > MatchInner > useRouterState >
    select`. It must have been left after a previous refactor.

--- packages/react-router/src/Match.tsx
+++ packages/react-router/src/Match.tsx
@@ -189,8 +189,7 @@ export const MatchInner = React.memo(function MatchInnerImpl({
 
   const { match, key, routeId } = useRouterState({
     select: (s) => {
-      const matchIndex = s.matches.findIndex((d) => d.id === matchId)
-      const match = s.matches[matchIndex]!
+      const match = s.matches.find((d) => d.id === matchId)!
       const routeId = match.routeId as string
 
       const remountFn =
