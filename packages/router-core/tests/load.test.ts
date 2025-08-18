66daf9d1fbbea937881e2ce9f67b8cd9d8d030fe
Sheraff <fpellet@ensc.fr>
2025-08-18 21:36:31 +0200

    refactor(router-core): rework the conditions under which a beforeLoad is executed or skipped

---
+++ packages/router-core/tests/load.test.ts
@@ -0,0 +1,24 @@
+import { describe, expect, test, vi } from "vitest"
+import { BaseRootRoute, BaseRoute, RouterCore } from "../src"
+
+
+describe('beforeLoad skip or exec', () => {
+	test('current behavior', async () => {
+		const rootRoute = new BaseRootRoute({})
+		const beforeLoad = vi.fn(() => new Promise((resolve) => setTimeout(resolve, 100)))
+		const fooRoute = new BaseRoute({
+			getParentRoute: () => rootRoute,
+			path: '/foo',
+			beforeLoad
+		})
+		const routeTree = rootRoute.addChildren([fooRoute])
+		const router = new RouterCore({
+			routeTree
+		})
+		router.preloadRoute({ to: '/foo' })
+		await Promise.resolve()
+		await router.navigate({ to: '/foo' })
+
+		expect(beforeLoad).toHaveBeenCalledTimes(2)
+	})
+})
