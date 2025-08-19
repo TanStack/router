4f52e46f8a94510f82cb38d7e1fb34f81f6c7bca
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-08-20 01:00:28 +0200

    refactor based on coderabbit recommendations

--- packages/solid-router/src/useActiveLocation.ts
+++ packages/solid-router/src/useActiveLocation.ts
@@ -1,51 +1,42 @@
 import { last } from '@tanstack/router-core'
-import { createEffect, createSignal } from 'solid-js'
-import * as Solid from 'solid-js'
+import {  createEffect, createMemo, createSignal } from 'solid-js'
 import { useMatch } from './useMatch'
 import { useRouter } from './useRouter'
 import { useRouterState } from './useRouterState'
+import type {Accessor} from 'solid-js';
 import type { ParsedLocation } from '@tanstack/router-core'
 
-export function useActiveLocation(location?: ParsedLocation) {
+export type UseLocationResult = {
+  activeLocation: Accessor<ParsedLocation>
+  getFromPath: (from?: string) => Accessor<string>
+  setActiveLocation: (location?: ParsedLocation) => void
+}
+
+export function useActiveLocation(location?: ParsedLocation): UseLocationResult {
   const router = useRouter()
-  const [activeLocation, setActiveLocation] = createSignal<ParsedLocation>(
-    location ?? useRouterState({ select: (state) => state.location })(),
-  )
-  const [customActiveLocation, _setCustomActiveLocation] =
-    createSignal<ParsedLocation>(
-      location ?? useRouterState({ select: (state) => state.location })(),
-    )
-  const [useCustomActiveLocation, setUseCustomActiveLocation] =
-    createSignal(!!location)
+  // we are not using a variable here for router state location since we need to only calculate that if the location is not passed in. It can result in unnecessary history actions if we do that.
+  const [activeLocation, setActiveLocation] = createSignal<ParsedLocation>(location ?? useRouterState({select: s => s.location})())
+  const [customActiveLocation, setCustomActiveLocation] = createSignal<ParsedLocation | undefined>(location)
 
   createEffect(() => {
-    if (!useCustomActiveLocation()) {
-      setActiveLocation(useRouterState({ select: (state) => state.location }))
-    } else {
-      setActiveLocation(customActiveLocation())
-    }
+    setActiveLocation(customActiveLocation() ?? useRouterState({select: s => s.location})())
   })
 
-  const setCustomActiveLocation = (location: ParsedLocation) => {
-    _setCustomActiveLocation(location)
-    setUseCustomActiveLocation(true)
-  }
-
-  const matchIndex = useMatch({
+  const currentRouteMatch = useMatch({
     strict: false,
-    select: (match) => match.index,
+    select: (match) => match,
   })
 
   const getFromPath = (from?: string) =>
-    Solid.createMemo(() => {
-      const currentRouteMatches = router.matchRoutes(activeLocation(), {
+    createMemo(() => {
+      const currentRouteMatches = router.matchRoutes(customActiveLocation() ?? activeLocation(), {
         _buildLocation: false,
       })
 
       return (
         from ??
         last(currentRouteMatches)?.fullPath ??
-        router.state.matches[matchIndex()]!.fullPath
+        currentRouteMatch().fullPath
       )
     })
 
