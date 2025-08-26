6fff457e950af80cd88cf1f43a72bcbcb2856374
wobsoriano <sorianorobertc@gmail.com>
2025-08-26 13:43:47 -0700

    chore(examples): Update Clerk server handler usage

--- examples/react/start-clerk-basic/src/server.ts
+++ examples/react/start-clerk-basic/src/server.ts
@@ -1,14 +1,18 @@
 import {
   createStartHandler,
   defaultStreamHandler,
+  defineHandlerCallback
 } from '@tanstack/react-start/server'
 import { createClerkHandler } from '@clerk/tanstack-react-start/server'
 import { createRouter } from './router'
 
-const handler = createStartHandler({
-  createRouter,
+const handlerFactory = createClerkHandler(
+  createStartHandler({
+    createRouter,
+  }),
+)
+
+export default defineHandlerCallback(async event => {
+  const startHandler = await handlerFactory(defaultStreamHandler);
+  return startHandler(event)
 })
-
-const clerkHandler = createClerkHandler(handler)
-
-export default clerkHandler(defaultStreamHandler)
