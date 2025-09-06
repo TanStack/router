1e01613608e23d6fd3338870d748b4d76ca9e142
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-09-06 13:50:04 +0200

    merge update from main

---
+++ e2e/solid-router/basic-file-based/src/routes/params-ps/named/$foo/$bar.route.tsx
@@ -0,0 +1,64 @@
+import {
+  Link,
+  Outlet,
+  createFileRoute,
+  useParams,
+} from '@tanstack/solid-router'
+import { createEffect, createSignal } from 'solid-js'
+
+export const Route = createFileRoute('/params-ps/named/$foo/$bar')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  const [renderBarCount, setBarRenderCountCount] = createSignal(0)
+  const [renderBazCount, setBazRenderCountCount] = createSignal(0)
+
+  const params = useParams({
+    strict: false,
+  })
+
+  createEffect(() => {
+    params().bar
+    setBarRenderCountCount((prev) => prev + 1)
+  })
+
+  createEffect(() => {
+    params().baz
+    setBazRenderCountCount((prev) => prev + 1)
+  })
+
+  return (
+    <div>
+      Hello "/params-ps/named/{params().foo}/{params().bar}"!
+      <div>
+        Bar Render Count:{' '}
+        <span data-testid="foo-bar-render-count">{renderBarCount()}</span>
+      </div>
+      <div>
+        Bar: <span data-testid="foo-bar-value">{params().bar}</span>
+      </div>
+      <div>
+        Baz in Bar Render Count:{' '}
+        <span data-testid="foo-baz-in-bar-render-count">
+          {renderBazCount()}
+        </span>
+      </div>
+      <div>
+        Baz in Bar:{' '}
+        <span data-testid="foo-baz-in-bar-value">
+          {params().baz ?? 'no param'}
+        </span>
+      </div>
+      <Link
+        data-testid="params-foo-bar-links-baz"
+        from={Route.fullPath}
+        to="./$baz"
+        params={{ baz: `${params().bar}_10` }}
+      >
+        To Baz
+      </Link>
+      <Outlet />
+    </div>
+  )
+}
