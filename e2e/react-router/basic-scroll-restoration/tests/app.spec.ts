69a7c1ea784fbf847601d1202ef64f8cac2fe3f7
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-09-11 23:11:49 +0200

    test(e2e): resolve missing waitForUrls

--- e2e/react-router/basic-scroll-restoration/tests/app.spec.ts
+++ e2e/react-router/basic-scroll-restoration/tests/app.spec.ts
@@ -5,6 +5,7 @@ test('restore scroll positions by page, home pages top message should not displa
 }) => {
   // Step 1: Navigate to the home page
   await page.goto('/')
+  await page.waitForURL('/')
 
   await expect(page.locator('#greeting')).toContainText('Welcome Home!')
   await expect(page.locator('#top-message')).toBeInViewport()
@@ -45,6 +46,7 @@ test('restore scroll positions by element, first regular list item should not di
 }) => {
   // Step 1: Navigate to the by-element page
   await page.goto('/by-element')
+  await page.waitForURL('/by-element')
 
   // Step 2: Scroll to a position that hides the first list item in regular list
   const targetScrollPosition = 1000
@@ -86,6 +88,7 @@ test('scroll to top when not scrolled, regression test for #4782', async ({
   page,
 }) => {
   await page.goto('/foo')
+  await page.waitForURL('/foo')
 
   await expect(page.getByTestId('foo-route-component')).toBeVisible()
 
