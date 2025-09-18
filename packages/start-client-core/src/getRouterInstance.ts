e42fb7517698769862c68401170f1aa3598e1ebe
Manuel Schiller <manuel.schiller@caligano.de>
2025-09-18 20:00:32 +0200

    formatting

--- packages/start-client-core/src/getRouterInstance.ts
+++ packages/start-client-core/src/getRouterInstance.ts
@@ -2,6 +2,7 @@ import { getStartContext } from '@tanstack/start-storage-context'
 import { createIsomorphicFn } from './createIsomorphicFn'
 import type { Awaitable, RegisteredRouter } from '@tanstack/router-core'
 
-export const getRouterInstance : () => Awaitable<RegisteredRouter>= createIsomorphicFn()
-  .client(()  => window.__TSR_ROUTER__!)
-  .server(() => getStartContext().getRouter())
+export const getRouterInstance: () => Awaitable<RegisteredRouter> =
+  createIsomorphicFn()
+    .client(() => window.__TSR_ROUTER__!)
+    .server(() => getStartContext().getRouter())
