91705d67c5fc78f51469a6171a9705307de202d0
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-09-22 02:20:31 +0200

    update tests for solid

---
+++ e2e/solid-router/basic-file-based/src/routes/non-nested/named/route.tsx
@@ -0,0 +1,58 @@
+import { Link, Outlet, createFileRoute } from '@tanstack/solid-router'
+
+export const Route = createFileRoute('/non-nested/named')({
+  component: RouteComponent,
+})
+
+function RouteComponent() {
+  return (
+    <div>
+      <div data-testid="non-nested-named-root-route-heading">
+        Hello non-nested named layout
+      </div>
+      <div>
+        <Link
+          from={Route.fullPath}
+          to="./$baz"
+          params={{ baz: 'baz' }}
+          data-testid="to-named-index"
+        >
+          To named index
+        </Link>
+        <Link
+          from={Route.fullPath}
+          to="./$baz/foo"
+          params={{ baz: 'baz' }}
+          data-testid="to-named-foo"
+        >
+          To named foo
+        </Link>
+        <Link
+          from={Route.fullPath}
+          to="./$baz/foo"
+          params={{ baz: 'baz_' }}
+          data-testid="to-named-foo-2"
+        >
+          To named foo 2
+        </Link>
+        <Link
+          from={Route.fullPath}
+          to="./$baz/bar"
+          params={{ baz: 'baz' }}
+          data-testid="to-named-bar"
+        >
+          To named bar
+        </Link>
+        <Link
+          from={Route.fullPath}
+          to="./$baz/bar"
+          params={{ baz: 'baz_' }}
+          data-testid="to-named-bar-2"
+        >
+          To named bar 2
+        </Link>
+      </div>
+      <Outlet />
+    </div>
+  )
+}
