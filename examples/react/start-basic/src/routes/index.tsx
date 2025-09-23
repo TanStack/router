febd5203baa82e24db607f58fcb5430fedf0aabd
Tanner Linsley <tannerlinsley@gmail.com>
2025-09-23 18:26:37 +0200

    Punted on global middleware types and cleaned up start-basic example

--- examples/react/start-basic/src/routes/index.tsx
+++ examples/react/start-basic/src/routes/index.tsx
@@ -1,76 +1,8 @@
 import { createFileRoute } from '@tanstack/react-router'
-import { createMiddleware, createServerFn } from '@tanstack/react-start'
-import { Foo } from '~/Foo'
-
-const indexMw = createMiddleware().server(({ next }) => {
-  return next({
-    context: {
-      fromIndex: true,
-    },
-  })
-})
 
 export const Route = createFileRoute('/')({
-  server: {
-    middleware: [indexMw],
-    // handlers: ({ createHandlers }) =>
-    // createHandlers({
-    //   GET: ({ next }) => {
-    //     return next({
-    //       context: {
-    //         test: true,
-    //       },
-    //     })
-    //   },
-    //   POST: ({ next }) => {
-    //     return next({
-    //       context: {
-    //         wrong: true,
-    //       },
-    //     })
-    //   },
-    // }),
-    handlers: {
-      GET: ({ next, context }) => {
-        context.fromRequest
-        context.fromIndex
-        return next({
-          context: {
-            fromGet: true,
-          },
-        })
-      },
-      POST: ({ next, context }) => {
-        context.fromRequest
-        context.fromIndex
-        return next({
-          context: {
-            fromPost: true,
-          },
-        })
-      },
-    },
-  },
-  beforeLoad: (ctx) => {
-    ctx.serverContext?.fromRequest
-    ctx.serverContext?.fromIndex
-  },
   component: Home,
-  loader: (ctx) => {
-    ctx.serverContext?.fromRequest
-    ctx.serverContext?.fromIndex
-    return new Foo('hello')
-  },
 })
-
-const fn = createServerFn()
-  .middleware([indexMw])
-  .handler(({ context }) => {
-    context.fromGlobalFnMiddleware
-    context.fromIndex
-    //       ^?
-    return null as any
-  })
 
 function Home() {
   return (
--- examples/react/start-basic/src/start.tsx
+++ examples/react/start-basic/src/start.tsx
@@ -1,12 +1,7 @@
-import {
-  createRouter,
-  createSerializationAdapter,
-} from '@tanstack/react-router'
-import { createMiddleware, createStart } from '@tanstack/react-start'
+import { createRouter } from '@tanstack/react-router'
 import { routeTree } from './routeTree.gen'
 import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
 import { NotFound } from './components/NotFound'
-import { Foo } from './Foo'
 
 export function getRouter() {
   const router = createRouter({
@@ -18,59 +13,4 @@ export function getRouter() {
   })
 
   return router
-}
-
-declare module '@tanstack/react-start' {
-  interface Register {
-    server: {
-      requestContext: {
-        fromRequest?: string
-      }
-    }
-  }
-}
-
-// const someReqMw = createMiddleware().server(({ next, context }) => {
-//   context?.fromRequest
-//   return next({
-//     context: { fromGlobalReqMiddleware: 'hello from the request middleware!' },
-//   })
-// })
-
-const someMw = createMiddleware({ type: 'function' }).server(
-  ({ next, context }) => {
-    context?.fromRequest
-    return next({
-      context: { fromGlobalFnMiddleware: 'hello from the server middleware!' },
-    })
-  },
-)
-
-// createMiddleware().server(({ next, context }) => {
-//   context.fromRequest
-//   context.fromGlobalReqMiddleware
-//   return next()
-// })
-
-createMiddleware({ type: 'function' }).server(({ next, context }) => {
-  context.fromRequest
-  context.fromGlobalReqMiddleware
-  context.fromGlobalFnMiddleware
-  return next()
-})
-
-const fooAdapter = createSerializationAdapter({
-  key: 'foo',
-  test: (value: any) => value instanceof Foo,
-  toSerializable: (foo) => foo.bar(),
-  fromSerializable: (value) => new Foo(value),
-})
-
-export function getStart() {
-  return createStart({
-    defaultSsr: true,
-    serializationAdapters: [fooAdapter],
-    // requestMiddleware: [someReqMw],
-    // functionMiddleware: [someMw],
-  })
 }
