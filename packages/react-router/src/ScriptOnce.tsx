05db3029e7c143b4ac473ba767a725d3526d8019
Manuel Schiller <manuel.schiller@caligano.de>
2025-09-23 18:26:37 +0200

    ScriptOnce uses nonce

--- packages/react-router/src/ScriptOnce.tsx
+++ packages/react-router/src/ScriptOnce.tsx
@@ -1,16 +1,14 @@
-export function ScriptOnce({
-  children,
-}: {
-  children: string
-  log?: boolean
-  sync?: boolean
-}) {
-  if (typeof document !== 'undefined') {
+import { useRouter } from './useRouter'
+
+export function ScriptOnce({ children }: { children: string }) {
+  const router = useRouter()
+  if (!router.isServer) {
     return null
   }
 
   return (
     <script
+      nonce={router.ssr?.nonce}
       className="$tsr"
       dangerouslySetInnerHTML={{
         __html: [children].filter(Boolean).join('\n') + ';$_TSR.c()',
