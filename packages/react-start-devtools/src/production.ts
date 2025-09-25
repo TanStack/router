1964ef9431ec2c1695d7b4709d74647357c8464a
Alem Tuzlak <t.zlak@hotmail.com>
2025-09-25 15:12:33 +0200

    feat: add start devtools

---
+++ packages/react-start-devtools/src/production.ts
@@ -0,0 +1,7 @@
+'use client'
+
+export { StartDevtoolsPanel } from './ReactStartDevtools'
+
+export type { StartDevtoolsReactInit } from './ReactStartDevtools'
+
+export { startDevtoolsPlugin } from './plugin'
--- packages/react-start/src/default-entry/server.ts
+++ packages/react-start/src/default-entry/server.ts
@@ -2,7 +2,7 @@ import {
   createStartHandler,
   defaultStreamHandler,
 } from '@tanstack/react-start/server'
-import type { Register } from '@tanstack/react-start'
+import type { Register } from '@tanstack/react-router'
 import type { RequestHandler } from '@tanstack/react-start/server'
 
 const fetch = createStartHandler(defaultStreamHandler)
