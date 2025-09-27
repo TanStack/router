478d283d3af3cc023b9c741f1bcadb938b63388e
Manuel Schiller <manuel.schiller@caligano.de>
2025-09-28 01:32:02 +0200

    fix: handle synchronous handler/middleware (#5270)

--- packages/start-server-core/src/createStartHandler.ts
+++ packages/start-server-core/src/createStartHandler.ts
@@ -495,33 +495,36 @@ function executeMiddleware(middlewares: TODO, ctx: TODO) {
     const middleware = middlewares[index]
     if (!middleware) return ctx
 
-    const result = await middleware({
-      ...ctx,
-      // Allow the middleware to call the next middleware in the chain
-      next: async (nextCtx: TODO) => {
-        // Allow the caller to extend the context for the next middleware
-        const nextResult = await next({
-          ...ctx,
-          ...nextCtx,
-          context: {
-            ...ctx.context,
-            ...(nextCtx?.context || {}),
-          },
-        })
+    let result
+    try {
+      result = await middleware({
+        ...ctx,
+        // Allow the middleware to call the next middleware in the chain
+        next: async (nextCtx: TODO) => {
+          // Allow the caller to extend the context for the next middleware
+          const nextResult = await next({
+            ...ctx,
+            ...nextCtx,
+            context: {
+              ...ctx.context,
+              ...(nextCtx?.context || {}),
+            },
+          })
 
-        // Merge the result into the context\
-        return Object.assign(ctx, handleCtxResult(nextResult))
-      },
-      // Allow the middleware result to extend the return context
-    }).catch((err: TODO) => {
+          // Merge the result into the context\
+          return Object.assign(ctx, handleCtxResult(nextResult))
+        },
+        // Allow the middleware result to extend the return context
+      })
+    } catch (err: TODO) {
       if (isSpecialResponse(err)) {
-        return {
+        result = {
           response: err,
         }
+      } else {
+        throw err
       }
-
-      throw err
-    })
+    }
 
     // Merge the middleware result into the context, just in case it
     // returns a partial context
