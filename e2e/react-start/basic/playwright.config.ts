090b0c773c85cb2a0f21dd3d66d34adc3a64b16f
Nico Lynzaad <nlynzaad@zylangroup.com>
2025-09-28 23:42:29 +0200

    nitpick

--- e2e/react-start/basic/playwright.config.ts
+++ e2e/react-start/basic/playwright.config.ts
@@ -3,7 +3,7 @@ import {
   getDummyServerPort,
   getTestServerPort,
 } from '@tanstack/router-e2e-utils'
-import { isSpaMode } from 'tests/utils/isSpaMode'
+import { isSpaMode } from './tests/utils/isSpaMode'
 import packageJson from './package.json' with { type: 'json' }
 
 const PORT = await getTestServerPort(
