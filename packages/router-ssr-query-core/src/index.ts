241100be000418ad738e3a9ea952051f7bb4c8d7
Ryan Gillie <gillie.ryan@hotmail.com>
2025-10-05 19:42:42 -0400

    Only use error, spread rest of parameters

--- packages/router-ssr-query-core/src/index.ts
+++ packages/router-ssr-query-core/src/index.ts
@@ -133,31 +133,26 @@ export function setupCoreRouterSsrQueryIntegration<TRouter extends AnyRouter>({
       const ogMutationCacheConfig = queryClient.getMutationCache().config
       queryClient.getMutationCache().config = {
         ...ogMutationCacheConfig,
-        onError: (error, _variables, _context, _mutation) => {
+        onError: (error, ...rest) => {
           if (isRedirect(error)) {
             error.options._fromLocation = router.state.location
             return router.navigate(router.resolveRedirect(error).options)
           }
 
-          return ogMutationCacheConfig.onError?.(
-            error,
-            _variables,
-            _context,
-            _mutation,
-          )
+          return ogMutationCacheConfig.onError?.(error, ...rest)
         },
       }
 
       const ogQueryCacheConfig = queryClient.getQueryCache().config
       queryClient.getQueryCache().config = {
         ...ogQueryCacheConfig,
-        onError: (error, _query) => {
+        onError: (error, ...rest) => {
           if (isRedirect(error)) {
             error.options._fromLocation = router.state.location
             return router.navigate(router.resolveRedirect(error).options)
           }
 
-          return ogQueryCacheConfig.onError?.(error, _query)
+          return ogQueryCacheConfig.onError?.(error, ...rest)
         },
       }
     }
