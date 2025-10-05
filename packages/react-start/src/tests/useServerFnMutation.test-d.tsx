1a7438d890d7675fcdefc3177fa7859c1ccf14e4
Christopher Menendez <christophermenendezmorales@gmail.com>
2025-10-05 21:22:28 +1100

    clean up

--- packages/react-start/src/tests/useServerFnMutation.test-d.tsx
+++ packages/react-start/src/tests/useServerFnMutation.test-d.tsx
@@ -3,23 +3,39 @@ import { createServerFn, useServerFn } from '../index'
 
 const serverFn = createServerFn({ method: 'POST' })
   .inputValidator((input: { name: string }) => input)
-  .handler(async ({ data }) => ({
-    message: `Hello ${data.name}!`,
-  }))
+  .handler(async ({ data }) => {
+    return await Promise.resolve({
+      message: `Hello ${data.name}!`,
+    })
+  })
 
-useMutation({
-  mutationFn: useServerFn(serverFn),
-  onSuccess: (data) => {
-    data.message
-  },
-})
+const optionalServerFn = createServerFn()
+  .handler(async () => {
+    return await Promise.resolve({
+      ok: true as const,
+    })
+  })
 
-const optionalServerFn = createServerFn().handler(async () => ({
-  ok: true as const,
-}))
+export function UseServerFnMutationRegressionComponent() {
+  const mutation = useMutation({
+    mutationFn: useServerFn(serverFn),
+    onSuccess: (data, variables) => {
+      data.message
+      variables?.data?.name
+    },
+  })
 
-useServerFn(optionalServerFn)().then((result) => {
-  result.ok
-})
+  void mutation
+  return null
+}
 
-useServerFn(optionalServerFn)({ headers: { 'x-test': '1' } })
+export function useOptionalServerFnRegressionHook() {
+  const optionalHandler = useServerFn(optionalServerFn)
+
+  optionalHandler().then((result) => {
+    result.ok
+  })
+
+  void optionalHandler()
+  void optionalHandler(undefined)
+}
