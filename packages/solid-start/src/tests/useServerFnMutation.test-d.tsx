745afd3bd75917b5d3ded3bb55f513d6614b309f
Christopher Menendez <christophermenendezmorales@gmail.com>
2025-10-05 21:23:10 +1100

    added solid test

---
+++ packages/solid-start/src/tests/useServerFnMutation.test-d.tsx
@@ -0,0 +1,38 @@
+import { createServerFn, useServerFn } from '../index'
+
+const serverFn = createServerFn({ method: 'POST' })
+  .inputValidator((input: { name: string }) => input)
+  .handler(async ({ data }) => {
+    return await Promise.resolve({
+      message: `Hello ${data.name}!`,
+    })
+  })
+
+const optionalServerFn = createServerFn()
+  .handler(async () => {
+    return await Promise.resolve({
+      ok: true as const,
+    })
+  })
+
+export function UseServerFnRegressionComponent() {
+  const handler = useServerFn(serverFn)
+
+  handler({ data: { name: 'TanStack' } }).then((result) => {
+    result.message
+  })
+
+  void handler
+  return null
+}
+
+export function useOptionalServerFnRegressionHook() {
+  const handler = useServerFn(optionalServerFn)
+
+  handler().then((result) => {
+    result.ok
+  })
+
+  void handler()
+  void handler(undefined)
+}
