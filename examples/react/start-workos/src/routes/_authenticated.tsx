d5f2e61f32f71560fe1974b6d7a2d576ddf1083b
Nick Nisi <nick.nisi@workos.com>
2025-10-07 08:50:21 -0500

    clean up and fixes

--- examples/react/start-workos/src/routes/_authenticated.tsx
+++ examples/react/start-workos/src/routes/_authenticated.tsx
@@ -1,4 +1,4 @@
-import { redirect, createFileRoute } from '@tanstack/react-router';
+import { createFileRoute, redirect } from '@tanstack/react-router';
 import { getAuth, getSignInUrl } from '@workos/authkit-tanstack-react-start';
 
 export const Route = createFileRoute('/_authenticated')({
