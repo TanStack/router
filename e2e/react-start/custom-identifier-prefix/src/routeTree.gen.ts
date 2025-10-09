c268da57f1a7c86239532f1d28fd35f2165d444f
Hokki Suwanda <hokkyss2@gmail.com>
2025-10-09 19:42:20 +0700

    e2e(start-custom-identifier-prefix) : remove file extension from export

--- e2e/react-start/custom-identifier-prefix/src/routeTree.gen.ts
+++ e2e/react-start/custom-identifier-prefix/src/routeTree.gen.ts
@@ -58,7 +58,7 @@ export const routeTree = rootRouteImport
   ._addFileChildren(rootRouteChildren)
   ._addFileTypes<FileRouteTypes>()
 
-import type { getRouter } from './router.tsx'
+import type { getRouter } from './router'
 import type { createStart } from '@tanstack/react-start'
 declare module '@tanstack/react-start' {
   interface Register {
