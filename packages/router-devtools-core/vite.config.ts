7582b93a39865ad50c7283f36e04e743eb0a8a4a
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-10-09 08:42:37 -0700

    fix(router-devtools-core): actually bundle solid

--- packages/router-devtools-core/vite.config.ts
+++ packages/router-devtools-core/vite.config.ts
@@ -15,6 +15,28 @@ const merged = mergeConfig(
   }),
 )
 
+// TODO: update tanstackViteConfig to expose the except option from vite-plugin-externalize-deps
+// so that we don't have to do this dance to bundle something
+merged.plugins.push({
+  name: 'vite-plugin-override-vite-plugin-externalize-deps',
+  config: (config: UserConfig) => {
+    if (typeof config?.build?.rollupOptions?.external !== 'function') {
+      // this can never happen, but we need to make typescript happy
+      return
+    }
+    const existing = config.build.rollupOptions.external
+    config.build.rollupOptions.external = (
+      id: string,
+      parentId?: string,
+      isResolved?: boolean,
+    ) => {
+      if (id === 'solid-js') return false
+      return existing(id, parentId, isResolved ?? false)
+    }
+    return config
+  },
+})
+
 merged.build.rollupOptions.output.manualChunks = false
 merged.build.rollupOptions.output.preserveModules = false
 
