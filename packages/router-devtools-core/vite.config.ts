186cfee77b9b015a9d723920071eeae91db666a0
Ben McCann <322311+benmccann@users.noreply.github.com>
2025-10-11 08:18:25 -0700

    cleanup

--- packages/router-devtools-core/vite.config.ts
+++ packages/router-devtools-core/vite.config.ts
@@ -12,30 +12,9 @@ const merged = mergeConfig(
   tanstackViteConfig({
     entry: './src/index.tsx',
     srcDir: './src',
+    bundledDeps: 'solid-js',
   }),
 )
-
-// TODO: update tanstackViteConfig to expose the except option from vite-plugin-externalize-deps
-// so that we don't have to do this dance to bundle something
-merged.plugins.push({
-  name: 'vite-plugin-override-vite-plugin-externalize-deps',
-  config: (config: UserConfig) => {
-    if (typeof config?.build?.rollupOptions?.external !== 'function') {
-      // this can never happen, but we need to make typescript happy
-      return
-    }
-    const existing = config.build.rollupOptions.external
-    config.build.rollupOptions.external = (
-      id: string,
-      parentId?: string,
-      isResolved?: boolean,
-    ) => {
-      if (id === 'solid-js') return false
-      return existing(id, parentId, isResolved ?? false)
-    }
-    return config
-  },
-})
 
 merged.build.rollupOptions.output.manualChunks = false
 merged.build.rollupOptions.output.preserveModules = false
