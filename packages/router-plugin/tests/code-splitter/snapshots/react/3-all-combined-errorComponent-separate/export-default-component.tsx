7a7f1f5f79d6ed20160984b0924fac9266fec65e
Manuel Schiller <manuel.schiller@caligano.de>
2025-10-13 01:12:46 +0200

    fix: only transform files in codesplitter if changes were made

--- packages/router-plugin/tests/code-splitter/snapshots/react/3-all-combined-errorComponent-separate/export-default-component.tsx
+++ packages/router-plugin/tests/code-splitter/snapshots/react/3-all-combined-errorComponent-separate/export-default-component.tsx
@@ -1,12 +1,16 @@
-console.warn("[tanstack-router] These exports from \"export-default-component.tsx\" will not be code-split and will increase your bundle size:\n- Home\nFor the best optimization, these items should either have their export statements removed, or be imported from another location that is not a route file.");
-import React, { useState } from 'react';
-import { createFileRoute } from '@tanstack/react-router';
+import React, { useState } from 'react'
+import { createFileRoute } from '@tanstack/react-router'
+
 export const Route = createFileRoute('/home')({
-  component: Home
-});
+  component: Home,
+})
+
 export default function Home() {
-  const [one, setOne] = useState('this is from a state');
-  return <div>
+  const [one, setOne] = useState('this is from a state')
+
+  return (
+    <div>
       <h1>{one}</h1>
-    </div>;
+    </div>
+  )
 }
