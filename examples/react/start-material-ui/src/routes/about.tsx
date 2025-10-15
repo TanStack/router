6bdc64a73949cfec69e2093836b96d519ffcc4f7
evanweible-wf <evan.weible@workiva.com>
2025-10-15 12:31:07 -0600

    Update TanStack Start + Material UI example to repro streaming hydration mismatch

--- examples/react/start-material-ui/src/routes/about.tsx
+++ examples/react/start-material-ui/src/routes/about.tsx
@@ -1,10 +1,54 @@
+import { useSuspenseQuery } from '@tanstack/react-query'
 import { createFileRoute } from '@tanstack/react-router'
 import { Typography } from '@mui/material'
+import { Suspense } from 'react'
+
+const isStreaming = true
+
+const aboutQueryOptions = {
+  queryKey: ['about'],
+  queryFn: () =>
+    new Promise((resolve) => setTimeout(() => resolve('Data!'), 1000)),
+}
 
 export const Route = createFileRoute('/about')({
+  loader: async ({ context }) => {
+    const prefetch = context.queryClient.prefetchQuery(aboutQueryOptions)
+
+    // To demonstrate streaming, we return without awaiting.
+    // To demonstrate prefetching without streaming, we await the prefetch.
+    if (!isStreaming) {
+      await prefetch
+    }
+  },
+
   component: RouteComponent,
 })
 
 function RouteComponent() {
-  return <Typography variant="h2">About</Typography>
+  return (
+    <>
+      <Typography variant="h2">About</Typography>
+      <Suspense fallback={<AboutLoading />}>
+        <AboutData />
+      </Suspense>
+    </>
+  )
+}
+
+function AboutLoading() {
+  return (
+    <Typography variant="body1" sx={{ color: 'text.secondary' }}>
+      Loading data...
+    </Typography>
+  )
+}
+
+function AboutData() {
+  useSuspenseQuery(aboutQueryOptions)
+  return (
+    <Typography variant="body1" sx={{ color: '#357a38' }}>
+      Data loaded!
+    </Typography>
+  )
 }
