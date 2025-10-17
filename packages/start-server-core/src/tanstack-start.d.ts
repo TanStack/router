a69f375c9538f0d2ccdc022c68582c4015a6eba5
Manuel Schiller <manuel.schiller@caligano.de>
2025-10-17 19:41:23 +0200

    fix: no invalidation for server functions in dev

--- packages/start-server-core/src/tanstack-start.d.ts
+++ packages/start-server-core/src/tanstack-start.d.ts
@@ -10,18 +10,9 @@ declare module 'tanstack-start-route-tree:v' {
   export const routeTree: AnyRoute | undefined
 }
 
-declare module 'tanstack-start-server-fn-manifest:v' {
-  const serverFnManifest: Record<
-    string,
-    {
-      importer: () => Promise<
-        Record<string, (...args: Array<any>) => Promise<any>> | undefined
-      >
-      functionName: string
-    }
-  >
-
-  export default serverFnManifest
+declare module '#tanstack-start-server-fn-manifest' {
+  type ServerFn = (...args: Array<any>) => Promise<any>
+  export async function getServerFnById(id: string): Promise<ServerFn>
 }
 
 declare module 'tanstack-start-injected-head-scripts:v' {
