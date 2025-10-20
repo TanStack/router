4780458d6e54a3b7cdbef09d32dd307a3b858211
Jamie Davenport <1329874+jamiedavenport@users.noreply.github.com>
2025-10-20 21:28:08 +0100

    Improve the server entrypoint tracing

--- examples/react/start-otel/src/server.ts
+++ examples/react/start-otel/src/server.ts
@@ -8,13 +8,19 @@ export default {
     return tracer.startActiveSpan(
       `${request.method} ${new URL(request.url).pathname}`,
       async (span) => {
-        span.setAttribute('http.method', request.method)
-        span.setAttribute('http.url', request.url)
-        span.setAttribute('http.route', new URL(request.url).pathname)
-        const response = await handler.fetch(request)
-        span.setAttribute('http.status_code', response.status)
-        span.end()
-        return response
+        try {
+          span.setAttribute('http.method', request.method)
+          span.setAttribute('http.url', request.url)
+          span.setAttribute('http.route', new URL(request.url).pathname)
+          const response = await handler.fetch(request)
+          span.setAttribute('http.status_code', response.status)
+          return response
+        } catch (error: any) {
+          span.recordException(error)
+          throw error
+        } finally {
+          span.end()
+        }
       },
     )
   },
