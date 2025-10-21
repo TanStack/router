c05a708d0d8b50168a35a14dab06093f1b6a8921
Brenley Dueck <brenleydueck@gmail.com>
2025-10-21 09:08:35 -0500

    fix: solid start error outside monorepo

--- packages/solid-start/src/default-entry/client.tsx
+++ packages/solid-start/src/default-entry/client.tsx
@@ -1,6 +1,5 @@
 import { hydrate } from 'solid-js/web'
-import { hydrateStart } from '@tanstack/start-client-core/client'
-import { StartClient } from '@tanstack/solid-start/client'
+import { StartClient, hydrateStart } from '@tanstack/solid-start/client'
 
 hydrateStart().then((router) => {
   hydrate(() => <StartClient router={router} />, document)
