2852acb3385b266e23c1650e6b2453182120c962
Birk Skyum <birk.skyum@pm.me>
2025-10-28 04:09:49 +0100

    add test

--- e2e/solid-router/basic/src/main.tsx
+++ e2e/solid-router/basic/src/main.tsx
@@ -71,7 +71,23 @@ function RootComponent() {
           }}
         >
           This Route Does Not Exist
-        </Link>
+        </Link>{' '}
+        <div class="flex items-center">
+          <svg width="20" height="20" viewBox="0 0 20 20" role="img">
+            <title id="rectTitle">Link in SVG</title>
+            <Link to="/posts" aria-label="Open posts from SVG">
+              <rect
+                x="0"
+                y="0"
+                width="20"
+                height="20"
+                rx="4"
+                fill="blue"
+                stroke-width="2"
+              />
+            </Link>
+          </svg>
+        </div>
       </div>
       <Outlet />
       <TanStackRouterDevtools position="bottom-right" />
