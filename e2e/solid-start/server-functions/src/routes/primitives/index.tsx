9d6c63ba4cd1d61d0f3ac04da538c73c1e82c14f
Brenley Dueck <brenleydueck@gmail.com>
2025-10-27 21:28:09 -0500

    test(solid-start): fix solid-query when used in ssr (#5665)
    * fix: ssr query for solid
    
    * Update query-core
    
    * fix solid ssr true
    
    * reset lockfile
    
    * set experimental_prefetchInRender: true
    
    * ci: apply automated fixes
    
    * reset lock
    
    ---------
    
    Co-authored-by: Birk Skyum <birk.skyum@pm.me>
    Co-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>

--- e2e/solid-start/server-functions/src/routes/primitives/index.tsx
+++ e2e/solid-start/server-functions/src/routes/primitives/index.tsx
@@ -5,7 +5,7 @@ import { For, Show } from 'solid-js'
 import { z } from 'zod'
 export const Route = createFileRoute('/primitives/')({
   component: RouteComponent,
-  ssr: 'data-only',
+  ssr: true,
 })
 
 function stringify(data: any) {
