c8a37284107797aa9da8fa3f36f84fce3965902b
Birk Skyum <birk.skyum@pm.me>
2025-10-30 00:42:59 +0100

    fix react test

--- packages/react-router/tests/store-updates-during-navigation.test.tsx
+++ packages/react-router/tests/store-updates-during-navigation.test.tsx
@@ -154,7 +154,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // This number should be as small as possible to minimize the amount of work
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
-    expect(updates).toBe(5)
+    expect(updates).toBe(4)
   })
 
   test('sync beforeLoad', async () => {
