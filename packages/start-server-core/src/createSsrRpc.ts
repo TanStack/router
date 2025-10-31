f549563a87f3432e3751d0d96ab9f45aadb7f820
Manuel Schiller <manuel.schiller@caligano.de>
2025-10-31 23:24:03 +0100

    forward all params

--- packages/start-server-core/src/createSsrRpc.ts
+++ packages/start-server-core/src/createSsrRpc.ts
@@ -3,9 +3,9 @@ import { getServerFnById } from './getServerFnById'
 
 export const createSsrRpc = (functionId: string) => {
   const url = process.env.TSS_SERVER_FN_BASE + functionId
-  const fn = async (opts: any, signal: any): Promise<any> => {
+  const fn = async (...args: Array<any>): Promise<any> => {
     const serverFn = await getServerFnById(functionId)
-    return serverFn(opts ?? {}, signal)
+    return serverFn(...args)
   }
 
   return Object.assign(fn, {
