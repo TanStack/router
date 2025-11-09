01682c5dfbcfc0e75d192dd8de0b1e8f116d419a
Birk Skyum <74932975+birkskyum@users.noreply.github.com>
2025-11-09 19:52:29 +0100

    fix(solid-router): use Solid.useTransition (#5801)

--- packages/solid-router/tests/store-updates-during-navigation.test.tsx
+++ packages/solid-router/tests/store-updates-during-navigation.test.tsx
@@ -137,7 +137,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
     // Note: Solid has different update counts than React due to different reactivity
-    expect(updates).toBe(13)
+    expect(updates).toBe(10)
   })
 
   test('redirection in preload', async () => {
@@ -173,7 +173,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
     // Note: Solid has different update counts than React due to different reactivity
-    expect(updates).toBe(12)
+    expect(updates).toBe(9)
   })
 
   test('nothing', async () => {
@@ -200,7 +200,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // This number should be as small as possible to minimize the amount of work
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
-    expect(updates).toBe(8)
+    expect(updates).toBe(7)
   })
 
   test('hover preload, then navigate, w/ async loaders', async () => {
