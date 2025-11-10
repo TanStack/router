1bea0990a2b29bc4ab7ae71895618604923a78f4
Brenley Dueck <brenleydueck@gmail.com>
2025-11-09 21:11:49 -0600

    setup start-clerk-basic for solid

---
+++ examples/solid/start-clerk-basic/src/start.ts
@@ -0,0 +1,8 @@
+import { clerkMiddleware } from '@clerk/tanstack-react-start/server'
+import { createStart } from '@tanstack/solid-start'
+
+export const startInstance = createStart(() => {
+  return {
+    requestMiddleware: [clerkMiddleware()],
+  }
+})
