1efc59d123971e375d4227302f50cd1f1e07906a
Brenley Dueck <brenleydueck@gmail.com>
2025-11-09 22:31:38 -0600

    tweaks

--- examples/solid/start-clerk-basic/src/routes/_authed.tsx
+++ examples/solid/start-clerk-basic/src/routes/_authed.tsx
@@ -1,5 +1,5 @@
 import { createFileRoute } from '@tanstack/solid-router'
-import { SignIn } from '@clerk/tanstack-react-start'
+import { SignIn } from '@clerk/tanstack-solid-start'
 
 export const Route = createFileRoute('/_authed')({
   beforeLoad: ({ context }) => {
