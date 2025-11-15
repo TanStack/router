a272ef03d94d0370fde0029178bb984e7b88c8e0
Manuel Schiller <manuel.schiller@caligano.de>
2025-11-15 01:32:06 +0100

    fix: cleanup streamed values

--- packages/router-core/src/ssr/constants.ts
+++ packages/router-core/src/ssr/constants.ts
@@ -1,1 +1,2 @@
 export const GLOBAL_TSR = '$_TSR'
+export declare const GLOBAL_SEROVAL = '$R'
--- packages/router-core/src/ssr/tsrScript.ts
+++ packages/router-core/src/ssr/tsrScript.ts
@@ -3,6 +3,10 @@ self.$_TSR = {
     document.querySelectorAll('.\\$tsr').forEach((o) => {
       o.remove()
     })
+    if (this.hydrated && this.streamEnd) {
+      delete self.$_TSR
+      delete self.$R['tsr']
+    }
   },
   p(script) {
     !this.initialized ? this.buffer.push(script) : script()
