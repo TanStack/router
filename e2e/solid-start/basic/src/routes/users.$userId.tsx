7cfa14ee749511ed94754661d6ad4b74d530a4fa
Manuel Schiller <manuel.schiller@caligano.de>
2025-11-19 00:44:18 +0100

    fix tests to not rely on fetch patch serverside

--- e2e/solid-start/basic/src/routes/users.$userId.tsx
+++ e2e/solid-start/basic/src/routes/users.$userId.tsx
@@ -1,14 +1,16 @@
 import { createFileRoute } from '@tanstack/solid-router'
 import axios from 'redaxios'
 
+import { getRouterInstance } from '@tanstack/solid-start'
 import type { User } from '~/utils/users'
 import { NotFound } from '~/components/NotFound'
 import { UserErrorComponent } from '~/components/UserErrorComponent'
 
 export const Route = createFileRoute('/users/$userId')({
   loader: async ({ params: { userId } }) => {
+    const router = await getRouterInstance()
     return await axios
-      .get<User>('/api/users/' + userId)
+      .get<User>('/api/users/' + userId, { baseURL: router.options.origin })
       .then((r) => r.data)
       .catch(() => {
         throw new Error('Failed to fetch user')
