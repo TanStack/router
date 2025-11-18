9c2a094955b3816ae4dd7e20a01a58f9816994b8
Hakan Shehu <hakanshehu15@gmail.com>
2025-11-17 17:13:23 -0800

    Include the necessary changes

--- packages/router-core/src/router.ts
+++ packages/router-core/src/router.ts
@@ -1787,15 +1787,28 @@ export class RouterCore<
           )
           if (match) {
             Object.assign(params, match.params) // Copy params, because they're cached
-            const { from: _from, ...maskProps } = match.route
+            const {
+              from: _from,
+              params: maskParams,
+              ...maskProps
+            } = match.route
+
+            // If mask has a params function, call it with the matched params as context
+            // Otherwise, use the matched params or the provided params value
+            const nextParams =
+              maskParams === false || maskParams === null
+                ? {}
+                : (maskParams ?? true) === true
+                  ? params
+                  : Object.assign(params, functionalUpdate(maskParams, params))
+
             maskedDest = {
               from: opts.from,
               ...maskProps,
-              params,
+              params: nextParams,
             }
             maskedNext = build(maskedDest)
           }
-        }
         }
       }
 
@@ -2097,7 +2110,6 @@ export class RouterCore<
             updateMatch: this.updateMatch,
             // eslint-disable-next-line @typescript-eslint/require-await
             onReady: async () => {
-              // eslint-disable-next-line @typescript-eslint/require-await
               // Wrap batch in framework-specific transition wrapper (e.g., Solid's startTransition)
               this.startTransition(() => {
                 this.startViewTransition(async () => {
