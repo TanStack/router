a99dfdbb3cad80596cb9c1a36b44884116453eac
Birk Skyum <birk.skyum@pm.me>
2025-11-19 22:58:31 +0100

    update solid-start auth example to prisma 7

--- examples/solid/start-basic-auth/src/utils/prisma.ts
+++ examples/solid/start-basic-auth/src/utils/prisma.ts
@@ -1,7 +1,14 @@
 import crypto from 'node:crypto'
 import { PrismaClient } from '@prisma/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
+import { createClient } from '@libsql/client'
 
-export const prismaClient = new PrismaClient()
+const libsql = createClient({
+  url: process.env.DATABASE_URL || 'file:./prisma/dev.db',
+})
+
+const adapter = new PrismaLibSQL(libsql)
+export const prismaClient = new PrismaClient({ adapter })
 
 export function hashPassword(password: string) {
   return new Promise<string>((resolve, reject) => {
