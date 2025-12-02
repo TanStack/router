9bf5227a585e9568453f997f008e1b37ddbcec8b
Birk Skyum <birk.skyum@pm.me>
2025-12-02 23:24:04 +0100

    add vue logo counter and vue-tsc

--- e2e/vue-router/basic-file-based-jsx/src/routes/index.tsx
+++ e2e/vue-router/basic-file-based-jsx/src/routes/index.tsx
@@ -1,4 +1,5 @@
 import { createFileRoute } from '@tanstack/vue-router'
+import VueLogo from '../components/VueLogo.vue'
 
 export const Route = createFileRoute('/')({
   component: IndexComponent,
@@ -8,6 +9,7 @@ function IndexComponent() {
   return (
     <div class="p-2">
       <h3>Welcome Home!</h3>
+      <VueLogo />
     </div>
   )
 }
