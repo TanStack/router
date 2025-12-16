b95ab7fbd73958e07ee4bb29d269b7a07aebd955
Birk Skyum <birk.skyum@pm.me>
2025-12-16 19:28:10 +0100

    disable tailwind

--- e2e/vue-router/basic-esbuild-file-based/src/posts.tsx
+++ e2e/vue-router/basic-esbuild-file-based/src/posts.tsx
@@ -7,17 +7,11 @@ export type PostType = {
   body: string
 }
 
-let queryURL = 'https://jsonplaceholder.typicode.com'
-
-if (import.meta.env.VITE_NODE_ENV === 'test') {
-  queryURL = `http://localhost:${import.meta.env.VITE_EXTERNAL_PORT}`
-}
-
 export const fetchPost = async (postId: string) => {
   console.info(`Fetching post with id ${postId}...`)
   await new Promise((r) => setTimeout(r, 500))
   const post = await axios
-    .get<PostType>(`${queryURL}/posts/${postId}`)
+    .get<PostType>(`https://jsonplaceholder.typicode.com/posts/${postId}`)
     .then((r) => r.data)
     .catch((err) => {
       if (err.status === 404) {
@@ -33,6 +27,6 @@ export const fetchPosts = async () => {
   console.info('Fetching posts...')
   await new Promise((r) => setTimeout(r, 500))
   return axios
-    .get<Array<PostType>>(`${queryURL}/posts`)
+    .get<Array<PostType>>('https://jsonplaceholder.typicode.com/posts')
     .then((r) => r.data.slice(0, 10))
 }
