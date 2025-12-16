1878cdebd975759cf933822a1218b44603f11cca
Birk Skyum <birk.skyum@pm.me>
2025-12-16 19:14:54 +0100

    esbuild

---
+++ e2e/vue-router/basic-esbuild-file-based/src/esbuild.config.js
@@ -0,0 +1,13 @@
+import { tanstackRouter } from '@tanstack/router-plugin/esbuild'
+import { vuePlugin } from 'esbuild-plugin-vue3'
+
+export default {
+  // ...
+  plugins: [
+    tanstackRouter({
+      target: 'vue',
+      autoCodeSplitting: true,
+    }),
+    vuePlugin(),
+  ],
+}
