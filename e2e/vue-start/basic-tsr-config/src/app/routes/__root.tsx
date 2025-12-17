9408cb4639e5bfc557980256c5fa7c1e59f2aeae
Birk Skyum <birk.skyum@pm.me>
2025-12-17 23:34:31 +0100

    vue-start/basic-tsr-config

---
+++ e2e/vue-start/basic-tsr-config/src/app/routes/__root.tsx
@@ -0,0 +1,42 @@
+import {
+  HeadContent,
+  Outlet,
+  Scripts,
+  createRootRoute,
+} from '@tanstack/solid-router'
+import { TanStackRouterDevtools } from '@tanstack/solid-router-devtools'
+import { HydrationScript } from 'solid-js/web'
+
+export const Route = createRootRoute({
+  head: () => ({
+    meta: [
+      {
+        charset: 'utf-8',
+      },
+      {
+        name: 'viewport',
+        content: 'width=device-width, initial-scale=1',
+      },
+      {
+        title: 'TanStack Start Starter',
+      },
+    ],
+  }),
+  component: RootComponent,
+})
+
+function RootComponent() {
+  return (
+    <html>
+      <head>
+        <HydrationScript />
+      </head>
+      <body>
+        <HeadContent />
+        <Outlet />
+        <TanStackRouterDevtools position="bottom-right" />
+        <Scripts />
+      </body>
+    </html>
+  )
+}
