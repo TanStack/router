ba11b8a069838569b4975b8210fbf06c8517fdc2
Birk Skyum <birk.skyum@pm.me>
2025-12-17 19:29:02 +0100

    use vue-query

--- e2e/vue-start/server-routes/src/router.tsx
+++ e2e/vue-start/server-routes/src/router.tsx
@@ -1,6 +1,6 @@
 import { createRouter } from '@tanstack/vue-router'
 import { setupRouterSsrQueryIntegration } from '@tanstack/vue-router-ssr-query'
-import { QueryClient } from '@tanstack/solid-query'
+import { QueryClient } from '@tanstack/vue-query'
 import { routeTree } from './routeTree.gen'
 import { DefaultCatchBoundary } from './components/DefaultCatchBoundary'
 import { NotFound } from './components/NotFound'
--- e2e/vue-start/server-routes/src/routes/methods/only-any.tsx
+++ e2e/vue-start/server-routes/src/routes/methods/only-any.tsx
@@ -1,6 +1,6 @@
 import { createFileRoute } from '@tanstack/vue-router'
-import { useQuery } from '@tanstack/solid-query'
-import { For } from 'solid-js'
+import { useQuery } from '@tanstack/vue-query'
+import { defineComponent } from 'vue'
 
 export const Route = createFileRoute('/methods/only-any')({
   ssr: false,
@@ -18,47 +18,63 @@ const HttpMethods = [
 ] as const
 type HttpMethods = (typeof HttpMethods)[number]
 
-function Test(props: { method: HttpMethods }) {
-  const query = useQuery(() => ({
-    queryKey: [props.method],
-    queryFn: async () => {
-      const response = await fetch(`/api/only-any`, {
-        method: props.method,
-      })
-      try {
-        const json = (await response.json()) as Promise<{
-          method: HttpMethods
-          handler: HttpMethods & 'ANY'
-        }>
-        return json
-      } catch (e) {}
-      // handle HEAD and OPTIONS that have no body
-      const result = {
-        handler: response.headers.get('x-handler') as HttpMethods & 'ANY',
-        method: response.headers.get('x-method') as HttpMethods,
-      }
-      return result
-    },
-  }))
-
-  return (
-    <div>
-      <h3>method={props.method}</h3>
-      <h4> expected </h4>
-      <div data-testid={`expected-${props.method}`}>{props.method}</div>
-      <h4> result</h4>
-      {query.data ? (
-        <div data-testid={`result-${props.method}`}>{query.data.method}</div>
-      ) : null}
-    </div>
-  )
+type OnlyAnyApiResponse = {
+  method: HttpMethods
+  handler: 'ANY'
 }
+
+const Test = defineComponent({
+  name: 'OnlyAnyTest',
+  props: {
+    method: {
+      type: String,
+      required: true,
+    },
+  },
+  setup(props) {
+    const query = useQuery(() => ({
+      queryKey: ['only-any', props.method],
+      queryFn: async () => {
+        const method = props.method as HttpMethods
+        const response = await fetch(`/api/only-any`, { method })
+
+        try {
+          return (await response.json()) as OnlyAnyApiResponse
+        } catch {
+          // handle HEAD and OPTIONS that have no body
+          return {
+            handler: (response.headers.get('x-handler') ??
+              'ANY') as OnlyAnyApiResponse['handler'],
+            method: (response.headers.get('x-method') ??
+              method) as OnlyAnyApiResponse['method'],
+          }
+        }
+      },
+    }))
+
+    return () => (
+      <div>
+        <h3>method={props.method}</h3>
+        <h4> expected </h4>
+        <div data-testid={`expected-${props.method}`}>{props.method}</div>
+        <h4> result</h4>
+        {query.data.value ? (
+          <div data-testid={`result-${props.method}`}>
+            {query.data.value.method}
+          </div>
+        ) : null}
+      </div>
+    )
+  },
+})
 
 function RouteComponent() {
   return (
     <div class="p-2 m-2 grid gap-2" data-testid="route-component">
       <h3>Server Route has only ANY handler</h3>
-      <For each={HttpMethods}>{(method) => <Test method={method} />}</For>
+      {HttpMethods.map((method) => (
+        <Test method={method} />
+      ))}
     </div>
   )
 }
