39ee2a4c2dcc2c3288f7dbaed0035be02535f4e1
Birk Skyum <birk.skyum@pm.me>
2025-12-18 00:19:11 +0100

    use vue suspense

--- e2e/vue-start/basic-vue-query/src/routes/posts.tsx
+++ e2e/vue-start/basic-vue-query/src/routes/posts.tsx
@@ -1,44 +1,52 @@
 import { useQuery } from '@tanstack/vue-query'
 import { Link, Outlet, createFileRoute } from '@tanstack/vue-router'
-import { defineComponent } from 'vue'
+import { Suspense, defineComponent } from 'vue'
 import { postsQueryOptions } from '~/utils/posts'
 
-const PostsComponent = defineComponent({
-  setup() {
+const PostsList = defineComponent({
+  async setup() {
     const postsQuery = useQuery(postsQueryOptions())
+    await postsQuery.suspense()
 
     return () => {
-      if (postsQuery.isLoading.value && !postsQuery.data.value) {
-        return (
-          <div class="p-2 flex gap-2">
-            <div>Loading posts...</div>
-          </div>
-        )
-      }
-
       const posts = [
         ...(postsQuery.data.value ?? []),
         { id: 'i-do-not-exist', title: 'Non-existent Post' },
       ]
 
       return (
+        <ul class="list-disc pl-4">
+          {posts.map((post) => (
+            <li class="whitespace-nowrap" key={post.id}>
+              <Link
+                to="/posts/$postId"
+                params={{
+                  postId: post.id,
+                }}
+                class="block py-1 text-blue-800 hover:text-blue-600"
+                activeProps={{ class: 'text-black font-bold' }}
+              >
+                <div>{post.title.substring(0, 20)}</div>
+              </Link>
+            </li>
+          ))}
+        </ul>
+      )
+    }
+  },
+})
+
+const PostsComponent = defineComponent({
+  setup() {
+    return () => {
+      return (
         <div class="p-2 flex gap-2">
-          <ul class="list-disc pl-4">
-            {posts.map((post) => (
-              <li class="whitespace-nowrap" key={post.id}>
-                <Link
-                  to="/posts/$postId"
-                  params={{
-                    postId: post.id,
-                  }}
-                  class="block py-1 text-blue-800 hover:text-blue-600"
-                  activeProps={{ class: 'text-black font-bold' }}
-                >
-                  <div>{post.title.substring(0, 20)}</div>
-                </Link>
-              </li>
-            ))}
-          </ul>
+          <Suspense>
+            {{
+              default: () => <PostsList />,
+              fallback: () => <div>Loading posts...</div>,
+            }}
+          </Suspense>
           <hr />
           <Outlet />
         </div>
