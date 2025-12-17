49beeae5536fbd409ee181df95a821f9371c9e4c
Birk Skyum <birk.skyum@pm.me>
2025-12-17 23:44:47 +0100

    basic-vue-query

---
+++ e2e/vue-start/basic-vue-query/src/components/DefaultCatchBoundary.tsx
@@ -0,0 +1,8 @@
+import {
+  ErrorComponent,
+  type ErrorComponentProps,
+} from '@tanstack/vue-router'
+
+export function DefaultCatchBoundary(props: ErrorComponentProps) {
+  return <ErrorComponent {...props} />
+}
