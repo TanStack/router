7283bedb2760a287cc2f23b5657352c6261e8287
Birk Skyum <birk.skyum@pm.me>
2025-12-18 10:10:38 +0100

    port cloudflare

--- e2e/vue-start/basic-cloudflare/vite.config.ts
+++ e2e/vue-start/basic-cloudflare/vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/vue-start/plugin/vite'
 import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
+import { cloudflare } from '@cloudflare/vite-plugin'
 
 export default defineConfig({
   server: {
