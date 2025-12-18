d8eea71c74b48d943d1ceb227dc46453b34d662f
Birk Skyum <birk.skyum@pm.me>
2025-12-18 11:51:49 +0100

    fix solid-start test suite

--- e2e/vue-start/basic-auth/tests/mock-db-setup.test.ts
+++ e2e/vue-start/basic-auth/tests/mock-db-setup.test.ts
@@ -1,19 +1,16 @@
 import { test as setup } from '@playwright/test'
 
-import { prismaClient } from '../src/utils/prisma'
+import { hashPassword, prismaClient } from '../src/utils/prisma'
 
 setup('create new database', async () => {
-  if (
-    await prismaClient.user.findUnique({
-      where: {
-        email: 'test2@gmail.com',
-      },
-    })
-  ) {
-    await prismaClient.user.delete({
-      where: {
-        email: 'test2@gmail.com',
-      },
-    })
-  }
+  await prismaClient.user.deleteMany()
+
+  const email = 'test@gmail.com'
+  const password = await hashPassword('test')
+  await prismaClient.user.create({
+    data: {
+      email,
+      password,
+    },
+  })
 })
