6c6ec1b884284774ae2796513ce723171f418b6c
Egor Fedorov <fegor2004@gmail.com>
2025-12-31 23:30:16 +0300

    demo: use result.response.headers

---
+++ examples/react/start-bare/src/data/fn.ts
@@ -0,0 +1,31 @@
+import { createMiddleware, createServerFn } from '@tanstack/react-start'
+import { getResponseHeaders } from '@tanstack/react-start/server'
+
+// https://tanstack.com/start/latest/docs/framework/react/guide/isr##using-middleware-for-cache-headers
+export const cacheMiddleware = createMiddleware().server(async ({ next }) => {
+  const result = await next()
+
+  console.log({result, response: result.response})
+  result.response.headers.set('Cache-Control', 'b')
+
+  return result
+})
+
+const data = [
+  { id: 1, name: 'Teenage Dirtbag', artist: 'Wheatus' },
+  { id: 2, name: 'Smells Like Teen Spirit', artist: 'Nirvana' },
+  { id: 3, name: 'The Middle', artist: 'Jimmy Eat World' },
+  { id: 4, name: 'My Own Worst Enemy', artist: 'Lit' },
+  { id: 5, name: 'Fat Lip', artist: 'Sum 41' },
+  { id: 6, name: 'All the Small Things', artist: 'blink-182' },
+  { id: 7, name: 'Beverly Hills', artist: 'Weezer' },
+]
+export const getPunkSongs = createServerFn({
+  method: 'GET',
+})
+  .middleware([cacheMiddleware])
+  .handler(async () => {
+    const headers = getResponseHeaders()
+    headers.set('Cache-Control', 'a')
+    return data
+  })
--- examples/react/start-bare/src/routes/index.tsx
+++ examples/react/start-bare/src/routes/index.tsx
@@ -1,12 +1,22 @@
 import { createFileRoute } from '@tanstack/react-router'
+import { getPunkSongs } from '~/data/fn'
+
 export const Route = createFileRoute('/')({
   component: RouteComponent,
+  loader: async () => {
+    return await getPunkSongs()
+  },
+  // Disable SSR to show the cache headers in cURL requests
+  ssr: false,
 })
 
 function RouteComponent() {
+  const data = Route.useLoaderData()
+
   return (
     <main>
       <h1>Hello world!</h1>
+      <pre>{JSON.stringify(data, null, 2)}</pre>
     </main>
   )
 }
