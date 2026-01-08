e93a4c31693ff6346ab448dd88269ed45d5446e2
Manuel Schiller <manuel.schiller@caligano.de>
2026-01-08 22:48:01 +0100

    examples: make basic runnable with nitro (#6336)

--- examples/solid/start-basic/vite.config.ts
+++ examples/solid/start-basic/vite.config.ts
@@ -3,6 +3,7 @@ import tsConfigPaths from 'vite-tsconfig-paths'
 import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
 import viteSolid from 'vite-plugin-solid'
 import tailwindcss from '@tailwindcss/vite'
+import { nitro } from 'nitro/vite'
 
 export default defineConfig({
   server: {
@@ -15,5 +16,6 @@ export default defineConfig({
     }),
     tanstackStart(),
     viteSolid({ ssr: true }),
+    nitro(),
   ],
 })
