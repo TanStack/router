8a099130f81d3e29178b8f0f0cbb28218d92884a
Nico Lynzaad <nlynzaad@zylangroup.com>
2026-01-18 18:28:36 +0200

    build pathname using url parts

--- packages/start-server-core/src/createStartHandler.ts
+++ packages/start-server-core/src/createStartHandler.ts
@@ -219,7 +219,7 @@ export function createStartHandler<TRegister = Register>(
     try {
       // normalizing and sanitizing the pathname here for server, so we always deal with the same format during SSR.
       const url = getNormalizedURL(request.url)
-      const href = url.href.replace(url.origin, '')
+      const href = url.pathname + url.search + url.hash
       const origin = getOrigin(request)
 
       const entries = await getEntries()
