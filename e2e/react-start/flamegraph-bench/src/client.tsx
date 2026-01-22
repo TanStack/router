ad7a1c36ded1e739bb084b25a53c5bf2dc29ce12
Sheraff <fpellet@ensc.fr>
2026-01-22 21:41:01 +0100

    cleanup

--- e2e/react-start/flamegraph-bench/src/client.tsx
+++ e2e/react-start/flamegraph-bench/src/client.tsx
@@ -1,8 +1,5 @@
 /// <reference types="vite/client" />
 import { hydrateRoot } from 'react-dom/client'
 import { StartClient } from '@tanstack/react-start/client'
-import { getRouter } from './router'
 
-const router = getRouter()
-
-hydrateRoot(document, <StartClient router={router} />)
+hydrateRoot(document, <StartClient />)
--- e2e/react-start/flamegraph-bench/src/routes/page.$id.tsx
+++ e2e/react-start/flamegraph-bench/src/routes/page.$id.tsx
@@ -1,4 +1,4 @@
-import { createFileRoute, Link } from '@tanstack/react-router'
+import { Link, createFileRoute } from '@tanstack/react-router'
 
 export const Route = createFileRoute('/page/$id')({
   component: PageComponent,
