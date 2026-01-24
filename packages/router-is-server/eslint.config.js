4b695ff8371048b63c91dd49188698dec22dbf05
Manuel Schiller <manuel.schiller@caligano.de>
2026-01-24 10:36:08 +0100

    clean lint config

--- packages/router-is-server/eslint.config.js
+++ packages/router-is-server/eslint.config.js
@@ -5,6 +5,11 @@ import rootConfig from '../../eslint.config.js'
 export default [
   ...rootConfig,
   {
-    rules: {},
+    rules: {
+      // We explicitly annotate `isServer: boolean` even though it's inferrable
+      // because the explicit type is important for documentation and ensures
+      // consistent typing across all conditional export files
+      '@typescript-eslint/no-inferrable-types': 'off',
+    },
   },
 ]
--- packages/router-is-server/src/client.ts
+++ packages/router-is-server/src/client.ts
@@ -1,2 +1,1 @@
-// eslint-disable-next-line @typescript-eslint/no-inferrable-types
 export const isServer: boolean = false
