93b83b57a28da50108736de7989fad5fabdece23
wobsoriano <sorianorobertc@gmail.com>
2026-02-04 12:49:28 -0800

    chore(examples): Bump Clerk TanStack version to 0.29.0

--- examples/react/start-clerk-basic/vite.config.ts
+++ examples/react/start-clerk-basic/vite.config.ts
@@ -16,10 +16,4 @@ export default defineConfig({
     tanstackStart(),
     viteReact(),
   ],
-  // See https://github.com/TanStack/router/issues/5738
-  resolve: {
-    alias: [
-      { find: 'use-sync-external-store/shim/index.js', replacement: 'react' },
-    ],
-  },
 })
