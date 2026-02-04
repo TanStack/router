0f8ee4815e0097f3d733f5131f2511a7c6fa4a4c
wobsoriano <sorianorobertc@gmail.com>
2026-02-04 12:50:29 -0800

    chore: add tmp fix

--- e2e/react-start/clerk-basic/vite.config.ts
+++ e2e/react-start/clerk-basic/vite.config.ts
@@ -13,4 +13,10 @@ export default defineConfig({
     tanstackStart(),
     viteReact(),
   ],
+  // See https://github.com/TanStack/router/issues/5738
+  resolve: {
+    alias: [
+      { find: 'use-sync-external-store/shim/index.js', replacement: 'react' },
+    ],
+  },
 })
