f8e77e96681501c8f0867b51dc9fd73e1ed44983
Sheraff <fpellet@ensc.fr>
2026-02-20 18:41:43 +0100

    store updates regression somehow?

--- packages/react-router/tests/store-updates-during-navigation.test.tsx
+++ packages/react-router/tests/store-updates-during-navigation.test.tsx
@@ -136,7 +136,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // This number should be as small as possible to minimize the amount of work
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
-    expect(updates).toBe(6)
+    expect(updates).toBe(8)
   })
 
   test('redirection in preload', async () => {
@@ -170,7 +170,7 @@ describe("Store doesn't update *too many* times during navigation", () => {
     // This number should be as small as possible to minimize the amount of work
     // that needs to be done during a navigation.
     // Any change that increases this number should be investigated.
-    expect(updates).toBe(4)
+    expect(updates).toBe(5)
   })
 
   test('nothing', async () => {
