ebc45ee80d0960b4bb79f276c8dc63f05898debd
github-action-benchmark <github@users.noreply.github.com>
2026-02-21 20:55:15 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 1870dc744f464e5a05efe85b394565e7da40be9b

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771699119038,
+  "lastUpdate": 1771707315928,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -109,6 +109,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/b8f166506d775946f1ba4eec88bf2da4c87e39f6"
         },
         "date": 1771699118371,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 88842,
+            "unit": "bytes",
+            "extra": "raw=279268; brotli=77219"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92002,
+            "unit": "bytes",
+            "extra": "raw=289990; brotli=79934"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 36910,
+            "unit": "bytes",
+            "extra": "raw=110457; brotli=33208"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41359,
+            "unit": "bytes",
+            "extra": "raw=123860; brotli=37125"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53145,
+            "unit": "bytes",
+            "extra": "raw=151409; brotli=47735"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58119,
+            "unit": "bytes",
+            "extra": "raw=167391; brotli=52203"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 101627,
+            "unit": "bytes",
+            "extra": "raw=319193; brotli=87994"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105121,
+            "unit": "bytes",
+            "extra": "raw=329346; brotli=90841"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49439,
+            "unit": "bytes",
+            "extra": "raw=148787; brotli=43722"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55051,
+            "unit": "bytes",
+            "extra": "raw=165122; brotli=48473"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "github@ustark.de",
+            "name": "Ulrich Stark",
+            "username": "ulrichstark"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1870dc744f464e5a05efe85b394565e7da40be9b",
+          "message": "docs: fix grammar and remove duplicate words (#6726)",
+          "timestamp": "2026-02-21T21:53:10+01:00",
+          "tree_id": "3e7f491637a667ef349ab8f00e2059852a932a37",
+          "url": "https://github.com/TanStack/router/commit/1870dc744f464e5a05efe85b394565e7da40be9b"
+        },
+        "date": 1771707315005,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
