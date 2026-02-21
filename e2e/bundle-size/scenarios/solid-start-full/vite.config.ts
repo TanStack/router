d8171c39948fd6c53d16f523868f74c880402583
Flo <fpellet@ensc.fr>
2026-02-21 19:28:30 +0100

    feat(benchmarks): add file-based bundle-size suite + PR trend reporting (#6723)

---
+++ e2e/bundle-size/scenarios/solid-start-full/vite.config.ts
@@ -0,0 +1,7 @@
+import { defineConfig } from 'vite'
+import solid from 'vite-plugin-solid'
+import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
+
+export default defineConfig({
+  plugins: [tanstackStart(), solid({ ssr: true })],
+})
