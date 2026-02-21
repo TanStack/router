6b3b9238be1abec06042f9904af61aa55f7c2098
Birk Skyum <birk.skyum@pm.me>
2026-02-21 02:20:56 +0100

    awaited

--- packages/solid-router/src/awaited.tsx
+++ packages/solid-router/src/awaited.tsx
@@ -24,24 +24,20 @@ export function useAwaited<T>({
   return [promise[TSR_DEFERRED_PROMISE].data, promise]
 }
 
+function InnerAwait<T>(props: { promise: Promise<T>, children: (res: T) => SolidNode }) {
+  const [data] = useAwaited({ promise: props.promise })
+  return props.children(data) as any
+}
+
 export function Await<T>(
   props: AwaitOptions<T> & {
     fallback?: SolidNode
     children: (result: T) => SolidNode
   },
 ) {
-  const [resource] = Solid.createResource(
-    () => defer(props.promise),
-    // Simple passthrough - just return the promise for Solid to await
-    (p) => p,
-    {
-      deferStream: true,
-    },
-  )
-
   return (
-    <Solid.Show fallback={props.fallback} when={resource()}>
-      {(data) => props.children(data())}
-    </Solid.Show>
+    <Solid.Loading fallback={props.fallback as any}>
+      <InnerAwait promise={props.promise}>{props.children}</InnerAwait>
+    </Solid.Loading>
   )
 }
