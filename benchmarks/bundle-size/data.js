a77bca55cac046490aa62d1144cb3ade4d609303
github-action-benchmark <github@users.noreply.github.com>
2026-02-21 21:13:29 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 996e1828441311bc709d5c02aca251d84ca73045

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1771707315928,
+  "lastUpdate": 1771708409000,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -193,6 +193,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/1870dc744f464e5a05efe85b394565e7da40be9b"
         },
         "date": 1771707315005,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 88842,
+            "unit": "bytes",
+            "extra": "raw=279268; brotli=77219"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92002,
+            "unit": "bytes",
+            "extra": "raw=289990; brotli=79934"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 36910,
+            "unit": "bytes",
+            "extra": "raw=110457; brotli=33208"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41359,
+            "unit": "bytes",
+            "extra": "raw=123860; brotli=37125"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53145,
+            "unit": "bytes",
+            "extra": "raw=151409; brotli=47735"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58119,
+            "unit": "bytes",
+            "extra": "raw=167391; brotli=52203"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 101627,
+            "unit": "bytes",
+            "extra": "raw=319193; brotli=87994"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105121,
+            "unit": "bytes",
+            "extra": "raw=329346; brotli=90841"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49439,
+            "unit": "bytes",
+            "extra": "raw=148787; brotli=43722"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55051,
+            "unit": "bytes",
+            "extra": "raw=165122; brotli=48473"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "manuel.schiller@caligano.de",
+            "name": "Manuel Schiller",
+            "username": "schiller-manuel"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "996e1828441311bc709d5c02aca251d84ca73045",
+          "message": "fix(start): prevent false positives for transitively resolved imports… (#6722)\n\nCo-authored-by: autofix-ci[bot] <114827586+autofix-ci[bot]@users.noreply.github.com>",
+          "timestamp": "2026-02-21T22:11:03+01:00",
+          "tree_id": "2ae16087c8e43a2d8c1bef3f8353b5b0e0631d97",
+          "url": "https://github.com/TanStack/router/commit/996e1828441311bc709d5c02aca251d84ca73045"
+        },
+        "date": 1771708407433,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
