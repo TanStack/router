45eab5ab5055b5a9536a486d7ad187045ddd6a4a
github-action-benchmark <github@users.noreply.github.com>
2026-03-03 14:24:51 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 2217f7c3f19f2b82c28cc1eec1b404ddfc3cfaab

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772531255575,
+  "lastUpdate": 1772547891531,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -2797,6 +2797,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/33188782e179c87bebc9b8aec96fbd14509a8cce"
         },
         "date": 1772531254582,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 88656,
+            "unit": "bytes",
+            "extra": "raw=278986; brotli=77021"
+          },
+          {
+            "name": "react-router.full",
+            "value": 91758,
+            "unit": "bytes",
+            "extra": "raw=289568; brotli=79771"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 36742,
+            "unit": "bytes",
+            "extra": "raw=110142; brotli=33034"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41179,
+            "unit": "bytes",
+            "extra": "raw=123506; brotli=37002"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 52996,
+            "unit": "bytes",
+            "extra": "raw=151077; brotli=47618"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 57904,
+            "unit": "bytes",
+            "extra": "raw=167039; brotli=52081"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 101492,
+            "unit": "bytes",
+            "extra": "raw=319062; brotli=87740"
+          },
+          {
+            "name": "react-start.full",
+            "value": 104961,
+            "unit": "bytes",
+            "extra": "raw=329104; brotli=90760"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49349,
+            "unit": "bytes",
+            "extra": "raw=148616; brotli=43689"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 54967,
+            "unit": "bytes",
+            "extra": "raw=164945; brotli=48506"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fpellet@ensc.fr",
+            "name": "Flo",
+            "username": "Sheraff"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "2217f7c3f19f2b82c28cc1eec1b404ddfc3cfaab",
+          "message": "test: client nav benchmark can detect granular re-rendering changes (#6808)",
+          "timestamp": "2026-03-03T15:22:34+01:00",
+          "tree_id": "2aa6450d5332d9a0ecbe40eb1dbd91c55e5307bf",
+          "url": "https://github.com/TanStack/router/commit/2217f7c3f19f2b82c28cc1eec1b404ddfc3cfaab"
+        },
+        "date": 1772547890098,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
