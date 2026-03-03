3c19d0efffbdad9a9ebbb1da222bbe0db141e223
Sheraff <florian.pellet@ensc.fr>
2026-03-03 07:46:35 +0100

    4x less time, will we get tracing?

--- benchmarks/ssr/bench-utils.ts
+++ benchmarks/ssr/bench-utils.ts
@@ -43,7 +43,7 @@ function randomRequestUrl(random: () => number) {
 
 export async function runSsrRequestLoop(
   handler: StartRequestHandler,
-  { seed, iterations = 20 }: RunSsrRequestLoopOptions,
+  { seed, iterations = 5 }: RunSsrRequestLoopOptions,
 ) {
   const random = createDeterministicRandom(seed)
   const pendingBodyReads: Array<Promise<void>> = []
