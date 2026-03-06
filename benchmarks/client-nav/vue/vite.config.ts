803b26189a533cd9e5e4f25a1955ec119380ab22
Sheraff <florian.pellet@ensc.fr>
2026-03-06 12:47:40 +0100

    benchmarks: define vue client-nav production env

--- benchmarks/client-nav/vue/vite.config.ts
+++ benchmarks/client-nav/vue/vite.config.ts
@@ -1,11 +1,16 @@
 import { defineConfig } from 'vitest/config'
+import vue from '@vitejs/plugin-vue'
 import vueJsx from '@vitejs/plugin-vue-jsx'
 import codspeedPlugin from '@codspeed/vitest-plugin'
 
 export default defineConfig({
+  define: {
+    'process.env.NODE_ENV': JSON.stringify('production'),
+  },
   plugins: [
     !!(process.env.VITEST && process.env.WITH_INSTRUMENTATION) &&
       codspeedPlugin(),
+    vue(),
     vueJsx(),
   ],
   build: {
