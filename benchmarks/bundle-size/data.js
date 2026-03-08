b0d3fc29156d159bf5f27f01fb3f764335e70526
github-action-benchmark <github@users.noreply.github.com>
2026-03-08 13:32:39 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 7003ccb50f7e3b692ebb3dba8c2dd0f45157d047

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1772961324456,
+  "lastUpdate": 1772976759103,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -3721,6 +3721,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/446d7973cfc5d930dc91cd8d1ef2d92e9fcb1296"
         },
         "date": 1772961323244,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 88925,
+            "unit": "bytes",
+            "extra": "raw=279895; brotli=77249"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92039,
+            "unit": "bytes",
+            "extra": "raw=290478; brotli=80059"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 37041,
+            "unit": "bytes",
+            "extra": "raw=111044; brotli=33269"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41472,
+            "unit": "bytes",
+            "extra": "raw=124407; brotli=37269"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53283,
+            "unit": "bytes",
+            "extra": "raw=151981; brotli=47830"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58210,
+            "unit": "bytes",
+            "extra": "raw=167943; brotli=52265"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 101787,
+            "unit": "bytes",
+            "extra": "raw=319982; brotli=88042"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105249,
+            "unit": "bytes",
+            "extra": "raw=330026; brotli=90948"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49656,
+            "unit": "bytes",
+            "extra": "raw=149537; brotli=43874"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55257,
+            "unit": "bytes",
+            "extra": "raw=165865; brotli=48649"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "fpellet@ensc.fr",
+            "name": "Flo",
+            "username": "Sheraff"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "7003ccb50f7e3b692ebb3dba8c2dd0f45157d047",
+          "message": "fix(react-router): correct useLoaderDeps structural sharing types (#6860)",
+          "timestamp": "2026-03-08T14:30:13+01:00",
+          "tree_id": "f7baa65efc92062a4448ec6ef1d39a55730d33f8",
+          "url": "https://github.com/TanStack/router/commit/7003ccb50f7e3b692ebb3dba8c2dd0f45157d047"
+        },
+        "date": 1772976758043,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
