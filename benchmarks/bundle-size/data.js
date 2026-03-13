11af3225bcb3c409b08a755ba6eded92192e905c
github-action-benchmark <github@users.noreply.github.com>
2026-03-13 19:07:05 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 80c3196246952c437c4ed8cf892b293800e24f3b

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773428108346,
+  "lastUpdate": 1773428825377,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -5569,6 +5569,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/ee3d2bdce3fd4a651b55ac65395afe05f7a1378f"
         },
         "date": 1773428107598,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89180,
+            "unit": "bytes",
+            "extra": "raw=280686; brotli=77499"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92238,
+            "unit": "bytes",
+            "extra": "raw=291186; brotli=80199"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 37299,
+            "unit": "bytes",
+            "extra": "raw=111917; brotli=33523"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41724,
+            "unit": "bytes",
+            "extra": "raw=125200; brotli=37454"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53541,
+            "unit": "bytes",
+            "extra": "raw=152925; brotli=48142"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58446,
+            "unit": "bytes",
+            "extra": "raw=168805; brotli=52504"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 102052,
+            "unit": "bytes",
+            "extra": "raw=320776; brotli=88290"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105442,
+            "unit": "bytes",
+            "extra": "raw=330738; brotli=91203"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49923,
+            "unit": "bytes",
+            "extra": "raw=150412; brotli=44175"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55515,
+            "unit": "bytes",
+            "extra": "raw=166658; brotli=49024"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "santosh.yadav198613@gmail.com",
+            "name": "Santosh Yadav",
+            "username": "santoshyadavdev"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "80c3196246952c437c4ed8cf892b293800e24f3b",
+          "message": "docs: fix partner logos in README (#6908)",
+          "timestamp": "2026-03-14T08:02:21+13:00",
+          "tree_id": "af87411c6fc5c31f6b0c9a9efbec015803da58c1",
+          "url": "https://github.com/TanStack/router/commit/80c3196246952c437c4ed8cf892b293800e24f3b"
+        },
+        "date": 1773428824189,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
