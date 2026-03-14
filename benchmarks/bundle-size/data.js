b2ff4c9cbbfaa4a365d0159e7c638a8652e1b090
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 09:55:25 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for e2811d39f6121e18fff6706db1d592495936d84e

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773474674213,
+  "lastUpdate": 1773482125781,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -5905,6 +5905,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/9db44632280c9e20cd90d26300cdac26226defb7"
         },
         "date": 1773474672981,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89221,
+            "unit": "bytes",
+            "extra": "raw=280804; brotli=77582"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92294,
+            "unit": "bytes",
+            "extra": "raw=291304; brotli=80318"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 37357,
+            "unit": "bytes",
+            "extra": "raw=112053; brotli=33594"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41777,
+            "unit": "bytes",
+            "extra": "raw=125336; brotli=37543"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53602,
+            "unit": "bytes",
+            "extra": "raw=153061; brotli=48147"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58509,
+            "unit": "bytes",
+            "extra": "raw=168941; brotli=52494"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 102105,
+            "unit": "bytes",
+            "extra": "raw=320894; brotli=88282"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105492,
+            "unit": "bytes",
+            "extra": "raw=330856; brotli=91219"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49966,
+            "unit": "bytes",
+            "extra": "raw=150548; brotli=44170"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55567,
+            "unit": "bytes",
+            "extra": "raw=166794; brotli=48960"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "nat@nize.ph",
+            "name": "Nathaniel John Tampus",
+            "username": "dotnize"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "e2811d39f6121e18fff6706db1d592495936d84e",
+          "message": "docs(start): update Nitro instructions for v3 beta (#6896)\n\n* docs: update Nitro instructions for v3 beta\n\n* docs: move nitro to dev dependencies\n\n* Apply @seancassiere suggestions from code review\n\nCo-authored-by: Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>\n\n---------\n\nCo-authored-by: Sean Cassiere <33615041+SeanCassiere@users.noreply.github.com>",
+          "timestamp": "2026-03-14T22:52:56+13:00",
+          "tree_id": "76dee7920797189c9c95729e291c7ff3d82d665d",
+          "url": "https://github.com/TanStack/router/commit/e2811d39f6121e18fff6706db1d592495936d84e"
+        },
+        "date": 1773482124448,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
