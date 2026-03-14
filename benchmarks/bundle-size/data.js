503cdcf0d620cc41773195c04e7ed9c1d34d1cbe
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 11:10:56 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for bb122e03796300572e1118ef282d13109d791e49

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773484278677,
+  "lastUpdate": 1773486656292,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -6073,6 +6073,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/773c7c9cb9c17646020286a2d8721e2f0b628f8c"
         },
         "date": 1773484277942,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89221,
+            "unit": "bytes",
+            "extra": "raw=280804; brotli=77582"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92294,
+            "unit": "bytes",
+            "extra": "raw=291304; brotli=80318"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 37357,
+            "unit": "bytes",
+            "extra": "raw=112053; brotli=33594"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41777,
+            "unit": "bytes",
+            "extra": "raw=125336; brotli=37543"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53602,
+            "unit": "bytes",
+            "extra": "raw=153061; brotli=48147"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58509,
+            "unit": "bytes",
+            "extra": "raw=168941; brotli=52494"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 102105,
+            "unit": "bytes",
+            "extra": "raw=320894; brotli=88282"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105492,
+            "unit": "bytes",
+            "extra": "raw=330856; brotli=91219"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49966,
+            "unit": "bytes",
+            "extra": "raw=150548; brotli=44170"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55567,
+            "unit": "bytes",
+            "extra": "raw=166794; brotli=48960"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "caweidmann.dev@gmail.com",
+            "name": "Cornelius",
+            "username": "caweidmann"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "bb122e03796300572e1118ef282d13109d791e49",
+          "message": "docs: Update README for translated pathnames (#6888)",
+          "timestamp": "2026-03-14T13:08:19+02:00",
+          "tree_id": "3268b192bba90ad1d684e9f5e8add8ea81bfebd4",
+          "url": "https://github.com/TanStack/router/commit/bb122e03796300572e1118ef282d13109d791e49"
+        },
+        "date": 1773486655424,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
