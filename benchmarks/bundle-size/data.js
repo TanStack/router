afaeeb1e582cb2d2ca7dfae13cb828fd7b450107
github-action-benchmark <github@users.noreply.github.com>
2026-03-14 11:34:11 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 29c0201441b58a8f143baf338be9827bf186444f

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773486656292,
+  "lastUpdate": 1773488051598,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -6157,6 +6157,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/bb122e03796300572e1118ef282d13109d791e49"
         },
         "date": 1773486655424,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89221,
+            "unit": "bytes",
+            "extra": "raw=280804; brotli=77582"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92294,
+            "unit": "bytes",
+            "extra": "raw=291304; brotli=80318"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 37357,
+            "unit": "bytes",
+            "extra": "raw=112053; brotli=33594"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41777,
+            "unit": "bytes",
+            "extra": "raw=125336; brotli=37543"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 53602,
+            "unit": "bytes",
+            "extra": "raw=153061; brotli=48147"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 58509,
+            "unit": "bytes",
+            "extra": "raw=168941; brotli=52494"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 102105,
+            "unit": "bytes",
+            "extra": "raw=320894; brotli=88282"
+          },
+          {
+            "name": "react-start.full",
+            "value": 105492,
+            "unit": "bytes",
+            "extra": "raw=330856; brotli=91219"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 49966,
+            "unit": "bytes",
+            "extra": "raw=150548; brotli=44170"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 55567,
+            "unit": "bytes",
+            "extra": "raw=166794; brotli=48960"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "113988347+s0h311@users.noreply.github.com",
+            "name": "Soheil",
+            "username": "s0h311"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "29c0201441b58a8f143baf338be9827bf186444f",
+          "message": "docs: import defaultRenderHandler instead of renderRouterToString in … (#6776)\n\ndocs: import defaultRenderHandler instead of renderRouterToString in ssr.md \n\nexample using defaultRenderHandler imports renderRouterToString instead of defaultRenderHandler\n\nCo-authored-by: Nico Lynzaad <44094871+nlynzaad@users.noreply.github.com>",
+          "timestamp": "2026-03-14T13:31:45+02:00",
+          "tree_id": "762ba71607ac03b8de56852b718f6a68e2445756",
+          "url": "https://github.com/TanStack/router/commit/29c0201441b58a8f143baf338be9827bf186444f"
+        },
+        "date": 1773488050233,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
