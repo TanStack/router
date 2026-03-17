0ec930f02247fd8a19e9b8425d472216cb86efa1
github-action-benchmark <github@users.noreply.github.com>
2026-03-17 17:38:52 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for 6651473d028a55c70f3f54af37a12b4379b46813

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1773701950945,
+  "lastUpdate": 1773769132297,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -6745,6 +6745,90 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/d6371529b5ab09af7d81463a6c4082b092411967"
         },
         "date": 1773701949482,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89714,
+            "unit": "bytes",
+            "extra": "raw=282693; brotli=77948"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92821,
+            "unit": "bytes",
+            "extra": "raw=293155; brotli=80570"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 38027,
+            "unit": "bytes",
+            "extra": "raw=114049; brotli=34088"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 42352,
+            "unit": "bytes",
+            "extra": "raw=127159; brotli=38014"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 54258,
+            "unit": "bytes",
+            "extra": "raw=155044; brotli=48791"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 59071,
+            "unit": "bytes",
+            "extra": "raw=170331; brotli=52839"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 104436,
+            "unit": "bytes",
+            "extra": "raw=331856; brotli=90321"
+          },
+          {
+            "name": "react-start.full",
+            "value": 107863,
+            "unit": "bytes",
+            "extra": "raw=341900; brotli=93205"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 52468,
+            "unit": "bytes",
+            "extra": "raw=161604; brotli=46364"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 57888,
+            "unit": "bytes",
+            "extra": "raw=177475; brotli=50913"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "74932975+birkskyum@users.noreply.github.com",
+            "name": "Birk Skyum",
+            "username": "birkskyum"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "6651473d028a55c70f3f54af37a12b4379b46813",
+          "message": "feat(vite-plugin-core): Spport both Vite 7 (`rollupOptions`) and Vite 8 (`rolldownOptions`) (#6955)",
+          "timestamp": "2026-03-17T18:35:17+01:00",
+          "tree_id": "55ec9d7509173c7b493f46edcd41a64d7070856d",
+          "url": "https://github.com/TanStack/router/commit/6651473d028a55c70f3f54af37a12b4379b46813"
+        },
+        "date": 1773769130077,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
