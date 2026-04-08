ba067287c19625bfc331c6db04b23ddaaec246ae
tmm <tmm@tmm.dev>
2026-04-07 23:05:48 -0400

    refactor: inline unmask-on-reload payload

--- packages/router-core/src/unmask-on-reload-script.ts
+++ packages/router-core/src/unmask-on-reload-script.ts
@@ -1,16 +1,10 @@
 import minifiedUnmaskOnReloadScript from './unmask-on-reload-inline?script-string'
 import { escapeHtml } from './utils'
 
-type InlineUnmaskOnReloadScriptOptions = {
-  routeMaskSources: Array<string>
-}
-
 export function getUnmaskOnReloadScript(routeMaskSources: Array<string>) {
   if (!routeMaskSources.length) return null
 
   return `(${minifiedUnmaskOnReloadScript})(${escapeHtml(
-    JSON.stringify({
-      routeMaskSources,
-    } satisfies InlineUnmaskOnReloadScriptOptions),
+    JSON.stringify({ routeMaskSources }),
   )})`
 }
