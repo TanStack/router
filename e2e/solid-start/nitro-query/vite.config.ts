e44ba1b1ffc9edd243542947844f33e8900f7141
Birk Skyum <birk.skyum@pm.me>
2026-04-26 04:05:16 +0200

    remote test

--- e2e/solid-start/nitro-query/vite.config.ts
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vite'
-import { tanstackStart } from '@tanstack/solid-start/plugin/vite'
-import solid from 'vite-plugin-solid'
-import { nitro } from 'nitro/vite'
-
-export default defineConfig({
-  plugins: [nitro(), tanstackStart(), solid({ ssr: true })],
-})
