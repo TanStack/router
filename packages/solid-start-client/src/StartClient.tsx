7b039d62bf2855150733ca8d77ae084ef0ad09b9
Brenley Dueck <brenleydueck@gmail.com>
2026-04-28 20:33:36 -0500

    Revert "fix: get soldi 2.0 beta.9 to pass"
    This reverts commit 5b4049675b0f2d1d7d0fe25ba0fabd99d8a3956d.

--- packages/solid-start-client/src/StartClient.tsx
+++ packages/solid-start-client/src/StartClient.tsx
@@ -1,12 +1,6 @@
-import * as Solid from 'solid-js'
 import { RouterProvider } from '@tanstack/solid-router'
 import type { AnyRouter } from '@tanstack/router-core'
 
 export function StartClient(props: { router: AnyRouter }) {
-  const routerProvider = <RouterProvider router={props.router} />
-  Solid.createRenderEffect(
-    () => Solid.flatten(routerProvider),
-    () => {},
-  )
-  return routerProvider
+  return <RouterProvider router={props.router} />
 }
