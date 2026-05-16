3be88cd6dd6ad5f09d4569dbb594b7a1fc05b54d
github-action-benchmark <github@users.noreply.github.com>
2026-05-16 05:05:25 +0000

    add Bundle Size (gzip) (customSmallerIsBetter) benchmark result for b3f2ab809e0fee12cd2a015a5256b8c451736d34

--- benchmarks/bundle-size/data.js
+++ benchmarks/bundle-size/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1778897455691,
+  "lastUpdate": 1778907925310,
   "repoUrl": "https://github.com/TanStack/router",
   "entries": {
     "Benchmark": [
@@ -16393,6 +16393,102 @@ window.BENCHMARK_DATA = {
           "url": "https://github.com/TanStack/router/commit/742941e2f1bf069c950d0a4985b2cd733639509e"
         },
         "date": 1778897454664,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "react-router.minimal",
+            "value": 89386,
+            "unit": "bytes",
+            "extra": "raw=280647; brotli=77673; initial_gzip=89245"
+          },
+          {
+            "name": "react-router.full",
+            "value": 92998,
+            "unit": "bytes",
+            "extra": "raw=292432; brotli=80788; initial_gzip=92858"
+          },
+          {
+            "name": "solid-router.minimal",
+            "value": 36363,
+            "unit": "bytes",
+            "extra": "raw=108916; brotli=32742; initial_gzip=36236"
+          },
+          {
+            "name": "solid-router.full",
+            "value": 41194,
+            "unit": "bytes",
+            "extra": "raw=123470; brotli=37009; initial_gzip=41066"
+          },
+          {
+            "name": "vue-router.minimal",
+            "value": 54564,
+            "unit": "bytes",
+            "extra": "raw=155147; brotli=48995; initial_gzip=54430"
+          },
+          {
+            "name": "vue-router.full",
+            "value": 59813,
+            "unit": "bytes",
+            "extra": "raw=171710; brotli=53646; initial_gzip=59680"
+          },
+          {
+            "name": "react-start.minimal",
+            "value": 104419,
+            "unit": "bytes",
+            "extra": "raw=330252; brotli=90283; initial_gzip=104279"
+          },
+          {
+            "name": "react-start.full",
+            "value": 107939,
+            "unit": "bytes",
+            "extra": "raw=340831; brotli=93378; initial_gzip=107799"
+          },
+          {
+            "name": "react-start.rsbuild.minimal",
+            "value": 101984,
+            "unit": "bytes",
+            "extra": "raw=324585; brotli=87773; initial_gzip=101808"
+          },
+          {
+            "name": "react-start.rsbuild.full",
+            "value": 105357,
+            "unit": "bytes",
+            "extra": "raw=335268; brotli=90617; initial_gzip=105181"
+          },
+          {
+            "name": "solid-start.minimal",
+            "value": 50803,
+            "unit": "bytes",
+            "extra": "raw=156140; brotli=44835; initial_gzip=50671"
+          },
+          {
+            "name": "solid-start.full",
+            "value": 56728,
+            "unit": "bytes",
+            "extra": "raw=173454; brotli=49889; initial_gzip=56595"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "41898282+github-actions[bot]@users.noreply.github.com",
+            "name": "github-actions[bot]",
+            "username": "github-actions[bot]"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "b3f2ab809e0fee12cd2a015a5256b8c451736d34",
+          "message": "ci: Version Packages (#7409)\n\nci: changeset release\n\nCo-authored-by: github-actions[bot] <41898282+github-actions[bot]@users.noreply.github.com>",
+          "timestamp": "2026-05-16T07:02:58+02:00",
+          "tree_id": "78da35d32e21707eae241761c6bf5b651fb4cf92",
+          "url": "https://github.com/TanStack/router/commit/b3f2ab809e0fee12cd2a015a5256b8c451736d34"
+        },
+        "date": 1778907924278,
         "tool": "customSmallerIsBetter",
         "benches": [
           {
