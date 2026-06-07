9aa8d6dee9f49798ab4d5c710c0dc7b8bff90101
Sheraff <me@florianpellet.com>
2026-06-07 16:48:43 +0200

    fix(benchmarks): preload SSR handlers before bench

--- benchmarks/ssr/vue/speed.bench.ts
+++ benchmarks/ssr/vue/speed.bench.ts
@@ -5,40 +5,18 @@ import type { StartRequestHandler } from '../bench-utils'
 const appModuleUrl = new URL('./dist/server/server.js', import.meta.url).href
 const benchmarkSeed = 0xdeadbeef
 
-let handler: StartRequestHandler | undefined
-let handlerPromise: Promise<StartRequestHandler> | undefined
-
-async function loadHandler() {
-  if (!handlerPromise) {
-    handlerPromise = import(/* @vite-ignore */ appModuleUrl).then((module) => {
-      return (module as { default: StartRequestHandler }).default
-    })
-  }
-
-  handler = await handlerPromise
-  return handler
-}
-
-async function setup() {
-  await loadHandler()
-}
-
-function teardown() {
-  handler = undefined
-}
-
-async function runBenchmark() {
-  // CodSpeed calls the benchmark body directly and bypasses tinybench setup.
-  const currentHandler = handler ?? (await loadHandler())
-  await runSsrRequestLoop(currentHandler, { seed: benchmarkSeed })
+const { default: handler } = (await import(/* @vite-ignore */ appModuleUrl)) as {
+  default: StartRequestHandler
 }
 
 describe('ssr', () => {
-  bench('ssr request loop (vue)', runBenchmark, {
-    warmupIterations: 100,
-    time: 10_000,
-    setup,
-    teardown,
-    throws: true,
-  })
+  bench(
+    'ssr request loop (vue)',
+    () => runSsrRequestLoop(handler, { seed: benchmarkSeed }),
+    {
+      warmupIterations: 100,
+      time: 10_000,
+      throws: true,
+    },
+  )
 })
