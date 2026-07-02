eb7ead89cb4f6685f17ddd81843313f079359f17
Cong-Cong Pan <dacongsama@live.com>
2026-07-02 15:31:01 +0800

    chore: add React Start basic Rsbuild example

---
+++ examples/react/start-basic-rsbuild/src/routes/_pathlessLayout/_nested-layout/route-b.tsx
@@ -0,0 +1,11 @@
+import { createFileRoute } from '@tanstack/react-router'
+
+export const Route = createFileRoute('/_pathlessLayout/_nested-layout/route-b')(
+  {
+    component: LayoutBComponent,
+  },
+)
+
+function LayoutBComponent() {
+  return <div>I'm B!</div>
+}
