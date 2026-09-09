18b52ab21580352ad1f7a5a553bb8bd72ca02637
Corbin Crutchley <git@crutchcorn.dev>
2026-09-09 12:26:15 -0700

    fix: add isServer edgecase to Vue Router

--- packages/vue-router/src/routerStores.ts
+++ packages/vue-router/src/routerStores.ts
@@ -1,15 +1,26 @@
 import { batch, createAtom } from '@tanstack/vue-store'
-import type { GetStoreConfig } from '@tanstack/router-core'
+import {
+  createNonReactiveMutableStore,
+  createNonReactiveReadonlyStore,
+} from '@tanstack/router-core'
+import { isServer } from '@tanstack/router-core/isServer'
 import type { Readable } from '@tanstack/vue-store'
+import type { GetStoreConfig } from '@tanstack/router-core'
 
 declare module '@tanstack/router-core' {
   export interface RouterReadableStore<TValue> extends Readable<TValue> {}
 }
-
-export const getStoreFactory: GetStoreConfig = (_opts) => {
+export const getStoreFactory: GetStoreConfig = (opts) => {
+  if (isServer ?? opts.isServer) {
+    return {
+      createMutableStore: createNonReactiveMutableStore,
+      createReadonlyStore: createNonReactiveReadonlyStore,
+      batch: (fn) => fn(),
+    }
+  }
   return {
     createMutableStore: createAtom,
     createReadonlyStore: createAtom,
-    batch,
+    batch: batch,
   }
 }
